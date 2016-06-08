66084df97aa7ffee56fec277814f4aca4e49b69d
Jeroen Engels <jfm.engels@gmail.com>
2016-06-08 20:24:13 +0200

    Get nice feedback with RuleTester using AVA

--- test/catch-error-name.js
+++ test/catch-error-name.js
@@ -1,8 +1,8 @@
 import test from 'ava';
-import {RuleTester} from 'eslint';
+import avaRuleTester from 'eslint-ava-rule-tester';
 import rule from '../rules/catch-error-name';
 
-const ruleTester = new RuleTester({
+const ruleTester = avaRuleTester(test, {
 	env: {
 		es6: true
 	}
@@ -16,44 +16,42 @@ function testCase(code, name, error) {
 	};
 }
 
-test(() => {
-	ruleTester.run('catch-error-name', rule, {
-		valid: [
-			testCase('try {} catch (err) {}'),
-			testCase('try {} catch (error) {}', 'error'),
-			testCase('try {} catch (outerError) { try {} catch (innerError) {} }'),
-			testCase('obj.catch(err => {})'),
-			testCase('obj.catch(() => {})'),
-			testCase('obj.catch(error => {})', 'error'),
-			testCase('obj.catch(outerError => { return obj2.catch(innerError => {}) })'),
-			testCase('obj.catch(function (err) {})'),
-			testCase('obj.catch(function () {})'),
-			testCase('obj.catch(function (error) {})', 'error'),
-			testCase('obj.catch(function (outerError) { return obj2.catch(function (innerError) {}) })'),
-			testCase('obj.catch()'),
-			testCase('foo(function (err) {})'),
-			testCase('foo().then(function (err) {})'),
-			testCase('foo().catch(function (err) {})')
-		],
-		invalid: [
-			testCase('try {} catch (error) {}', null, true),
-			testCase('try {} catch (err) {}', 'error', true),
-			testCase('try {} catch (outerError) {}', null, true),
-			testCase('try {} catch (innerError) {}', null, true),
-			testCase('obj.catch(error => {})', null, true),
-			testCase('obj.catch(err => {})', 'error', true),
-			testCase('obj.catch(function (error) {})', null, true),
-			testCase('obj.catch(function (err) {})', 'error', true),
-			{
-				code: `
-					obj.catch(error => {});
-					obj.catch(error => {});
-				`,
-				errors: [
-					{ruleId: 'catch-error-name'},
-					{ruleId: 'catch-error-name'}
-				]
-			}
-		]
-	});
+ruleTester.run('catch-error-name', rule, {
+	valid: [
+		testCase('try {} catch (err) {}'),
+		testCase('try {} catch (error) {}', 'error'),
+		testCase('try {} catch (outerError) { try {} catch (innerError) {} }'),
+		testCase('obj.catch(err => {})'),
+		testCase('obj.catch(() => {})'),
+		testCase('obj.catch(error => {})', 'error'),
+		testCase('obj.catch(outerError => { return obj2.catch(innerError => {}) })'),
+		testCase('obj.catch(function (err) {})'),
+		testCase('obj.catch(function () {})'),
+		testCase('obj.catch(function (error) {})', 'error'),
+		testCase('obj.catch(function (outerError) { return obj2.catch(function (innerError) {}) })'),
+		testCase('obj.catch()'),
+		testCase('foo(function (err) {})'),
+		testCase('foo().then(function (err) {})'),
+		testCase('foo().catch(function (err) {})')
+	],
+	invalid: [
+		testCase('try {} catch (error) {}', null, true),
+		testCase('try {} catch (err) {}', 'error', true),
+		testCase('try {} catch (outerError) {}', null, true),
+		testCase('try {} catch (innerError) {}', null, true),
+		testCase('obj.catch(error => {})', null, true),
+		testCase('obj.catch(err => {})', 'error', true),
+		testCase('obj.catch(function (error) {})', null, true),
+		testCase('obj.catch(function (err) {})', 'error', true),
+		{
+			code: `
+				obj.catch(error => {});
+				obj.catch(error => {});
+			`,
+			errors: [
+				{ruleId: 'catch-error-name'},
+				{ruleId: 'catch-error-name'}
+			]
+		}
+	]
 });
--- test/no-abusive-eslint-disable.js
+++ test/no-abusive-eslint-disable.js
@@ -1,8 +1,8 @@
 import test from 'ava';
-import {RuleTester} from 'eslint';
+import avaRuleTester from 'eslint-ava-rule-tester';
 import rule from '../rules/no-abusive-eslint-disable';
 
-const ruleTester = new RuleTester({
+const ruleTester = avaRuleTester(test, {
 	env: {
 		es6: true
 	}
@@ -13,44 +13,42 @@ const error = (message) => ({
 	message
 });
 
-test(() => {
-	ruleTester.run('no-abusive-eslint-disable', rule, {
-		valid: [
-			`eval();`,
-			`eval(); // eslint-disable-line no-eval`,
-			`eval(); // eslint-disable-line no-eval, no-console`,
-			`eval(); //eslint-disable-line no-eval`,
-			`eval(); //     eslint-disable-line no-eval`,
-			`eval(); //\teslint-disable-line no-eval`,
-			`eval(); /* eslint-disable-line no-eval */`,
-			`eval(); /* eslint-disable-line no-eval */`,
-			`eval(); // eslint-line-disable`,
-			`eval(); // some comment`,
-			`foo();
-			// eslint-disable-line no-eval
-			eval();`,
-			'/* eslint-disable no-eval */',
-			`foo();
-			/* eslint-disable no-eval */
-			eval();`
-		],
-		invalid: [
-			{
-				code: `eval(); // eslint-disable-line`,
-				errors: [error('Specify the rules you want to disable at line 1:8')]
-			},
-			{
-				code: 'foo();\neval(); // eslint-disable-line',
-				errors: [error('Specify the rules you want to disable at line 2:8')]
-			},
-			{
-				code: '/* eslint-disable */',
-				errors: [error('Specify the rules you want to disable at line 1:0')]
-			},
-			{
-				code: 'foo();\n/* eslint-disable */\neval();',
-				errors: [error('Specify the rules you want to disable at line 2:0')]
-			}
-		]
-	});
+ruleTester.run('no-abusive-eslint-disable', rule, {
+	valid: [
+		`eval();`,
+		`eval(); // eslint-disable-line no-eval`,
+		`eval(); // eslint-disable-line no-eval, no-console`,
+		`eval(); //eslint-disable-line no-eval`,
+		`eval(); //     eslint-disable-line no-eval`,
+		`eval(); //\teslint-disable-line no-eval`,
+		`eval(); /* eslint-disable-line no-eval */`,
+		`eval(); /* eslint-disable-line no-eval */`,
+		`eval(); // eslint-line-disable`,
+		`eval(); // some comment`,
+		`foo();
+		// eslint-disable-line no-eval
+		eval();`,
+		'/* eslint-disable no-eval */',
+		`foo();
+		/* eslint-disable no-eval */
+		eval();`
+	],
+	invalid: [
+		{
+			code: `eval(); // eslint-disable-line`,
+			errors: [error('Specify the rules you want to disable at line 1:8')]
+		},
+		{
+			code: 'foo();\neval(); // eslint-disable-line',
+			errors: [error('Specify the rules you want to disable at line 2:8')]
+		},
+		{
+			code: '/* eslint-disable */',
+			errors: [error('Specify the rules you want to disable at line 1:0')]
+		},
+		{
+			code: 'foo();\n/* eslint-disable */\neval();',
+			errors: [error('Specify the rules you want to disable at line 2:0')]
+		}
+	]
 });
