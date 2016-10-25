59946b424d54517377422cbcf8b0a977559d2cb8
Matijs Brinkhuis <matijs@gmail.com>
2016-10-25 17:07:55 +0200

    Fix nonsensical test, add more tests

--- rules/prefer-string-slice.js
+++ rules/prefer-string-slice.js
@@ -3,12 +3,12 @@
 const create = context => {
 	return {
 		Identifier: node => {
-			const name = node.name;
-
-			if (name === 'substr' || name === 'substring') {
+			if (['substr', 'substring'].indexOf(node.name) > -1) {
+				const args = node.parent.parent.arguments;
+				console.dir(args[0].type);
 				context.report({
 					node,
-					message: `Use \`String.slice\` instead of \`String.${name}\`.`
+					message: `Use \`String.slice\` instead of \`String.${node.name}\`.`
 				});
 			}
 		}
