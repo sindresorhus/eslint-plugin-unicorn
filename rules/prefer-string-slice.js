e8231268311c157d41ae62025416f6612ac3b792
Matijs Brinkhuis <matijs@gmail.com>
2016-10-26 14:51:41 +0200

    Remove console.dir

--- rules/prefer-string-slice.js
+++ rules/prefer-string-slice.js
@@ -3,9 +3,7 @@
 const create = context => {
 	return {
 		Identifier: node => {
-			if (['substr', 'substring'].indexOf(node.name) > -1) {
-				const args = node.parent.parent.arguments;
-				console.dir(args[0].type);
+			if (['substr', 'substring'].indexOf(node.name) !== -1) {
 				context.report({
 					node,
 					message: `Use \`String.slice\` instead of \`String.${node.name}\`.`
