0bde2883ea05f537a0dbc5766e8745ddf76b222a
Matijs Brinkhuis <matijs@gmail.com>
2016-10-27 08:57:04 +0200

    Make no-hex-escape more concise

--- rules/no-hex-escape.js
+++ rules/no-hex-escape.js
@@ -1,25 +1,21 @@
 'use strict';
 
-function checkEscape(context, node, value) {
-	const fixedValue = typeof value === 'string' ? value.replace(/\\x/g, '\\u00') : value;
-
-	if (value !== fixedValue) {
-		context.report({
-			node,
-			message: 'Use unicode escapes instead of hexadecimal escapes.',
-			fix: fixer => fixer.replaceTextRange([node.start, node.end], fixedValue)
-		});
-	}
-}
-
 const create = context => {
-	return {
-		Literal: node => {
-			checkEscape(context, node, node.raw);
-		},
-		TemplateElement: node => {
-			checkEscape(context, node, node.value.raw);
+	const checkEscape = (node, value) => {
+		const fixedValue = typeof value === 'string' ? value.replace(/\\x/g, '\\u00') : value;
+
+		if (value !== fixedValue) {
+			context.report({
+				node,
+				message: 'Use unicode escapes instead of hexadecimal escapes.',
+				fix: fixer => fixer.replaceTextRange([node.start, node.end], fixedValue)
+			});
 		}
+	};
+
+	return {
+		Literal: node => checkEscape(node, node.raw),
+		TemplateElement: node => checkEscape(node, node.value.raw)
 	};
 };
 
