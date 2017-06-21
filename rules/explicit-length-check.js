b7da70b369e1ceb71ad4126d8dc03d7790505366
Paulo Marcos <paulobmarcos@outlook.com>
2017-06-21 22:19:16 +0100

    Add support for empty option in the `explicit-length-check` rule (#56)

--- rules/explicit-length-check.js
+++ rules/explicit-length-check.js
@@ -1,6 +1,62 @@
 'use strict';
 
+const types = {
+	eq: ['===', '=='],
+	lt: ['<']
+};
+
+function checkComparisionType(type, operator, value) {
+	switch (type) {
+		case 'eq':
+			if (types.lt.includes(operator) && value === 1) {
+				return 'empty `length` should be compared with `=== 0`';
+			}
+			break;
+		case 'lt':
+			if (types.eq.includes(operator) && value === 0) {
+				return 'empty `length` should be compared with `< 1`';
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+function checkBinaryExpression(context, node, empty) {
+	let arrayNode;
+	let valueNode;
+
+	if (node.left.type === 'Literal' && node.right.type === 'MemberExpression') {
+		context.report({
+			node,
+			message: '`length` property should be first argument of comparision'
+		});
+		return;
+	}
+
+	if (node.left.type === 'MemberExpression' && node.right.type === 'Literal') {
+		arrayNode = node.left;
+		valueNode = node.right;
+	}
+
+	if (arrayNode &&
+		valueNode &&
+		arrayNode.property.type === 'Identifier' &&
+		arrayNode.property.name === 'length'
+	) {
+		const compTypeRes = checkComparisionType(empty, node.operator, valueNode.value);
+		if (typeof compTypeRes === 'string') {
+			context.report({
+				node,
+				message: compTypeRes
+			});
+		}
+	}
+}
+
 function checkExpression(context, node) {
+	const {empty} = context.options[0] || {};
+
 	if (node.type === 'LogicalExpression') {
 		checkExpression(context, node.left);
 		checkExpression(context, node.right);
@@ -10,6 +66,10 @@ function checkExpression(context, node) {
 	if (node.type === 'UnaryExpression' && node.operator === '!') {
 		checkExpression(context, node.argument);
 		return;
+	}
+
+	if (node.type === 'BinaryExpression') {
+		checkBinaryExpression(context, node, empty);
 	}
 
 	if (node.type === 'MemberExpression' &&
--- test/explicit-length-check.js
+++ test/explicit-length-check.js
@@ -8,58 +8,97 @@ const ruleTester = avaRuleTester(test, {
 	}
 });
 
-const errors = [{
-	ruleId: 'explicit-length-check',
-	message: '`length` property should be compared to a value.'
-}];
+const errorMessages = {
+	lengthFirst: '`length` property should be first argument of comparision',
+	compareToValue: '`length` property should be compared to a value.',
+	emptyEqual: 'empty `length` should be compared with `=== 0`',
+	emptyLess: 'empty `length` should be compared with `< 1`'
+};
+
+function testCase(code, emptyType, error) {
+	return {
+		code,
+		options: [{empty: emptyType}],
+		errors: [{
+			ruleId: 'explicit-length-check',
+			message: error
+		}]
+	};
+}
 
 ruleTester.run('explicit-length-check', rule, {
 	valid: [
-		'array.foo',
-		'array.length',
-		'array.length === 0',
-		'array.length !== 0',
-		'array.length > 0',
-		'if (array.foo) {}',
-		'if (length) {}',
-		'if ([].length > 0) {}',
-		'if ("".length > 0) {}',
-		'if (array.length === 0) {}',
-		'if (array.length !== 0) {}',
-		'if (array.length !== 0 && array[0] === 1) {}'
+		testCase('array.foo'),
+		testCase('array.length'),
+		testCase('array.length === 0'),
+		testCase('array.length !== 0'),
+		testCase('array.length > 0'),
+		testCase('if (array.foo) {}'),
+		testCase('if (length) {}'),
+		testCase('if ([].length > 0) {}'),
+		testCase('if ("".length > 0) {}'),
+		testCase('if (array.length === 0) {}'),
+		testCase('if (array.length !== 0) {}'),
+		testCase('if (array.length !== 0 && array[0] === 1) {}'),
+		testCase('if (array.length == 0) {}', 'eq'),
+		testCase('if (array.length === 0) {}', 'eq'),
+		testCase('if (array.length === 1) {}', 'eq'),
+		testCase('if (array.length <= 1) {}', 'eq'),
+		testCase('if (array.length > 1) {}', 'eq'),
+		testCase('if ([].length < 1) {}', 'lt')
 	],
 	invalid: [
-		{
-			code: 'if ([].length) {}',
-			errors
-		},
-		{
-			code: 'if ("".length) {}',
-			errors
-		},
-		{
-			code: 'if (array.length) {}',
-			errors
-		},
-		{
-			code: 'if (!array.length) {}',
-			errors
-		},
-		{
-			code: 'if (array.foo.length) {}',
-			errors
-		},
-		{
-			code: 'if (!!array.length) {}',
-			errors
-		},
-		{
-			code: 'if (array.length && array[0] === 1) {}',
-			errors
-		},
-		{
-			code: 'if (array[0] === 1 || array.length) {}',
-			errors
-		}
+		testCase('if ([].length) {}',
+			undefined,
+			errorMessages.compareToValue
+		),
+		testCase('if ("".length) {}',
+			undefined,
+			errorMessages.compareToValue
+		),
+		testCase('if (array.length) {}',
+			undefined,
+			errorMessages.compareToValue
+		),
+		testCase('if (!array.length) {}',
+			undefined,
+			errorMessages.compareToValue
+		),
+		testCase('if (array.foo.length) {}',
+			undefined,
+			errorMessages.compareToValue
+		),
+		testCase('if (!!array.length) {}',
+			undefined,
+			errorMessages.compareToValue
+		),
+		testCase('if (array.length && array[0] === 1) {}',
+			undefined,
+			errorMessages.compareToValue
+		),
+		testCase('if (array[0] === 1 || array.length) {}',
+			undefined,
+			errorMessages.compareToValue
+		),
+		testCase('if (1 === array.length) {}',
+			undefined,
+			errorMessages.lengthFirst
+		),
+		testCase('if ([].length === 0 || 0 < array.length) {}',
+			undefined,
+			errorMessages.lengthFirst
+		),
+		testCase('if (array.length < 1) {}',
+			'eq',
+			errorMessages.emptyEqual
+		),
+		testCase('if (array.length === 0) {}',
+			'lt',
+			errorMessages.emptyLess
+		),
+		testCase('if (array.length == 0) {}',
+			'lt',
+			errorMessages.emptyLess
+		)
 	]
 });
