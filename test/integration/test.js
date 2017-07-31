9629b9184f4e421cae135f7a013d2df0f92d615f
Sam Verschueren <sam.verschueren@gmail.com>
2017-07-31 12:07:49 +0200

    Add integration tests - fixes #84 (#106)

---
+++ test/integration/test.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+'use strict';
+const path = require('path');
+const Listr = require('listr');
+const tempy = require('tempy');
+const execa = require('execa');
+const del = require('del');
+const chalk = require('chalk');
+
+const packages = new Map([
+	['ava', 'https://github.com/avajs/ava'],
+	['chalk', 'https://github.com/chalk/chalk'],
+	['wrap-ansi', 'https://github.com/chalk/wrap-ansi'],
+	['got', 'https://github.com/sindresorhus/got'],
+	['pageres', 'https://github.com/sindresorhus/pageres'],
+	['np', 'https://github.com/sindresorhus/np'],
+	['ora', 'https://github.com/sindresorhus/ora'],
+	['p-map', 'https://github.com/sindresorhus/p-map'],
+	['detect-indent', 'https://github.com/sindresorhus/detect-indent'],
+	['os-locale', 'https://github.com/sindresorhus/os-locale'],
+	['execa', 'https://github.com/sindresorhus/execa'],
+	['pify', 'https://github.com/sindresorhus/pify'],
+	['boxen', 'https://github.com/sindresorhus/boxen'],
+	['make-dir', 'https://github.com/sindresorhus/make-dir'],
+	['listr', 'http://github.com/SamVerschueren/listr'],
+	['listr-update-renderer', 'http://github.com/SamVerschueren/listr-update-renderer'],
+	['clinton', 'http://github.com/SamVerschueren/clinton'],
+	['bragg', 'http://github.com/SamVerschueren/bragg'],
+	['bragg-router', 'http://github.com/SamVerschueren/bragg-router'],
+	['dev-time', 'http://github.com/SamVerschueren/dev-time'],
+	['decode-uri-component', 'https://github.com/SamVerschueren/decode-uri-component'],
+	['to-ico', 'https://github.com/kevva/to-ico'],
+	['download', 'https://github.com/kevva/download'],
+	['brightness', 'https://github.com/kevva/brightness'],
+	['decompress', 'https://github.com/kevva/decompress'],
+	['npm-conf', 'https://github.com/kevva/npm-conf'],
+	['imagemin', 'https://github.com/imagemin/imagemin'],
+	['color-convert', 'https://github.com/qix-/color-convert']
+]);
+
+const cwd = path.join(__dirname, 'eslint-config-unicorn-tester');
+
+const execute = name => {
+	const dest = tempy.directory();
+
+	return new Listr([
+		{
+			title: 'Cloning',
+			task: () => execa('git', ['clone', packages.get(name), '--single-branch', dest])
+		},
+		{
+			title: 'Running tests',
+			task: () => execa('eslint', ['--config', path.join(cwd, 'index.js'), dest], {cwd, localDir: __dirname})
+				.catch(err => {
+					if (!/✖ \d+ problems? \(\d+ errors?, \d+ warnings?\)/.test(err.message)) {
+						err.package = name;
+						throw err;
+					}
+				})
+		},
+		{
+			title: 'Clean up',
+			task: () => del(dest, {force: true})
+		}
+	], {
+		exitOnError: false
+	});
+};
+
+const list = new Listr([
+	{
+		title: 'Setup',
+		task: () => execa('npm', ['install', '../../..', 'eslint'], {cwd})
+	},
+	{
+		title: 'Integration tests',
+		task: () => {
+			const tests = new Listr({concurrent: true});
+
+			for (const [name] of packages) {
+				tests.add([
+					{
+						title: name,
+						task: () => execute(name)
+					}
+				]);
+			}
+
+			return tests;
+		}
+	}
+]);
+
+list.run()
+	.catch(err => {
+		for (const error of err.errors) {
+			console.error('\n' + chalk.red.bold.underline(error.package));
+			console.error(error.message);
+		}
+
+		process.exit(1);
+	});
