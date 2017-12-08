4847a3dc6dd4779a6e64f1fc182ecc0bf9c8c504
Sam Verschueren <sam.verschueren@gmail.com>
2017-12-08 01:20:39 +0100

    Fix custom error definition when exporting - fixes #127 (#132)

--- rules/custom-error-definition.js
+++ rules/custom-error-definition.js
@@ -42,85 +42,88 @@ const isAssignmentExpression = (node, name) => {
 	return lhs.property.name === name;
 };
 
+const customErrorDefinition = (context, node) => {
+	if (!hasValidSuperClass(node)) {
+		return;
+	}
+
+	const name = node.id.name;
+	const className = getClassName(name);
+
+	if (name !== className) {
+		context.report({
+			node: node.id,
+			message: `Invalid class name, use \`${className}\`.`
+		});
+	}
+
+	const body = node.body.body;
+
+	const constructor = body.find(x => x.kind === 'constructor');
+
+	if (!constructor) {
+		context.report({
+			node,
+			message: 'Add a constructor to your error.',
+			fix: fixer => fixer.insertTextAfterRange([
+				node.body.start,
+				node.body.start + 1
+			], getConstructorMethod(name))
+		});
+		return;
+	}
+
+	const constructorBodyNode = constructor.value.body;
+	const constructorBody = constructorBodyNode.body;
+
+	const superExpression = constructorBody.find(isSuperExpression);
+	const messageExpressionIndex = constructorBody.findIndex(x => isAssignmentExpression(x, 'message'));
+
+	if (!superExpression) {
+		context.report({
+			node: constructorBodyNode,
+			message: 'Missing call to `super()` in constructor.'
+		});
+	} else if (messageExpressionIndex !== -1 && superExpression.expression.arguments.length === 0) {
+		const rhs = constructorBody[messageExpressionIndex].expression.right;
+
+		context.report({
+			node: superExpression,
+			message: 'Pass the error message to `super()`.',
+			fix: fixer => fixer.insertTextAfterRange([
+				superExpression.start,
+				superExpression.start + 6
+			], rhs.raw || rhs.name)
+		});
+	}
+
+	if (messageExpressionIndex !== -1) {
+		const expression = constructorBody[messageExpressionIndex];
+
+		context.report({
+			node: expression,
+			message: 'Pass the error message to `super()` instead of setting `this.message`.',
+			fix: fixer => fixer.removeRange([
+				messageExpressionIndex === 0 ? constructorBodyNode.start : constructorBody[messageExpressionIndex - 1].end,
+				expression.end
+			])
+		});
+	}
+
+	const nameExpression = constructorBody.find(x => isAssignmentExpression(x, 'name'));
+
+	if (!nameExpression || nameExpression.expression.right.value !== name) {
+		context.report({
+			node: nameExpression ? nameExpression.expression.right : constructorBodyNode,
+			message: `The \`name\` property should be set to \`${name}\`.`
+		});
+	}
+};
+
 const create = context => {
 	return {
-		ClassDeclaration: node => {
-			if (!hasValidSuperClass(node)) {
-				return;
-			}
-
-			const name = node.id.name;
-			const className = getClassName(name);
-
-			if (name !== className) {
-				context.report({
-					node: node.id,
-					message: `Invalid class name, use \`${className}\`.`
-				});
-			}
-
-			const body = node.body.body;
-
-			const constructor = body.find(x => x.kind === 'constructor');
-
-			if (!constructor) {
-				context.report({
-					node,
-					message: 'Add a constructor to your error.',
-					fix: fixer => fixer.insertTextAfterRange([
-						node.body.start,
-						node.body.start + 1
-					], getConstructorMethod(name))
-				});
-				return;
-			}
-
-			const constructorBodyNode = constructor.value.body;
-			const constructorBody = constructorBodyNode.body;
-
-			const superExpression = constructorBody.find(isSuperExpression);
-			const messageExpressionIndex = constructorBody.findIndex(x => isAssignmentExpression(x, 'message'));
-
-			if (!superExpression) {
-				context.report({
-					node: constructorBodyNode,
-					message: 'Missing call to `super()` in constructor.'
-				});
-			} else if (messageExpressionIndex !== -1 && superExpression.expression.arguments.length === 0) {
-				const rhs = constructorBody[messageExpressionIndex].expression.right;
-
-				context.report({
-					node: superExpression,
-					message: 'Pass the error message to `super()`.',
-					fix: fixer => fixer.insertTextAfterRange([
-						superExpression.start,
-						superExpression.start + 6
-					], rhs.raw || rhs.name)
-				});
-			}
-
-			if (messageExpressionIndex !== -1) {
-				const expression = constructorBody[messageExpressionIndex];
-
-				context.report({
-					node: expression,
-					message: 'Pass the error message to `super()` instead of setting `this.message`.',
-					fix: fixer => fixer.removeRange([
-						messageExpressionIndex === 0 ? constructorBodyNode.start : constructorBody[messageExpressionIndex - 1].end,
-						expression.end
-					])
-				});
-			}
-
-			const nameExpression = constructorBody.find(x => isAssignmentExpression(x, 'name'));
-
-			if (!nameExpression || nameExpression.expression.right.value !== name) {
-				context.report({
-					node: nameExpression ? nameExpression.expression.right : constructorBodyNode,
-					message: `The \`name\` property should be set to \`${name}\`.`
-				});
-			}
-		}
+		ClassDeclaration: node => customErrorDefinition(context, node),
+		'AssignmentExpression[right.type="ClassExpression"]': node => customErrorDefinition(context, node.right)
 	};
 };
 
