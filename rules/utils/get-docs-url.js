9b35aa37df28f4ce863db84d161a3977490af578
Landon Abney <Landonabney@gmail.com>
2018-01-09 08:55:03 -0800

    :art: Address feedback

--- rules/utils/get-docs-url.js
+++ rules/utils/get-docs-url.js
@@ -1,10 +1,10 @@
 'use strict';
-const {basename} = require('path');
+const path = require('path');
 
 const repoUrl = 'https://github.com/sindresorhus/eslint-plugin-unicorn';
 
-module.exports = function (ruleName, commitHash) {
-	ruleName = ruleName || basename(module.parent.filename).replace('.js', '');
+module.exports = (ruleName, commitHash) => {
+	ruleName = ruleName || path.basename(module.parent.filename, '.js');
 	commitHash = commitHash || 'master';
 
 	return `${repoUrl}/blob/${commitHash}/docs/rules/${ruleName}.md`;
--- test/get-docs-url.js
+++ test/get-docs-url.js
@@ -2,19 +2,16 @@ import test from 'ava';
 import getDocsUrl from '../rules/utils/get-docs-url';
 
 test('returns the URL of the a named rule\'s documentation', t => {
-	t.plan(1);
 	const url = 'https://github.com/sindresorhus/eslint-plugin-unicorn/blob/master/docs/rules/foo.md';
 	t.is(getDocsUrl('foo'), url);
 });
 
 test('returns the URL of the a named rule\'s documentation at a commit hash', t => {
-	t.plan(1);
 	const url = 'https://github.com/sindresorhus/eslint-plugin-unicorn/blob/bar/docs/rules/foo.md';
 	t.is(getDocsUrl('foo', 'bar'), url);
 });
 
 test('determines the rule name from the file', t => {
-	t.plan(1);
 	const url = 'https://github.com/sindresorhus/eslint-plugin-unicorn/blob/master/docs/rules/get-docs-url.md';
 	t.is(getDocsUrl(), url);
 });
