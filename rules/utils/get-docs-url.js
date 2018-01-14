1a29f9e71c97b20639e057b42a93a01f4275070b
Sindre Sorhus <sindresorhus@gmail.com>
2018-01-14 23:21:42 +0100

    Update get-docs-url.js

--- rules/utils/get-docs-url.js
+++ rules/utils/get-docs-url.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const repoUrl = 'https://github.com/sindresorhus/eslint-plugin-unicorn';
 
-module.exports = (ruleName) => {
+module.exports = ruleName => {
 	ruleName = ruleName || path.basename(module.parent.filename, '.js');
 	return `${repoUrl}/blob/master/docs/rules/${ruleName}.md`;
 };
