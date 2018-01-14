5a5e8ac7dca80b3b78c747cc8690ad4b3e2755aa
Sindre Sorhus <sindresorhus@gmail.com>
2018-01-14 23:31:36 +0100

    Update get-docs-url.js

--- test/get-docs-url.js
+++ test/get-docs-url.js
@@ -6,11 +6,6 @@ test('returns the URL of the a named rule\'s documentation', t => {
 	t.is(getDocsUrl('foo'), url);
 });
 
-test('returns the URL of the a named rule\'s documentation at a commit hash', t => {
-	const url = 'https://github.com/sindresorhus/eslint-plugin-unicorn/blob/bar/docs/rules/foo.md';
-	t.is(getDocsUrl('foo', 'bar'), url);
-});
-
 test('determines the rule name from the file', t => {
 	const url = 'https://github.com/sindresorhus/eslint-plugin-unicorn/blob/master/docs/rules/get-docs-url.md';
 	t.is(getDocsUrl(), url);
