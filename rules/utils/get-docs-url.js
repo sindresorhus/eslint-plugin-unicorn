1c5fdf07e0a87bdccf04cb653ee703469e9caead
Suhas Karanth <sudo-suhas@users.noreply.github.com>
2018-01-21 01:46:31 +0530

    Use link to tagged version for rule docs (#145)

--- rules/utils/get-docs-url.js
+++ rules/utils/get-docs-url.js
@@ -1,9 +1,10 @@
 'use strict';
 const path = require('path');
+const pkg = require('../../package');
 
 const repoUrl = 'https://github.com/sindresorhus/eslint-plugin-unicorn';
 
 module.exports = ruleName => {
 	ruleName = ruleName || path.basename(module.parent.filename, '.js');
-	return `${repoUrl}/blob/master/docs/rules/${ruleName}.md`;
+	return `${repoUrl}/blob/v${pkg.version}/docs/rules/${ruleName}.md`;
 };
--- test/get-docs-url.js
+++ test/get-docs-url.js
@@ -1,12 +1,13 @@
 import test from 'ava';
+import pkg from '../package';
 import getDocsUrl from '../rules/utils/get-docs-url';
 
 test('returns the URL of the a named rule\'s documentation', t => {
-	const url = 'https://github.com/sindresorhus/eslint-plugin-unicorn/blob/master/docs/rules/foo.md';
+	const url = `https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v${pkg.version}/docs/rules/foo.md`;
 	t.is(getDocsUrl('foo'), url);
 });
 
 test('determines the rule name from the file', t => {
-	const url = 'https://github.com/sindresorhus/eslint-plugin-unicorn/blob/master/docs/rules/get-docs-url.md';
+	const url = `https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v${pkg.version}/docs/rules/get-docs-url.md`;
 	t.is(getDocsUrl(), url);
 });
