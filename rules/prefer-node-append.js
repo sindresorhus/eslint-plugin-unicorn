9e9238e5bbb38a27440a6d3189f933194bce7c29
Nikhil Saldanha <nikhil@marax.in>
2018-11-04 01:07:34 +0530

    Simplify rule for prefer-node-append
    Replace property at node level instead of constructing the fix manually.

--- rules/prefer-node-append.js
+++ rules/prefer-node-append.js
@@ -3,24 +3,16 @@ const getDocsUrl = require('./utils/get-docs-url');
 
 const getMethodName = memberExpression => memberExpression.property.name;
 
-const fix = (fixer, sourceCode, callNode, memberExpression) => {
-	const nodeCode = sourceCode.getText(memberExpression.object);
-	const argumentsCode = sourceCode.getText(callNode.arguments[0]);
-	const fixedCodeStatement = `${nodeCode}.append(${argumentsCode})`;
-	return fixer.replaceText(callNode, fixedCodeStatement);
-};
-
 const create = context => {
 	return {
 		CallExpression(node) {
-			const memberExpression = node.callee;
-			const methodName = getMethodName(memberExpression);
+			const {callee} = node;
 
-			if (methodName === 'appendChild') {
+			if (callee.type === 'MemberExpression' && getMethodName(callee) === 'appendChild') {
 				context.report({
 					node,
 					message: 'Prefer `append` over `appendChild`',
-					fix: fixer => fix(fixer, context.getSourceCode(), node, memberExpression)
+					fix: fixer => fixer.replaceText(callee.property, 'append')
 				});
 			}
 		}
