ae3107dcd574a2899f48b4aac7afcaeba10f57c1
dmitriy <d.samovilov@gmail.com>
2019-01-24 22:13:45 +0300

    prefer-includes rule improved, unrelated tests have been cleared

--- rules/prefer-includes.js
+++ rules/prefer-includes.js
@@ -6,19 +6,14 @@ const isIndexOfCallExpression = node => {
 		return false;
 	}
 
-	const property = node.callee.property;
+	const {property} = node.callee;
 
 	return property.name === 'indexOf';
 };
 
-const isUnaryNotExpression = node => (
-	node.type === 'UnaryExpression' && node.operator === '!'
-);
-
 const isNegativeOne = (operator, value) => operator === '-' && value === 1;
 
 const getSourceCode = (context, node) => (
-	// Context.getSourceCode().text.slice(node.range[0], node.range[1])
 	context.getSourceCode().getText(node)
 );
 
@@ -27,30 +22,30 @@ const report = (context, node, target, pattern) => {
 	const patternSource = getSourceCode(context, pattern);
 	context.report({
 		node,
-		message: 'Use .includes(), rather than .indexOf(), when checking for existence.',
-		fix: fixer => fixer.replaceText(node, `${targetSource}.includes(${patternSource})`)
+		message: 'Use `.includes()`, rather than `.indexOf()`, when checking for existence.',
+		fix: fixer => fixer.replaceText(node, `${['!==', '!=', '<'].includes(node.operator) ? '!' : ''}${targetSource}.includes(${patternSource})`)
 	});
 };
 
 const create = context => ({
 	BinaryExpression: node => {
-		const left = node.left;
-		const right = node.right;
+		const {left} = node;
+		const {right} = node;
 
 		if (isIndexOfCallExpression(left)) {
 			const target = left.callee.object;
 			const pattern = left.arguments[0];
 
 			if (right.type === 'UnaryExpression') {
-				const argument = right.argument;
+				const {argument} = right;
 
 				if (argument.type !== 'Literal') {
 					return false;
 				}
 
-				const value = argument.value;
+				const {value} = argument;
 
-				if (['!==', '!=', '>'].indexOf(node.operator) !== -1 && isNegativeOne(right.operator, value)) {
+				if (['!==', '!=', '>', '===', '=='].includes(node.operator) && isNegativeOne(right.operator, value)) {
 					report(context, node, target, pattern);
 				}
 			}
@@ -59,46 +54,10 @@ const create = context => ({
 				return false;
 			}
 
-			if (right.type === 'Literal' && node.operator === '>=' && right.value === 0) {
+			if (right.type === 'Literal' && ['>=', '<'].includes(node.operator) && right.value === 0) {
 				report(context, node, target, pattern);
 			}
-			return;
-		}
-
-		if (isUnaryNotExpression(left)) {
-			const argument = left.argument;
-
-			if (isIndexOfCallExpression(argument)) {
-				const target = argument.callee.object;
-				const pattern = argument.arguments[0];
-
-				if (right.type === 'UnaryExpression') {
-					const argument = right.argument;
-
-					if (argument.type !== 'Literal') {
-						return false;
-					}
-
-					const value = argument.value;
-
-					if (node.operator === '===' && isNegativeOne(right.operator, value)) {
-						report(context, node, target, pattern);
-					}
-					if (node.operator === '==' && isNegativeOne(right.operator, value)) {
-						report(context, node, target, pattern);
-					}
-				}
-
-				if (right.type !== 'Literal') {
-					return false;
-				}
-
-				if (node.operator === '<' && right.value === 0) {
-					report(context, node, target, pattern);
-				}
-
-				return false;
-			}
+			return false;
 		}
 	}
 });
@@ -107,7 +66,7 @@ module.exports = {
 	create,
 	meta: {
 		docs: {
-			url: getDocsUrl()
+			url: getDocsUrl(__filename)
 		},
 		fixable: 'code'
 	}
--- test/prefer-includes.js
+++ test/prefer-includes.js
@@ -10,58 +10,48 @@ const ruleTester = avaRuleTester(test, {
 
 const errors = [{
 	ruleId: 'prefer-includes',
-	message: 'Use .includes(), rather than .indexOf(), when checking for existence.'
+	message: 'Use `.includes()`, rather than `.indexOf()`, when checking for existence.'
 }];
 
 ruleTester.run('prefer-includes', rule, {
 	valid: [
-		`str.indexOf('foo') !== -n`,
-		`str.indexOf('foo') !== 1`,
-		`!str.indexOf('foo') === 1`,
-		`!str.indexOf('foo') === -n`,
-		`str.includes('foo')`,
-		`'foobar'.includes('foo')`,
+		'str.indexOf(\'foo\') !== -n',
+		'str.indexOf(\'foo\') !== 1',
+		'!str.indexOf(\'foo\') === 1',
+		'!str.indexOf(\'foo\') === -n',
+		'str.includes(\'foo\')',
+		'\'foobar\'.includes(\'foo\')',
 		'[1,2,3].includes(4)'
 	],
 	invalid: [
 		{
-			code: `'foobar'.indexOf('foo') !== -1`,
-			output: `'foobar'.includes('foo')`,
+			code: '\'foobar\'.indexOf(\'foo\') !== -1',
+			output: '!\'foobar\'.includes(\'foo\')',
 			errors
 		},
 		{
-			code: `str.indexOf('foo') != -1`,
-			output: `str.includes('foo')`,
+			code: 'str.indexOf(\'foo\') != -1',
+			output: '!str.includes(\'foo\')',
 			errors
 		},
 		{
-			code: `str.indexOf('foo') > -1`,
-			output: `str.includes('foo')`,
+			code: 'str.indexOf(\'foo\') > -1',
+			output: 'str.includes(\'foo\')',
 			errors
 		},
 		{
-			code: `'foobar'.indexOf('foo') >= 0`,
-			output: `'foobar'.includes('foo')`,
-			errors
-		},
-		{
-			code: `!str.indexOf('foo') === -1`,
-			output: `str.includes('foo')`,
-			errors
-		},
-		{
-			code: `!str.indexOf('foo') == -1`,
-			output: `str.includes('foo')`,
-			errors
-		},
-		{
-			code: `!'foobar'.indexOf('foo') < 0`,
-			output: `'foobar'.includes('foo')`,
+			code: '\'foobar\'.indexOf(\'foo\') >= 0',
+			output: '\'foobar\'.includes(\'foo\')',
 			errors
 		},
 		{
 			code: '[1,2,3].indexOf(4) !== -1',
-			output: '[1,2,3].includes(4)',
+			output: '![1,2,3].includes(4)',
+			errors
+		},
+		{
+			code: 'str.indexOf(\'foo\') < 0',
+			output: '!str.includes(\'foo\')',
 			errors
 		}
 	]
