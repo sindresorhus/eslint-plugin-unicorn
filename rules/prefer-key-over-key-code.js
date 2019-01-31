364c6bead1d11c29e2dc85b4f519410f5a6365c3
Ankeet Maini <ankeet.maini@gmail.com>
2019-01-31 20:21:06 +0530

    Improves code and handles destructuring as well.

--- rules/prefer-key-over-key-code.js
+++ rules/prefer-key-over-key-code.js
@@ -1,34 +1,114 @@
 'use strict';
 const getDocsUrl = require('./utils/get-docs-url');
 
+const keys = ['keyCode', 'charCode', 'which'];
+
+const isPropertyNamedAddEventListener = node =>
+	node &&
+	node.callee &&
+	node.callee.property &&
+	node.callee.property.name === 'addEventListener' &&
+	node;
+
+const isInsideEventCallback = ancestors => {
+	return ancestors.reduce((isInside, current) => {
+		return isInside || isPropertyNamedAddEventListener(current);
+	}, null);
+};
+
+const isPropertyOf = (node, objectName) => {
+	return (
+		node &&
+		node.parent &&
+		node.parent.object &&
+		node.parent.object.name === objectName
+	);
+};
+
+const getEventNode = node => {
+	switch (node && node.type) {
+		case 'ArrowFunctionExpression':
+		case 'FunctionExpression': {
+			return node.params && node.params[0] && node.params[0].name;
+		}
+
+		default:
+			return null;
+	}
+};
+
+const getNearestAncestorByType = (nodes, type) => {
+	for (let i = nodes.length - 1; i >= 0; i--) {
+		if (nodes[i].type === type) {
+			return nodes[i];
+		}
+	}
+};
+
+const extraCitation = 'See https://goo.gl/cRK532 for more info.';
+
+const util = context => {
+	const report = reportError(context);
+	const ancestors = context.getAncestors();
+	const callExpressionNode = isInsideEventCallback(
+		ancestors.filter(n => n.type === 'CallExpression')
+	);
+	const event = getEventNode(
+		callExpressionNode && callExpressionNode.arguments[1]
+	);
+	return {
+		report,
+		event,
+		ancestors
+	};
+};
+
+const directAccessRule = context => node => {
+	const {report, event} = util(context);
+	// Normal case when usage is direct -> event.keyCode
+
+	const isPropertyOfEvent = isPropertyOf(node, event);
+	if (isPropertyOfEvent) {
+		report(node);
+	}
+};
+
+const destructuredPropertyRule = context => node => {
+	const propertyName = node.value && node.value.name;
+	if (!keys.includes(propertyName)) {
+		return;
+	}
+
+	const {report, event, ancestors} = util(context);
+
+	// Destructured case
+	const nearestVariableDeclarator = getNearestAncestorByType(
+		ancestors,
+		'VariableDeclarator'
+	);
+	const isDestructuredFromEvent =
+		nearestVariableDeclarator &&
+		nearestVariableDeclarator.init &&
+		nearestVariableDeclarator.init.name === event;
+
+	if (isDestructuredFromEvent) {
+		report(node.value);
+	}
+};
+
+const reportError = context => node => {
+	context.report({
+		message: `Use key instead of ${node.name}. ${extraCitation}`,
+		node
+	});
+};
+
 const create = context => {
 	return {
-		'MemberExpression Identifier[name=addEventListener]': node => {
-			const callExp = node.parent.parent;
-			const callback = callExp.arguments[1];
-			const {body} = callback;
-			const eventParam = callback.params[0];
-			const sourceCode = context.getSourceCode();
-			const tokens = sourceCode.getTokens(body);
-			const problematicTokens = tokens.filter(
-				t =>
-					t.type === 'Identifier' &&
-					['keyCode', 'charCode', 'which'].indexOf(t.value) > -1
-			);
-			problematicTokens.forEach(problematicToken => {
-				const eventToken = sourceCode.getTokenBefore(problematicToken, {
-					skip: 1
-				});
-				if (eventToken.value === eventParam.name) {
-					context.report({
-						message: `Use key instead of ${
-							problematicToken.value
-						}. See https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key`,
-						node: problematicToken
-					});
-				}
-			});
-		}
+		'Identifier:matches([name=keyCode], [name=charCode], [name=which])': directAccessRule(
+			context
+		),
+		Property: destructuredPropertyRule(context)
 	};
 };
 
--- test/prefer-key-over-key-code.js
+++ test/prefer-key-over-key-code.js
@@ -10,17 +10,47 @@ const ruleTester = avaRuleTester(test, {
 
 const error = key => ({
 	ruleId: 'prefer-key-over-key-code',
-	message: `Use key instead of ${key}. See https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key`
+	message: `Use key instead of ${key}. See https://goo.gl/cRK532 for more info.`
 });
 
-ruleTester.run('no-hex-escape', rule, {
+ruleTester.run('prefer-key-over-key-code', rule, {
 	valid: [
 		`window.addEventListener('click', e => {
 			console.log(e.key);
 		})`,
+		'const { keyCode } = e',
+		'const { charCode } = e',
+		'const {a, b, c} = event',
+		'const keyCode = () => 4',
+		'const which = keyCode => 5',
+		'function which(abc) { const {keyCode} = abc; return keyCode}',
+		'const { which } = e',
+		'const { keyCode: key } = e',
+		'const { keyCode: abc } = e',
 		`foo.addEventListener('keydown', e => {
 			if (e.key === 'ArrowLeft') return true;
-		})`
+		})`,
+		`a.addEventListener('keyup', function (event) {
+			const key = event.key;
+		})`,
+		`a.addEventListener('keyup', function (event) {
+			const { key } = event;
+		})`,
+		`foo.addEventListener('click', e => {
+			const good = {};
+			good.keyCode = '34';
+		});`,
+		`foo.addEventListener('click', e => {
+			const good = {};
+			good.charCode = '34';
+		});`,
+		`foo.addEventListener('click', e => {
+			const good = {};
+			good.which = '34';
+		});`,
+		`foo.addEventListener('click', e => {
+			const {keyCode: a, charCode: b, charCode: c} = e;
+		});`
 	],
 	invalid: [
 		{
@@ -32,12 +62,65 @@ ruleTester.run('no-hex-escape', rule, {
 			errors: [error('keyCode')]
 		},
 		{
-			code: `
-				window.addEventListener('click', e => {
-					console.log(e.which);
-				})
+			code: `				
+				foo.addEventListener('click', event => {
+					if (event.keyCode === 27) {
+					}
+				});
 			`,
-			errors: [error('which')]
+			errors: [error('keyCode')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', a => {
+					if (a.keyCode === 27) {
+					}
+				});
+			`,
+			errors: [error('keyCode')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', (a, b, c) => {
+					if (a.keyCode === 27) {
+					}
+				});
+			`,
+			errors: [error('keyCode')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', function(a, b, c) {
+					if (a.keyCode === 27) {
+					}
+				});
+			`,
+			errors: [error('keyCode')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', function(b) {
+					if (b.keyCode === 27) {
+					}
+				});
+			`,
+			errors: [error('keyCode')]
+		},
+		{
+			code: `
+				foo.addEventListener('click', e => {
+					const {keyCode, a, b} = e;
+				});
+			`,
+			errors: [error('keyCode')]
+		},
+		{
+			code: `
+				foo.addEventListener('click', e => {
+					const {a: keyCode, a, b} = e;
+				});
+			`,
+			errors: [error('keyCode')]
 		},
 		{
 			code: `
@@ -46,6 +129,136 @@ ruleTester.run('no-hex-escape', rule, {
 				})
 			`,
 			errors: [error('charCode')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', event => {
+					if (event.charCode === 27) {
+					}
+				});
+			`,
+			errors: [error('charCode')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', a => {
+					if (a.charCode === 27) {
+					}
+				});
+			`,
+			errors: [error('charCode')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', (a, b, c) => {
+					if (a.charCode === 27) {
+					}
+				});
+			`,
+			errors: [error('charCode')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', function(a, b, c) {
+					if (a.charCode === 27) {
+					}
+				});
+			`,
+			errors: [error('charCode')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', function(b) {
+					if (b.charCode === 27) {
+					}
+				});
+			`,
+			errors: [error('charCode')]
+		},
+		{
+			code: `
+				foo.addEventListener('click', e => {
+					const {charCode, a, b} = e;
+				});
+			`,
+			errors: [error('charCode')]
+		},
+		{
+			code: `
+				foo.addEventListener('click', e => {
+					const {a: charCode, a, b} = e;
+				});
+			`,
+			errors: [error('charCode')]
+		},
+		{
+			code: `
+				window.addEventListener('click', e => {
+					console.log(e.which);
+				})
+			`,
+			errors: [error('which')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', event => {
+					if (event.which === 27) {
+					}
+				});
+			`,
+			errors: [error('which')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', a => {
+					if (a.which === 27) {
+					}
+				});
+			`,
+			errors: [error('which')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', (a, b, c) => {
+					if (a.which === 27) {
+					}
+				});
+			`,
+			errors: [error('which')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', function(a, b, c) {
+					if (a.which === 27) {
+					}
+				});
+			`,
+			errors: [error('which')]
+		},
+		{
+			code: `				
+				foo.addEventListener('click', function(b) {
+					if (b.which === 27) {
+					}
+				});
+			`,
+			errors: [error('which')]
+		},
+		{
+			code: `
+				foo.addEventListener('click', e => {
+					const {which, a, b} = e;
+				});
+			`,
+			errors: [error('which')]
+		},
+		{
+			code: `
+				foo.addEventListener('click', e => {
+					const {a: which, a, b} = e;
+				});
+			`,
+			errors: [error('which')]
 		}
 	]
 });
