77540ad33d149c3e622dafbc67d20a1495811bde
Yanis Benson <yanis.benson@gmail.com>
2019-02-07 13:24:04 +0300

    initial commit

---
+++ rules/prefer-better-name.js
@@ -0,0 +1,162 @@
+'use strict';
+const getDocsUrl = require('./utils/get-docs-url');
+
+const defaultRules = {
+	err: 'error',
+	cb: 'callback',
+	opts: 'options',
+	str: 'string',
+	obj: 'object',
+	num: 'number',
+	val: 'value',
+	e: 'event|error',
+	el: 'element',
+	req: 'request',
+	res: 'response|result',
+	btn: 'button',
+	msg: 'message',
+	len: 'length',
+	env: 'environment',
+	dev: 'development',
+	prod: 'production',
+	tmp: 'temporary',
+	arg: 'argument',
+	tbl: 'table',
+	db: 'database',
+	ctx: 'context',
+	mod: 'module'
+};
+
+const extendedRules = {
+	elem: 'element',
+	arr: 'array',
+	btn: 'button',
+	msg: 'message',
+	len: 'length',
+	tmp: 'temporary',
+	ans: 'answer',
+	arg: 'argument',
+	rec: 'record',
+	attrs: 'attributes',
+	ns: 'namespace',
+	prop: 'property',
+	ref: 'reference',
+	cmd: 'command',
+	k: 'key',
+	v: 'value',
+	idx: 'index'
+};
+
+const rulesets = {
+	default: [defaultRules],
+	extended: [defaultRules, extendedRules]
+};
+
+const compileReplaceMap = (rulesetName, additionalRules) => {
+	let ruleset;
+	if (rulesetName === undefined) {
+		rulesetName = 'default';
+	}
+
+	if (rulesetName === null) {
+		ruleset = [];
+	} else {
+		ruleset = rulesets[rulesetName].slice(0);
+	}
+
+	if (additionalRules !== null && additionalRules !== undefined) {
+		ruleset.push(additionalRules);
+	}
+
+	const replaceMap = {};
+	ruleset.forEach(rules => {
+		Object.keys(rules).forEach(fromRule => {
+			let toRule = rules[fromRule];
+
+			if (toRule.includes('|')) {
+				toRule = toRule.split('|').map(word => word.trim());
+			}
+
+			fromRule.split('|').forEach(word => {
+				replaceMap[word.trim()] = toRule;
+			});
+		});
+	});
+	return replaceMap;
+};
+
+const findFreeName = (scope, baseName) => {
+	const variables = scope.variableScope.set;
+	let name = baseName;
+	let index = 0;
+	while (variables.has(name)) {
+		index++;
+		name = baseName + index;
+	}
+
+	return name;
+};
+
+const create = context => {
+	const replaceMap = compileReplaceMap(context.options[0], context.options[1]);
+
+	function checkId(node, replaceId, getReferences) {
+		const {name} = node;
+		let betterName = replaceMap[name];
+		if (betterName !== undefined && betterName !== null) {
+			if (Array.isArray(betterName)) {
+				context.report({
+					node,
+					message: `Name \`${name}\` is ambiguous, is it ${betterName.map(word => `\`${word}\``).join(' or ')} or something else`
+				});
+			} else {
+				betterName = findFreeName(context.getScope(), betterName);
+				context.report({
+					node,
+					fix: fixer => {
+						const fixes = [];
+
+						if (replaceId) {
+							fixes.push(fixer.replaceText(node, betterName));
+						}
+
+						getReferences().forEach(reference => {
+							fixes.push(fixer.replaceText(reference.identifier, betterName));
+						});
+						return fixes;
+					},
+					message: `Prefer \`${betterName}\` over \`${name}\``
+				});
+			}
+		}
+	}
+
+	return {
+		FunctionDeclaration: node => {
+			context.getDeclaredVariables(node).forEach(variable => {
+				checkId(variable.defs[0].name, true, () => variable.references);
+			});
+		},
+		VariableDeclarator: node => {
+			checkId(node.id, false, () => {
+				return context.getDeclaredVariables(node)[0].references;
+			});
+		},
+		CatchClause: node => {
+			checkId(node.param, true, () => {
+				return context.getDeclaredVariables(node)[0].references;
+			});
+		}
+	};
+};
+
+module.exports = {
+	create,
+	meta: {
+		type: 'suggestion',
+		docs: {
+			url: getDocsUrl(__filename)
+		},
+		fixable: 'code'
+	}
+};
---
+++ test/prefer-better-name.js
@@ -0,0 +1,65 @@
+import test from 'ava';
+import avaRuleTester from 'eslint-ava-rule-tester';
+import rule from '../rules/prefer-better-name';
+
+const ruleTester = avaRuleTester(test, {
+	env: {
+		es6: true
+	},
+	parserOptions: {
+		sourceType: 'module'
+	}
+});
+
+const invalidAmbiguousTestCase = (code, name, betterNames) => {
+	return {
+		code,
+		output: code,
+		errors: [{message: `Name \`${name}\` is ambiguous, is it ${betterNames.map(word => `\`${word}\``).join(' or ')} or something else`}]
+	};
+};
+
+const makeReplaceError = (name, betterName) => {
+	return {message: `Prefer \`${betterName}\` over \`${name}\``};
+};
+
+const invalidReplaceTestCase = (code, output, name, betterName) => {
+	return {
+		code,
+		output,
+		errors: [makeReplaceError(name, betterName)]
+	};
+};
+
+ruleTester.run('prefer-better-name', rule, {
+	valid: [
+		'let error'
+	],
+	invalid: [
+		invalidReplaceTestCase(
+			'let str = "abc"; console.log(str);',
+			'let string = "abc"; console.log(string);',
+			'str',
+			'string'),
+		invalidReplaceTestCase(
+			'try {} catch(err) { throw err; }',
+			'try {} catch(error) { throw error; }',
+			'err',
+			'error'),
+		invalidReplaceTestCase(
+			`function test(err){
+				const error = null;
+				console.log(err);
+			}`,
+			`function test(error1){
+				const error = null;
+				console.log(error1);
+			}`,
+			'err',
+			'error1'),
+		invalidAmbiguousTestCase(
+			'let e;',
+			'e',
+			['event', 'error'])
+	]
+});
