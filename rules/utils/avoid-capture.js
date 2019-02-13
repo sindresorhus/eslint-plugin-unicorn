14798e5d83c845f819098a06f490fdff2042b0cb
futpib <futpib@gmail.com>
2019-02-13 21:39:11 +0300

    Document the new utility functions

--- rules/utils/avoid-capture.js
+++ rules/utils/avoid-capture.js
@@ -5,6 +5,15 @@ const indexifyName = (name, index) => index === 1 ? name : name + index;
 
 const someScopeHasVariableName = (name, scopes) => scopes.some(scope => resolveVariableName(name, scope));
 
+/**
+ * Generates a unique name prefixed with `name` such that it is not defined in any of the `scopes`.
+ * Useful when you want to rename a variable (or create a new variable)
+ * while being sure not to shadow any other variables in the code.
+ *
+ * @param {string} name - The desired name for a new variable
+ * @param {Scope[]} scopes - The list of scopes the new variable will be referenced in
+ * @returns {string} - Either `name` as is, or a string like `${name}1` suffixed with a number to make the name unique
+ */
 module.exports = (name, scopes) => {
 	let index = 1;
 	while (someScopeHasVariableName(indexifyName(name, index), scopes)) {
