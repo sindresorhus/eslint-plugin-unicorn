bb608e0b1e763d8b9dda779278266ef1f63a6d22
Ankeet Maini <ankeet.maini@gmail.com>
2019-02-16 00:03:00 +0530

    Uses String.fromCharCode instead of keeping a map.

--- rules/prefer-key-over-key-code.js
+++ rules/prefer-key-over-key-code.js
@@ -2,13 +2,17 @@
 const getDocsUrl = require('./utils/get-docs-url');
 
 const keys = ['keyCode', 'charCode', 'which'];
-const keyCodeToKey = {
+// https://github.com/facebook/react/blob/b87aabd/packages/react-dom/src/events/getEventKey.js#L36
+// only meta characters which can't be deciphered from String.fromCharCode
+const translateToKey = {
 	8: 'Backspace',
 	9: 'Tab',
+	12: 'Clear',
 	13: 'Enter',
 	16: 'Shift',
 	17: 'Control',
 	18: 'Alt',
+	19: 'Pause',
 	20: 'CapsLock',
 	27: 'Escape',
 	32: ' ',
@@ -18,45 +22,25 @@ const keyCodeToKey = {
 	36: 'Home',
 	37: 'ArrowLeft',
 	38: 'ArrowUp',
-	39: 'ArrowDown',
+	39: 'ArrowRight',
 	40: 'ArrowDown',
+	45: 'Insert',
 	46: 'Delete',
-	48: 0,
-	49: 1,
-	50: 2,
-	51: 3,
-	52: 4,
-	53: 5,
-	54: 6,
-	55: 7,
-	56: 8,
-	57: 9,
-	65: 'a',
-	66: 'b',
-	67: 'c',
-	68: 'd',
-	69: 'e',
-	70: 'f',
-	71: 'g',
-	72: 'h',
-	73: 'i',
-	74: 'j',
-	75: 'k',
-	76: 'l',
-	77: 'm',
-	78: 'n',
-	79: '0',
-	80: 'p',
-	81: 'q',
-	82: 'r',
-	83: 's',
-	84: 't',
-	85: 'u',
-	86: 'v',
-	87: 'w',
-	88: 'x',
-	89: 'y',
-	90: 'z'
+	112: 'F1',
+	113: 'F2',
+	114: 'F3',
+	115: 'F4',
+	116: 'F5',
+	117: 'F6',
+	118: 'F7',
+	119: 'F8',
+	120: 'F9',
+	121: 'F10',
+	122: 'F11',
+	123: 'F12',
+	144: 'NumLock',
+	145: 'ScrollLock',
+	224: 'Meta'
 };
 
 const isPropertyNamedAddEventListener = node =>
@@ -161,15 +145,18 @@ const fix = (context, node) => fixer => {
 	const {right} = nearestIf.test;
 	const isRightValid =
 		right.type === 'Literal' && typeof right.value === 'number';
+
+	// Either a meta key or a printable character
+	const keyCode = translateToKey[right.value] || String.fromCharCode(right.value);
 	// And if we recognize the keyCode
-	if (!isRightValid || !keyCodeToKey[right.value]) {
+	if (!isRightValid || !keyCode) {
 		return;
 	}
 
 	// Apply fixes
 	return [
 		fixer.replaceText(node, 'key'),
-		fixer.replaceText(right, `'${keyCodeToKey[right.value]}'`)
+		fixer.replaceText(right, `'${keyCode}'`)
 	];
 };
 
