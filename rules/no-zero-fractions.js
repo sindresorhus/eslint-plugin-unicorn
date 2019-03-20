51498f0ab3d7264ac993ff80b2cd8c36165bbdd1
Yanis Benson <yanis.benson@gmail.com>
2019-03-21 02:35:52 +0300

    linter

--- rules/no-zero-fractions.js
+++ rules/no-zero-fractions.js
@@ -9,23 +9,24 @@ const MSG_DANGLING_DOT = 'Dangling dot in number.';
 // 2 - dangling dot or dot with zeroes
 // 3 - dot with digits except last zeroes
 // 4 - scientific notation
-const RE_DANGLINGDOT_OR_ZERO_FRACTIONS = /^([+-]?[0-9]*)(?:(\.0*)|(\.[0-9]*[1-9])0+)(e[+-]?[0-9]+)?$/;
+const RE_DANGLINGDOT_OR_ZERO_FRACTIONS = /^([+-]?\d*)(?:(\.0*)|(\.\d*[1-9])0+)(e[+-]?\d+)?$/;
 
 const create = context => {
 	return {
 		Literal: node => {
-			if (typeof node.value === 'number'){
+			if (typeof node.value === 'number') {
 				const m = RE_DANGLINGDOT_OR_ZERO_FRACTIONS.exec(node.raw);
-				if(m !== null){
+				if (m !== null) {
 					const isDanglingDot = m[2] === '.';
 					context.report({
 						node,
 						message: isDanglingDot ? MSG_DANGLING_DOT : MSG_ZERO_FRACTION,
 						fix: fixer => {
-							let wantedStr = (m[2] === undefined) ? m[1]+m[3] : m[1];
-							if(m[4] !== undefined){
-								wantedStr+= m[4];
+							let wantedStr = (m[2] === undefined) ? m[1] + m[3] : m[1];
+							if (m[4] !== undefined) {
+								wantedStr += m[4];
 							}
+
 							return fixer.replaceText(node, wantedStr);
 						}
 					});
