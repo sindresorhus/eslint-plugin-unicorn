73e8d5d274e519c100ce86e70f4d391d94a06b5a
Sam Verschueren <sam.verschueren@gmail.com>
2019-03-20 19:21:42 +0100

    Add blacklist to `no-fn-reference-in-iterator` rule to prevent false-positives (#172)

--- rules/no-fn-reference-in-iterator.js
+++ rules/no-fn-reference-in-iterator.js
@@ -13,12 +13,20 @@ const iteratorMethods = new Map([
 	['reduceRight', 2]
 ]);
 
-const whitelist = new Set([
+const functionWhitelist = new Set([
 	'Boolean'
 ]);
 
+const calleeBlacklist = [
+	'Promise',
+	'React.children',
+	'_',
+	'Async',
+	'async'
+];
+
 const isIteratorMethod = node => node.callee.property && iteratorMethods.has(node.callee.property.name);
-const hasFunctionArgument = node => node.arguments.length > 0 && (node.arguments[0].type === 'Identifier' || node.arguments[0].type === 'CallExpression') && !whitelist.has(node.arguments[0].name);
+const hasFunctionArgument = node => node.arguments.length > 0 && (node.arguments[0].type === 'Identifier' || node.arguments[0].type === 'CallExpression') && !functionWhitelist.has(node.arguments[0].name);
 
 const getNumberOfArguments = node => node.callee.property && iteratorMethods.get(node.callee.property.name);
 const parseArgument = (context, arg) => arg.type === 'Identifier' ? arg.name : context.getSourceCode().getText(arg);
@@ -31,8 +39,16 @@ const fix = (context, node) => {
 	return fixer => fixer.replaceText(arg, `${numberOfArgs === 1 ? argString : `(${argString})`} => ${parseArgument(context, arg)}(${argString})`);
 };
 
+const toSelector = name => {
+	const splitted = name.split('.');
+	return `[callee.${'object.'.repeat(splitted.length)}name!="${splitted.shift()}"]`;
+};
+
+// Select all the call expressions except the ones present in the blacklist
+const selector = `CallExpression${calleeBlacklist.map(toSelector).join('')}`;
+
 const create = context => ({
-	'CallExpression[callee.object.name!="Promise"]': node => {
+	[selector]: node => {
 		if (isIteratorMethod(node) && hasFunctionArgument(node)) {
 			const [arg] = node.arguments;
 
