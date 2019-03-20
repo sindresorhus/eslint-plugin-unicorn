bc751742b20ce05703d21dfdd9c9c08481bd8352
Yanis Benson <yanis.benson@gmail.com>
2019-03-21 02:24:45 +0300

    implemented separate error for dangling dot, handles all trailing zeroes, handles scientific notations

--- rules/no-zero-fractions.js
+++ rules/no-zero-fractions.js
@@ -1,18 +1,35 @@
 'use strict';
 const getDocsUrl = require('./utils/get-docs-url');
 
-const message = 'Zero fraction or dangling dot in number.';
+const MSG_ZERO_FRACTION = 'Zero fraction in number.';
+const MSG_DANGLING_DOT = 'Dangling dot in number.';
 
-const regexpEndsWithZeroFraction = /\.0*$/;
+// Groups:
+// 1 - integer part
+// 2 - dangling dot or dot with zeroes
+// 3 - dot with digits except last zeroes
+// 4 - scientific notation
+const RE_DANGLINGDOT_OR_ZERO_FRACTIONS = /^([+-]?[0-9]*)(?:(\.0*)|(\.[0-9]*[1-9])0+)(e[+-]?[0-9]+)?$/;
+
 const create = context => {
 	return {
 		Literal: node => {
-			if (typeof node.value === 'number' && node.value % 1 === 0 && regexpEndsWithZeroFraction.test(node.raw)) {
-				context.report({
-					node,
-					message,
-					fix: fixer => fixer.replaceText(node, `${node.value}`)
-				});
+			if (typeof node.value === 'number'){
+				const m = RE_DANGLINGDOT_OR_ZERO_FRACTIONS.exec(node.raw);
+				if(m !== null){
+					const isDanglingDot = m[2] === '.';
+					context.report({
+						node,
+						message: isDanglingDot ? MSG_DANGLING_DOT : MSG_ZERO_FRACTION,
+						fix: fixer => {
+							let wantedStr = (m[2] === undefined) ? m[1]+m[3] : m[1];
+							if(m[4] !== undefined){
+								wantedStr+= m[4];
+							}
+							return fixer.replaceText(node, wantedStr);
+						}
+					});
+				}
 			}
 		}
 	};
