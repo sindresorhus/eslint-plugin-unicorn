9754238a3b26d9a4e4c84b4f35593b6163c810dd
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-23 17:03:14 +0700

    Update no-zero-fractions.js

--- rules/no-zero-fractions.js
+++ rules/no-zero-fractions.js
@@ -5,11 +5,11 @@ const MESSAGE_ZERO_FRACTION = 'Don\'t use a zero fraction in the number.';
 const MESSAGE_DANGLING_DOT = 'Don\'t use a dangling dot in the number.';
 
 // Groups:
-// 1 - integer part
-// 2 - dangling dot or dot with zeroes
-// 3 - dot with digits except last zeroes
-// 4 - scientific notation
-const RE_DANGLINGDOT_OR_ZERO_FRACTIONS = /^([+-]?\d*)(?:(\.0*)|(\.\d*[1-9])0+)(e[+-]?\d+)?$/; // TODO Possibly use named capture groups when targeting Node.js 10
+// 1. Integer part
+// 2. Dangling dot or dot with zeroes
+// 3. Dot with digits except last zeroes
+// 4. Scientific notation
+const RE_DANGLINGDOT_OR_ZERO_FRACTIONS = /^([+-]?\d*)(?:(\.0*)|(\.\d*[1-9])0+)(e[+-]?\d+)?$/; // TODO: Possibly use named capture groups when targeting Node.js 10
 
 const create = context => {
 	return {
