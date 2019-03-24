16e100e153ddfa92910650d9cb3c3de3ef078065
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-03-24 04:49:06 +0100

    regex-shorthand: undo to support NewExpression

--- rules/regex-shorthand.js
+++ rules/regex-shorthand.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const cleanRegexp = require('clean-regexp');
+
 const {parse, generate, optimize} = require('regexp-tree');
 
 const getDocsUrl = require('./utils/get-docs-url');
@@ -14,23 +16,60 @@ const create = context => {
 	];
 
 	return {
-		'Literal[regex]': node => {
-			const {type, value} = sourceCode.getFirstToken(node);
+		'NewExpression[callee.name="RegExp"]': node => {
+			const args = node.arguments;
 
-			if (type !== 'RegularExpression') {
+			if (args.length === 0 || args[0].type !== 'Literal') {
 				return;
 			}
+
+			const hasRegExp = args[0].regex;
+
+			let oldPattern = null;
+			let flags = null;
+
+			if (hasRegExp) {
+				oldPattern = args[0].regex.pattern;
+				flags = args[1] && args[1].type === 'Literal' ? args[1].value : args[0].regex.flags;
+				return;
+			} else {
+				oldPattern = args[0].value;
+				flags = args[1] && args[1].type === 'Literal' ? args[1].value : '';
+			}
+
+			const newPattern = cleanRegexp(oldPattern, flags);
+
+			if (oldPattern !== newPattern) {
+				let fixed;
+				if (hasRegExp) {
+					fixed = `/${newPattern}/`;
+				} else {
+					// Escape backslash and apostrophe because we wrap the result in single quotes.
+					fixed = (newPattern || '').replace(/\\/, '\\\\');
+					fixed = fixed.replace(/'/, '\'');
+					fixed = `'${fixed}'`;
+				}
+
+				context.report({
+					node,
+					message,
+					fix: fixer => fixer.replaceTextRange(args[0].range, fixed)
+				});
+			}
+		},
+		'Literal[regex]': node => {
+			const {type, value} = sourceCode.getFirstToken(node);
 
 			let parsedSource;
 			try {
 				parsedSource = parse(value);
-			} catch (error) {
+			} catch ({message}) {
 				context.report({
 					node,
 					message: '{{original}} can\'t be parsed: {{message}}',
 					data: {
 						original: value,
-						message: error.message
+						message
 					}
 				});
 
