72262fbadc650412bf6b1aad4e761e06e2f828e1
Yanis Benson <yanis.benson@gmail.com>
2019-03-24 14:07:52 +0300

    Alphabetically sort the rules and enforce that (#260)

--- test/package.js
+++ test/package.js
@@ -11,7 +11,17 @@ test.before(async () => {
 	ruleFiles = files.filter(file => path.extname(file) === '.js');
 });
 
-test('Every rule is defined in index file', t => {
+const testSorted = (t, actualOrder, sourceName) => {
+	const sortedOrder = actualOrder.slice(0).sort();
+
+	for (const [wantedIndex, name] of sortedOrder.entries()) {
+		const actualIndex = actualOrder.indexOf(name);
+		const whereMessage = (wantedIndex === 0) ? '' : `, after '${sortedOrder[wantedIndex - 1]}'`;
+		t.is(actualIndex, wantedIndex, `${sourceName} should be alphabetically sorted, '${name}' should be placed at index ${wantedIndex}${whereMessage}`);
+	}
+};
+
+test('Every rule is defined in index file in alphabetical order', t => {
 	for (const file of ruleFiles) {
 		const name = path.basename(file, '.js');
 		t.truthy(index.rules[name], `'${name}' is not exported in 'index.js'`);
@@ -24,6 +34,47 @@ test('Every rule is defined in index file', t => {
 		'There are more exported rules than rule files.');
 	t.is(Object.keys(index.configs.recommended.rules).length, ruleFiles.length,
 		'There are more exported rules in the recommended config than rule files.');
+
+	testSorted(t, Object.keys(index.configs.recommended.rules), 'configs.recommended.rules');
+});
+
+test('Every rule is defined in readme.md usage and list of rules in alphabetical order', async t => {
+	const readme = await pify(fs.readFile)('readme.md', 'utf8');
+	let usageRules;
+	try {
+		const usageRulesMatch = /## Usage.*?"rules": (\{.*?\})/ms.exec(readme);
+		t.truthy(usageRulesMatch, 'List of rules should be defined in readme.md ## Usage');
+		usageRules = JSON.parse(usageRulesMatch[1]);
+	} catch (_) {}
+
+	t.truthy(usageRules, 'List of rules should be defined in readme.md ## Usage and be valid JSON');
+
+	const rulesMatch = /## Rules(.*?)## Recommended config/ms.exec(readme);
+	t.truthy(rulesMatch, 'List of rules should be defined in readme.md in ## Rules before ## Recommended config');
+	const rulesText = rulesMatch[1];
+	const re = /- \[(.*?)\]\((.*?)\) - (.*)\n/gm;
+	const rules = [];
+	let match;
+	do {
+		match = re.exec(rulesText);
+		if (match) {
+			t.is(match[2], `docs/rules/${match[1]}.md`, `${match[1]} link to docs should be correct`);
+			t.true(match[3].trim().length > 0, `${match[1]} should have description in readme.md ## Rules`);
+			rules.push(match[1]);
+		}
+	} while (match);
+
+	for (const file of ruleFiles) {
+		const name = path.basename(file, '.js');
+		t.truthy(usageRules[`unicorn/${name}`], `'${name}' is not described in the readme.md ## Usage`);
+		t.truthy(rules.includes(name), `'${name}' is not described in the readme.md ## Rules`);
+	}
+
+	t.is(Object.keys(usageRules).length, ruleFiles.length, 'There are more rules in readme.md ## Usage than rule files.');
+	t.is(Object.keys(rules).length, ruleFiles.length, 'There are more rules in readme.md ## Rules than rule files.');
+
+	testSorted(t, Object.keys(usageRules), 'readme.md ## Usage rules');
+	testSorted(t, rules, 'readme.md ## Rules');
 });
 
 test('Every rule has valid meta.type', t => {
