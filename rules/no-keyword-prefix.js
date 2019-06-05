0e28f5cc9d83dd27c94c31d48253cfa6e5ff5c3c
Adam Babcock <github@mrhen.com>
2019-06-04 22:23:00 -0500

    Cleanup

--- rules/no-keyword-prefix.js
+++ rules/no-keyword-prefix.js
@@ -21,6 +21,84 @@ const prepareOptions = ({
 	};
 };
 
+function findKeywordPrefix(name, options) {
+	return options.blacklist.find(keyword => {
+		const suffix = options.onlyCamelCase ? '[A-Z]' : '';
+		const regex = new RegExp(`^${keyword}${suffix}`);
+		return name.match(regex);
+	});
+}
+
+function isInsideObjectPattern(node) {
+	let current = node;
+
+	while (current) {
+		const {parent} = current;
+
+		if (parent && parent.type === 'Property' && parent.computed && parent.key === current) {
+			return false;
+		}
+
+		if (current.type === 'ObjectPattern') {
+			return true;
+		}
+
+		current = parent;
+	}
+
+	return false;
+}
+
+function checkMemberExpression(report, node, options) {
+	const {name} = node;
+	const keyword = findKeywordPrefix(name, options);
+	const effectiveParent = (node.parent.type === 'MemberExpression') ? node.parent.parent : node.parent;
+
+	if (!options.checkProperties) {
+		return;
+	}
+
+	if (node.parent.object.type === 'Identifier' && node.parent.object.name === node.name && Boolean(keyword)) {
+		report(node, keyword);
+	} else if (
+		effectiveParent.type === 'AssignmentExpression' &&
+		Boolean(keyword) &&
+		(effectiveParent.right.type !== 'MemberExpression' || effectiveParent.left.type === 'MemberExpression') &&
+		effectiveParent.left.property.name === node.name
+	) {
+		report(node, keyword);
+	}
+}
+
+function checkObjectPattern(report, node, options) {
+	const {name} = node;
+	const keyword = findKeywordPrefix(name, options);
+
+	if (node.parent.shorthand && node.parent.value.left && Boolean(keyword)) {
+		report(node, keyword);
+	}
+
+	const assignmentKeyEqualsValue = node.parent.key.name === node.parent.value.name;
+
+	if (Boolean(keyword) && node.parent.computed) {
+		report(node, keyword);
+	}
+
+	// Prevent checking righthand side of destructured object
+	if (node.parent.key === node && node.parent.value !== node) {
+		return true;
+	}
+
+	const valueIsInvalid = node.parent.value.name && Boolean(keyword);
+
+	// Ignore destructuring if the option is set, unless a new identifier is created
+	if (valueIsInvalid && !(assignmentKeyEqualsValue && options.ignoreDestructuring)) {
+		report(node, keyword);
+	}
+
+	return false;
+}
+
 // Core logic copied from:
 // https://github.com/eslint/eslint/blob/master/lib/rules/camelcase.js
 const create = context => {
@@ -29,34 +107,6 @@ const create = context => {
 	// Contains reported nodes to avoid reporting twice on destructuring with shorthand notation
 	const reported = [];
 	const ALLOWED_PARENT_TYPES = new Set(['CallExpression', 'NewExpression']);
-
-	function findKeywordPrefix(name) {
-		return options.blacklist.find(keyword => {
-			const suffix = options.onlyCamelCase ? '[A-Z]' : '';
-			const regex = new RegExp(`^${keyword}${suffix}`);
-			return name.match(regex);
-		});
-	}
-
-	function isInsideObjectPattern(node) {
-		let current = node;
-
-		while (current) {
-			const {parent} = current;
-
-			if (parent && parent.type === 'Property' && parent.computed && parent.key === current) {
-				return false;
-			}
-
-			if (current.type === 'ObjectPattern') {
-				return true;
-			}
-
-			current = parent;
-		}
-
-		return false;
-	}
 
 	function report(node, keyword) {
 		if (reported.indexOf(node) < 0) {
@@ -73,43 +123,18 @@ const create = context => {
 	}
 
 	return {
-		Identifier(node) {
+		Identifier: node => {
 			const {name} = node;
-			const keyword = findKeywordPrefix(name);
+			const keyword = findKeywordPrefix(name, options);
 			const effectiveParent = (node.parent.type === 'MemberExpression') ? node.parent.parent : node.parent;
 
 			if (node.parent.type === 'MemberExpression') {
-				if (!options.checkProperties) {
-					return;
-				}
-
-				if (node.parent.object.type === 'Identifier' && node.parent.object.name === node.name && Boolean(keyword)) {
-					report(node, keyword);
-				} else if (effectiveParent.type === 'AssignmentExpression' && Boolean(keyword) && (effectiveParent.right.type !== 'MemberExpression' || effectiveParent.left.type === 'MemberExpression' && effectiveParent.left.property.name === node.name)) {
-					report(node, keyword);
-				}
+				checkMemberExpression(report, node, options);
 			} else if (node.parent.type === 'Property' || node.parent.type === 'AssignmentPattern') {
 				if (node.parent.parent && node.parent.parent.type === 'ObjectPattern') {
-					if (node.parent.shorthand && node.parent.value.left && Boolean(keyword)) {
-						report(node, keyword);
-					}
-
-					const assignmentKeyEqualsValue = node.parent.key.name === node.parent.value.name;
-
-					if (Boolean(keyword) && node.parent.computed) {
-						report(node, keyword);
-					}
-
-					// Prevent checking righthand side of destructured object
-					if (node.parent.key === node && node.parent.value !== node) {
+					const finished = checkObjectPattern(report, node, options);
+					if (finished) {
 						return;
-					}
-
-					const valueIsInvalid = node.parent.value.name && Boolean(keyword);
-
-					// ignore destructuring if the option is set, unless a new identifier is created
-					if (valueIsInvalid && !(assignmentKeyEqualsValue && options.ignoreDestructuring)) {
-						report(node, keyword);
 					}
 				}
 
@@ -125,7 +150,7 @@ const create = context => {
 			// Check if it's an import specifier
 			} else if (['ImportSpecifier', 'ImportNamespaceSpecifier', 'ImportDefaultSpecifier'].indexOf(node.parent.type) >= 0) {
 				// Report only if the local imported identifier is invalid
-				if (node.parent.local && node.parent.local.name === node.name && Boolean(keyword)) {
+				if (Boolean(keyword) && node.parent.local && node.parent.local.name === node.name) {
 					report(node, keyword);
 				}
 
@@ -134,7 +159,6 @@ const create = context => {
 				report(node, keyword);
 			}
 		}
-
 	};
 };
 
