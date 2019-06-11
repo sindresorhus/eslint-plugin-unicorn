b270e3846d4f01a114e462b5059f7b062ebcd4dd
Adam Babcock <github@mrhen.com>
2019-06-10 22:40:37 -0500

    Only fix when expression is not used

--- rules/prefer-node-append.js
+++ rules/prefer-node-append.js
@@ -3,16 +3,30 @@ const getDocsUrl = require('./utils/get-docs-url');
 
 const getMethodName = memberExpression => memberExpression.property.name;
 
+const ignoredParentTypes = [
+	'VariableDeclarator',
+	'CallExpression'
+];
+
 const create = context => {
 	return {
 		CallExpression(node) {
-			const {callee} = node;
+			const {
+				callee,
+				parent
+			} = node;
 
 			if (callee.type === 'MemberExpression' && getMethodName(callee) === 'appendChild') {
+				let fix = fixer => fixer.replaceText(callee.property, 'append');
+
+				if (parent && ignoredParentTypes.includes(parent.type)) {
+					fix = undefined;
+				}
+
 				context.report({
 					node,
 					message: 'Prefer `Node#append()` over `Node#appendChild()`.',
-					fix: fixer => fixer.replaceText(callee.property, 'append')
+					fix
 				});
 			}
 		}
--- test/prefer-node-append.js
+++ test/prefer-node-append.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import avaRuleTester from 'eslint-ava-rule-tester';
+import {outdent} from 'outdent';
 import rule from '../rules/prefer-node-append';
 
 const ruleTester = avaRuleTester(test, {
@@ -40,6 +41,29 @@ ruleTester.run('prefer-node-append', rule, {
 		{
 			code: 'node.appendChild(null)',
 			output: 'node.append(null)',
+			errors
+		},
+		{
+			code: outdent`
+				function foo() {
+					node.appendChild(null);
+				}
+			`,
+			output: outdent`
+				function foo() {
+					node.append(null);
+				}
+			`,
+			errors
+		},
+		{
+			code: 'const foo = node.appendChild(child);',
+			output: 'const foo = node.appendChild(child);',
+			errors
+		},
+		{
+			code: 'console.log(node.appendChild(child));',
+			output: 'console.log(node.appendChild(child));',
 			errors
 		}
 	]
