7075a77986edd11c4a4296ce19d58df5f2d96d06
George Karagkiaouris <karaggeorge0@gmail.com>
2019-07-03 15:06:11 -0400

    Add more tests

--- test/no-nested-ternary.js
+++ test/no-nested-ternary.js
@@ -12,35 +12,46 @@ const errors = [{ruleId: 'my-rule'}];
 
 ruleTester.run('new-error', rule, {
 	valid: [
-		'var foo = i > 5 ? true : false;',
-		'var foo = i > 5 ? true : (i < 100 ? true : false);',
-		'var foo = i > 5 ? (i < 100 ? true : false) : true;',
-		'var foo = i > 5 ? (i < 100 ? true : false) : (i < 100 ? true : false);',
-		'var foo = i > 5 ? true : (i < 100 ? FOO(i > 50 ? false : true) : false);'
-
+		'const foo = i > 5 ? true : false;',
+		'const foo = i > 5 ? true : (i < 100 ? true : false);',
+		'const foo = i > 5 ? (i < 100 ? true : false) : true;',
+		'const foo = i > 5 ? (i < 100 ? true : false) : (i < 100 ? true : false);',
+		'const foo = i > 5 ? true : (i < 100 ? FOO(i > 50 ? false : true) : false);',
+		'foo ? doBar() : doBaz();',
+		'var foo = bar === baz ? qux : quxx;'
 	],
 	invalid: [
 		{
-			code: 'var foo = i > 5 ? true : (i < 100 ? true : (i < 1000 ? true : false));',
+			code: 'const foo = i > 5 ? true : (i < 100 ? true : (i < 1000 ? true : false));',
 			errors
 		},
 		{
-			code: 'var foo = i > 5 ? true : (i < 100 ? (i > 50 ? false : true) : false);',
+			code: 'const foo = i > 5 ? true : (i < 100 ? (i > 50 ? false : true) : false);',
 			errors
 		},
 		{
-			code: 'var foo = i > 5 ? i < 100 ? true : false : true;',
-			output: 'var foo = i > 5 ? (i < 100 ? true : false) : true;',
+			code: 'const foo = i > 5 ? i < 100 ? true : false : true;',
+			output: 'const foo = i > 5 ? (i < 100 ? true : false) : true;',
 			errors
 		},
 		{
-			code: 'var foo = i > 5 ? i < 100 ? true : false : i < 100 ? true : false;',
-			output: 'var foo = i > 5 ? (i < 100 ? true : false) : (i < 100 ? true : false);',
-			errors: [{ruleId: 'my-rule', column: 19}, {ruleId: 'my-rule', column: 44}]
+			code: 'const foo = i > 5 ? i < 100 ? true : false : i < 100 ? true : false;',
+			output: 'const foo = i > 5 ? (i < 100 ? true : false) : (i < 100 ? true : false);',
+			errors: [{ruleId: 'my-rule', column: 21}, {ruleId: 'my-rule', column: 46}]
 		},
 		{
-			code: 'var foo = i > 5 ? true : i < 100 ? true : false;',
-			output: 'var foo = i > 5 ? true : (i < 100 ? true : false);',
+			code: 'const foo = i > 5 ? true : i < 100 ? true : false;',
+			output: 'const foo = i > 5 ? true : (i < 100 ? true : false);',
+			errors
+		},
+		{
+			code: 'foo ? bar : baz === qux ? quxx : foobar;',
+			output: 'foo ? bar : (baz === qux ? quxx : foobar);',
+			errors
+		},
+		{
+			code: 'foo ? baz === qux ? quxx : foobar : bar;',
+			output: 'foo ? (baz === qux ? quxx : foobar) : bar;',
 			errors
 		}
 	]
