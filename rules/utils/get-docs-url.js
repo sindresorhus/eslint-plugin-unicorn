eefa70f8055733597240cfe4d49449b65408773a
fisker <lionkay@gmail.com>
2019-09-23 16:55:24 +0800

    Add missing file

---
+++ rules/utils/get-docs-url.js
@@ -0,0 +1,10 @@
+'use strict';
+const path = require('path');
+const packageJson = require('../../package');
+
+const repoUrl = 'https://github.com/sindresorhus/eslint-plugin-unicorn';
+
+module.exports = filename => {
+	const ruleName = path.basename(filename, '.js');
+	return `${repoUrl}/blob/v${packageJson.version}/docs/rules/${ruleName}.md`;
+};
