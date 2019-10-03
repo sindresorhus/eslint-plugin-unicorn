cb2673ba4814f3a1dfbd9e9a6af1a5ed0760b1dd
fisker <lionkay@gmail.com>
2019-10-03 21:47:51 +0800

    disable `consistent-function-scoping`

--- test/lint/lint.js
+++ test/lint/lint.js
@@ -20,7 +20,7 @@ const cli = new CLIEngine({
 		],
 
 		// TODO: remove this override, when #391 is fixed
-		'consistent-function-scoping': 'off',
+		'unicorn/consistent-function-scoping': 'off',
 	},
 	useEslintrc: false
 });
