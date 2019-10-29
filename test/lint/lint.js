ae0f95ff246f851dc6bf65774aff2ef0fd0da829
fisker <lionkay@gmail.com>
2019-10-29 10:42:47 +0800

    Improve lint script

--- test/lint/lint.js
+++ test/lint/lint.js
@@ -4,15 +4,12 @@ const {CLIEngine} = require('eslint');
 const unicorn = require('../..');
 
 const {configs: {recommended}} = unicorn;
-const {rules} = recommended;
 const files = [process.argv[2] || '.'];
 const fix = process.argv.includes('--fix');
 
 const cli = new CLIEngine({
-	...recommended,
+	baseConfig: recommended,
 	rules: {
-		...rules,
-
 		// TODO: remove this override, when #391 is fixed
 		'unicorn/consistent-function-scoping': 'off'
 	},
@@ -24,12 +21,30 @@ cli.addPlugin('eslint-plugin-unicorn', unicorn);
 
 const report = cli.executeOnFiles(files);
 
-if (fix) {
+const {errorCount, warningCount, fixableErrorCount, fixableWarningCount} = report
+
+const hasFixable = fixableErrorCount || fixableWarningCount
+
+if (fix && hasFixable) {
 	CLIEngine.outputFixes(report);
 }
 
-const formatter = cli.getFormatter();
+if (errorCount || warningCount) {
+	const formatter = cli.getFormatter();
+	console.log(formatter(report.results));
 
-console.log(formatter(report.results));
+	console.log();
+	console.log('Some test fails, you need fix them, and run `npm run lint <file>` again.');
 
-process.exit(report.errorCount);
+	if (hasFixable) {
+		console.log();
+		console.log('You may also want run `npm run lint <file> --fix` to fix fixable problems.');
+	}
+
+	console.log();
+	console.log('* If you\'re making a new rule, you can fix this later. *');
+} else {
+	console.log('All test passed.')
+}
+
+process.exit(errorCount);
