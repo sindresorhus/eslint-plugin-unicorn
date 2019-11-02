cdbb9ffa57d7c927e07c0a44c6aab8e243a626fd
fisker Cheung <lionkay@gmail.com>
2019-11-02 16:00:13 +0800

    Improve lint script (#426)
    * Improve lint script
    
    * Fix code style
    
    * style
    
    * should fail on `prefer-string-slice` and `prevent-abbreviations` rules
    
    * Revert "should fail on `prefer-string-slice` and `prevent-abbreviations` rules"
    
    This reverts commit a75d068b2d4f0abe88bcaa12b1bbc6d93df3b08e.
    
    * Update test/lint/lint.js
    
    Co-Authored-By: yakov116 <16872793+yakov116@users.noreply.github.com>
    
    * Update messages
    
    * Add rules definition test
    
    * remove assert
    
    * style
    
    * should fail on `prefer-string-slice` rule
    
    * Revert "should fail on `prefer-string-slice` rule"
    
    * add a note to `new-rule`
    
    * Update test/lint/lint.js
    
    Co-Authored-By: yakov116 <16872793+yakov116@users.noreply.github.com>

--- test/lint/lint.js
+++ test/lint/lint.js
@@ -3,16 +3,15 @@
 const {CLIEngine} = require('eslint');
 const unicorn = require('../..');
 
-const {configs: {recommended}} = unicorn;
-const {rules} = recommended;
+const {recommended} = unicorn.configs;
 const files = [process.argv[2] || '.'];
 const fix = process.argv.includes('--fix');
+const ruleIds = Object.keys(unicorn.rules);
+const unicornRules = new Map(Object.entries(unicorn.rules));
 
 const cli = new CLIEngine({
-	...recommended,
+	baseConfig: recommended,
 	rules: {
-		...rules,
-
 		// TODO: remove this override, when #391 is fixed
 		'unicorn/consistent-function-scoping': 'off'
 	},
@@ -22,14 +21,39 @@ const cli = new CLIEngine({
 
 cli.addPlugin('eslint-plugin-unicorn', unicorn);
 
+// Make sure rules are loaded from codebase
+const loadedRules = cli.getRules();
+if (!ruleIds.every(ruleId => unicornRules.get(ruleId) === loadedRules.get(`unicorn/${ruleId}`))) {
+	console.error('`eslint-plugin-unicorn` rules are not loaded from codebase.');
+	process.exit(1);
+}
+
 const report = cli.executeOnFiles(files);
 
-if (fix) {
+const {errorCount, warningCount, fixableErrorCount, fixableWarningCount} = report;
+
+const hasFixable = fixableErrorCount || fixableWarningCount;
+
+if (fix && hasFixable) {
 	CLIEngine.outputFixes(report);
 }
 
-const formatter = cli.getFormatter();
+if (errorCount || warningCount) {
+	const formatter = cli.getFormatter();
+	console.log(formatter(report.results));
 
-console.log(formatter(report.results));
+	console.log();
+	console.log(`You need to fix the failed test${errorCount + warningCount > 1 ? 's' : ''} above and run \`npm run lint <file>\` to check again.`);
 
-process.exit(report.errorCount);
+	if (hasFixable) {
+		console.log();
+		console.log('You may also want run `npm run lint <file> --fix` to fix fixable problems.');
+	}
+
+	console.log();
+	console.log('* If you\'re making a new rule, you can fix this later. *');
+} else {
+	console.log('All tests have passed.');
+}
+
+process.exit(errorCount);
