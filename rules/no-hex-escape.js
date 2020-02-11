ebcb60b234dbaa3730860da357414a5ad42925e9
JounQin <admin@1stg.me>
2020-02-11 07:04:19 +0000

    fix: `node.start` and `node.end` could be `undefined`

--- rules/no-hex-escape.js
+++ rules/no-hex-escape.js
@@ -10,7 +10,16 @@ function checkEscape(context, node, value) {
 		context.report({
 			node,
 			message: 'Use Unicode escapes instead of hexadecimal escapes.',
-			fix: fixer => fixer.replaceTextRange([node.start, node.end], fixedValue)
+			fix: fixer => {
+				let {range: [start, end], type, tail} = node;
+
+				if (type === 'TemplateElement') {
+					start += 1;
+					end -= tail ? 1 : 2;
+				}
+
+				return fixer.replaceTextRange([start, end], fixedValue);
+			}
 		});
 	}
 }
