4260f47ec0bda5ef39c82abcd7428d2ddf47bc92
fisker <lionkay@gmail.com>
2020-02-12 23:04:22 +0800

    Add `object` support to `methodSelector`

--- rules/utils/method-selector.js
+++ rules/utils/method-selector.js
@@ -1,26 +1,36 @@
 'use strict';
 
 module.exports = options => {
-	const {name, length} = {
+	const {name, length, object} = {
 		...options
 	};
 
-	const checkLength = typeof length === 'number';
-
-	return [
+	const selector = [
 		'CallExpression',
 		'[callee.type="MemberExpression"]',
 		'[callee.computed=false]',
-		'[callee.property.type="Identifier"]',
-		name ? `[callee.property.name="${name}"]` : '',
-		checkLength ? `[arguments.length=${length}]` : '',
-		...(
-			!checkLength || length === 0 ?
-				[] :
-				Array.from(
-					{length},
-					(_, index) => `[arguments.${index}.type!="SpreadElement"]`
-				)
-		)
-	].join('');
+		'[callee.property.type="Identifier"]'
+	];
+
+	if (name) {
+		selector.push(`[callee.property.name="${name}"]`);
+	}
+
+	if (object) {
+		selector.push('[callee.object.type="Identifier"]');
+		selector.push(`[callee.object.name="${object}"]`);
+	}
+
+	if (typeof length === 'number') {
+		selector.push(`[arguments.length=${length}]`);
+
+		// Exclude arguments with `SpreadElement` type
+		for (let index = 0; index < length; index += 1) {
+			selector.push(`[arguments.${index}.type!="SpreadElement"]`);
+		}
+	} else if (typeof length === 'string') {
+		selector.push(`[arguments.length${length}]`);
+	}
+
+	return selector.join('');
 };
