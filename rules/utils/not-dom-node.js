2b08a2af508622595392dbb0ea2377042c7d2c7e
fisker <lionkay@gmail.com>
2020-02-17 17:44:08 +0800

    Code style

--- rules/utils/not-dom-node.js
+++ rules/utils/not-dom-node.js
@@ -2,8 +2,8 @@
 
 // AST Types:
 // https://github.com/eslint/espree/blob/master/lib/ast-node-types.js#L18
-// Only types possible to be `callee` or `argument` listed
-const impossibleTypes = [
+// Only types possible to be `callee` or `argument` are listed
+const impossibleNodeTypes = [
 	'ArrayExpression',
 	'ArrowFunctionExpression',
 	'ClassExpression',
@@ -15,17 +15,14 @@ const impossibleTypes = [
 
 // We might need this later
 /* istanbul ignore next */
-const isNotDomNode = node => {
-	return impossibleTypes.includes(node.type) ||
+const isNotDomNode = node =>
+	impossibleNodeTypes.includes(node.type) ||
 	(node.type === 'Identifier' && node.name === 'undefined');
-};
 
-const notDomNodeSelector = node => {
-	return [
-		...impossibleTypes.map(type => `[${node}.type!="${type}"]`),
-		`:not([${node}.type="Identifier"][${node}.name="undefined"])`
-	].join('');
-};
+const notDomNodeSelector = node => [
+	...impossibleNodeTypes.map(type => `[${node}.type!="${type}"]`),
+	`:not([${node}.type="Identifier"][${node}.name="undefined"])`
+].join('');
 
 module.exports = {
 	isNotDomNode,
