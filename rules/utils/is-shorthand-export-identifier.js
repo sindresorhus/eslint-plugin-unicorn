534e5168073d75c26fd4606aec7de4f45f330ac2
fisker <lionkay@gmail.com>
2020-03-16 16:45:08 +0800

    style

--- rules/utils/is-shorthand-export-identifier.js
+++ rules/utils/is-shorthand-export-identifier.js
@@ -2,5 +2,5 @@
 
 module.exports = identifier =>
 	identifier.parent.type === 'ExportSpecifier' &&
-		identifier.parent.exported.name === identifier.name &&
-		identifier.parent.local.name === identifier.name;
+	identifier.parent.exported.name === identifier.name &&
+	identifier.parent.local.name === identifier.name;
