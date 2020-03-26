be14fa49f4be5843fc30d7cd44a6441bbdeda524
fisker <lionkay@gmail.com>
2020-03-26 19:50:26 +0800

    Fix link

--- test/integration/test.js
+++ test/integration/test.js
@@ -147,7 +147,7 @@ list.run()
 					const {file, project, destination} = error2.eslintJob;
 					const {line} = error2.eslintMessage;
 
-					console.error(chalk.gray(`${project.repository}/tree/master/${path.relative(destination, file.filePath)}L${line}`));
+					console.error(chalk.gray(`${project.repository}/tree/master/${path.relative(destination, file.filePath)}#L${line}`));
 					console.error(chalk.gray(JSON.stringify(error2.eslintMessage, null, 2)));
 				}
 			}
