faee841d6d2e0958b85a65bb2c9e972a2b033c3e
fisker Cheung <lionkay@gmail.com>
2020-03-27 16:02:38 +0800

    Simplify `no-console-spaces` (#634)

--- rules/no-console-spaces.js
+++ rules/no-console-spaces.js
@@ -1,6 +1,7 @@
 'use strict';
 const getDocumentationUrl = require('./utils/get-documentation-url');
 const methodSelector = require('./utils/method-selector');
+const replaceStringRaw = require('./utils/replace-string-raw');
 
 const message = 'Do not use leading/trailing space between `console.{{method}}` parameters.';
 
@@ -18,97 +19,62 @@ const selector = methodSelector({
 	object: 'console'
 });
 
-const getArgumentValue = (context, nodeArgument) => {
-	let value = '';
+// Find exactly one leading space, allow exactly one space
+const fixLeadingSpace = value =>
+	value.length > 1 && value.charAt(0) === ' ' && value.charAt(1) !== ' ' ?
+		value.slice(1) :
+		value;
 
-	if (
-		nodeArgument.type === 'Literal' &&
-		typeof nodeArgument.value === 'string'
-	) {
-		value = nodeArgument.raw;
-	}
-
-	if (nodeArgument.type === 'TemplateLiteral') {
-		value = context.getSourceCode().getText(nodeArgument);
-	}
-
-	// Strip off quotes and backticks
-	return value.slice(1, -1);
-};
-
-const fixValue = (value, {
-	fixLeading = true,
-	fixTrailing = true
-}) => {
-	if (!value) {
-		return value;
-	}
-
-	// Allow exactly one space
-	if (value.length <= 1) {
-		return value;
-	}
-
-	let fixed = value;
-
-	// Find exactly one leading space
-	if (fixLeading && fixed.startsWith(' ') && !fixed.startsWith('  ')) {
-		fixed = fixed.slice(1);
-	}
-
-	// Find exactly one trailing space
-	if (fixTrailing && fixed.endsWith(' ') && !fixed.endsWith('  ')) {
-		fixed = fixed.slice(0, -1);
-	}
-
-	return fixed;
-};
-
-const getFixableArguments = (context, node) => {
-	const {arguments: arguments_} = node;
-
-	const fixables = arguments_.map((nodeArgument, i) => {
-		const fixLeading = i !== 0;
-		const fixTrailing = i !== arguments_.length - 1;
-
-		const value = getArgumentValue(context, nodeArgument);
-		const fixed = fixValue(value, {fixLeading, fixTrailing});
-
-		return {
-			nodeArgument,
-			value,
-			fixed,
-			fixable: value !== fixed
-		};
-	});
-
-	return fixables.filter(fixable => fixable.fixable);
-};
-
-const fixArgument = (context, fixable, fixer) => {
-	const {nodeArgument, fixed} = fixable;
-
-	// Ignore quotes and backticks
-	const range = [
-		nodeArgument.range[0] + 1,
-		nodeArgument.range[1] - 1
-	];
-
-	return fixer.replaceTextRange(range, fixed);
-};
+// Find exactly one trailing space, allow exactly one space
+const fixTrailingSpace = value =>
+	value.length > 1 && value.charAt(value.length - 1) === ' ' && value.charAt(value.length - 2) !== ' ' ?
+		value.slice(0, -1) :
+		value;
 
 const create = context => {
+	const sourceCode = context.getSourceCode();
+
+	const fixParamter = (node, index, parameters) => {
+		if (
+			!(node.type === 'Literal' && typeof node.value === 'string') &&
+			node.type !== 'TemplateLiteral'
+		) {
+			return;
+		}
+
+		const raw = sourceCode.getText(node).slice(1, -1);
+
+		let fixed = raw;
+
+		if (index !== 0) {
+			fixed = fixLeadingSpace(fixed);
+		}
+
+		if (index !== parameters.length - 1) {
+			fixed = fixTrailingSpace(fixed);
+		}
+
+		if (raw !== fixed) {
+			return {
+				node,
+				fixed
+			};
+		}
+	};
+
 	return {
 		[selector](node) {
 			const method = node.callee.property.name;
+			const fixedParameters = node.arguments
+				.map((parameter, index) => fixParamter(parameter, index, node.arguments))
+				.filter(Boolean);
 
-			const fixables = getFixableArguments(context, node);
-			for (const fixable of fixables) {
+			for (const {node, fixed} of fixedParameters) {
 				context.report({
-					node: fixable.nodeArgument,
+					node,
 					message,
 					data: {method},
-					fix: fixer => fixArgument(context, fixable, fixer)
+					fix: fixer => replaceStringRaw(fixer, node, fixed)
 				});
 			}
 		}
