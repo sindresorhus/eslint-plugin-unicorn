399abaf746cba7229cc3ff561d38d092043e4a8a
fisker <lionkay@gmail.com>
2020-03-27 15:47:30 +0800

    Add comment

--- rules/utils/replace-string-raw.js
+++ rules/utils/replace-string-raw.js
@@ -1,11 +1,12 @@
 'use strict';
 
-module.exports = (fixer, node, replacement) =>
+// Replace `StringLiteral` or `TemplateLiteral` node with raw text
+module.exports = (fixer, node, raw) =>
 	fixer.replaceTextRange(
 		// Ignore quotes and backticks
 		[
 			node.range[0] + 1,
 			node.range[1] - 1
 		],
-		replacement
+		raw
 	);
