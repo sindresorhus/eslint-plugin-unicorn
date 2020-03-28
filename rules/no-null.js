6cbfb0344fab82c24af6063c16e27aecc0fc8d12
himynameisdave <dave@fossa.com>
2020-03-28 13:33:05 -0700

    💫 Replace hacky selector with a better one

--- rules/no-null.js
+++ rules/no-null.js
@@ -1,20 +1,26 @@
 'use strict';
 const getDocumentationUrl = require('./utils/get-documentation-url');
+const methodSelector = require('./utils/method-selector');
 
-// The number of chars in "Object.create("
-const objectCreateCharCount = 14;
-const objectCreate = 'Object.create(null';
-// Check if the 14 chars preceding `null` are "Object.create("
-const isObjectCreate = (context, node) => context.getSource(node, objectCreateCharCount) === objectCreate;
+const objectCreateSelector = methodSelector({
+	object: 'Object',
+	name: 'create',
+	length: 1
+});
+
+const selector = [
+	`:not(${objectCreateSelector})`,
+	'>',
+	'Literal',
+	'[raw="null"]'
+].join('');
 
 const create = context => ({
-	Literal: node => {
-		if (node.raw === 'null' && !isObjectCreate(context, node)) {
-			context.report({
-				node,
-				message: 'Use undefined instead of null'
-			});
-		}
+	[selector]: node => {
+		context.report({
+			node,
+			message: 'Use undefined instead of null'
+		});
 	}
 });
 
