a7a0377741661ed250e71cbbfdb29b22345d6441
fisker <lionkay@gmail.com>
2020-03-31 15:33:34 +0800

    Add more tests

--- test/no-null.js
+++ test/no-null.js
@@ -3,9 +3,6 @@ import avaRuleTester from 'eslint-ava-rule-tester';
 import rule from '../rules/no-null';
 
 const ruleTester = avaRuleTester(test, {
-	env: {
-		es6: true
-	},
 	parserOptions: {
 		sourceType: 'module'
 	}
@@ -19,7 +16,11 @@ const noNullError = {
 ruleTester.run('no-null', rule, {
 	valid: [
 		'let foo',
-		'Object.create(null)'
+		'Object.create(null)',
+		// Not `null`
+		'const foo = "null";',
+		// More/Less arguments
+		'Object.create()'
 	],
 	invalid: [
 		{
@@ -30,6 +31,68 @@ ruleTester.run('no-null', rule, {
 		},
 		{
 			code: 'if (bar === null) {}',
+			errors: [
+				noNullError
+			]
+		},
+		// Not `CallExpression`
+		{
+			code: 'new Object.create(null)',
+			errors: [
+				noNullError
+			]
+		},
+		// Not `MemberExpression`
+		{
+			code: 'create(null)',
+			errors: [
+				noNullError
+			]
+		},
+		// `callee.property` is not a `Identifier`
+		{
+			code: 'Object["create"](null)',
+			errors: [
+				noNullError
+			]
+		},
+		// Computed
+		{
+			code: 'Object[create](null)',
+			errors: [
+				noNullError
+			]
+		},
+		// Not `create`
+		{
+			code: 'Object.notCreate(null)',
+			errors: [
+				noNullError
+			]
+		},
+		// Not `Object`
+		{
+			code: 'NotObject.create(null)',
+			errors: [
+				noNullError
+			]
+		},
+		// `callee.object.type` is not a `Identifier`
+		{
+			code: 'lib.Object.create(null)',
+			errors: [
+				noNullError
+			]
+		},
+		// More/Less arguments
+		{
+			code: 'Object.create(null, "")',
+			errors: [
+				noNullError
+			]
+		},
+		{
+			code: 'Object.create(...[null])',
 			errors: [
 				noNullError
 			]
