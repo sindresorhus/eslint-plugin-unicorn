742f8990278d11ff6689b8f531ee41aa604d75e7
fisker <lionkay@gmail.com>
2020-03-31 16:22:35 +0800

    Add fix

--- rules/no-null.js
+++ rules/no-null.js
@@ -2,6 +2,10 @@
 const getDocumentationUrl = require('./utils/get-documentation-url');
 const methodSelector = require('./utils/method-selector');
 
+const ERROR_MESSAGE_ID = 'error';
+const SUGGESTION_REPLACE_MESSAGE_ID = 'replace';
+const SUGGESTION_REMOVE_MESSAGE_ID = 'remove';
+
 const objectCreateSelector = methodSelector({
 	object: 'Object',
 	name: 'create',
@@ -15,21 +19,59 @@ const selector = [
 	'[raw="null"]'
 ].join('');
 
+const isLooseEqual = node => node.type === 'BinaryExpression' && ['==', '!='].includes(node.operator);
+
 const create = context => ({
 	[selector]: node => {
-		context.report({
+		const problem = {
 			node,
-			message: 'Use undefined instead of null'
-		});
+			messageId: ERROR_MESSAGE_ID
+		};
+
+		const fix = fixer => fixer.replaceText(node, 'undefined');
+
+		const {parent = {}} = node;
+		if (isLooseEqual(parent)) {
+			problem.fix = fix;
+		} else if (parent.type === 'ReturnStatement' && parent.argument === node) {
+			problem.suggest = [
+				{
+					messageId: SUGGESTION_REMOVE_MESSAGE_ID,
+					fix: fixer => fixer.replaceText(node, '')
+				}
+			];
+		} else if (parent.type === 'VariableDeclarator' && parent.init === node && parent.parent.kind !== 'const') {
+			problem.suggest = [
+				{
+					messageId: SUGGESTION_REMOVE_MESSAGE_ID,
+					fix: fixer => fixer.replaceTextRange([parent.id.range[1], node.range[1]], '')
+				}
+			];
+		} else {
+			problem.suggest = [
+				{
+					messageId: SUGGESTION_REPLACE_MESSAGE_ID,
+					fix
+				}
+			];
+		}
+
+		context.report(problem);
 	}
 });
 
 module.exports = {
+	create,
 	meta: {
 		type: 'suggestion',
 		docs: {
 			url: getDocumentationUrl(__filename)
-		}
-	},
-	create
+		},
+		messages: {
+			[ERROR_MESSAGE_ID]: 'Use `undefined` instead of `null`.',
+			[SUGGESTION_REPLACE_MESSAGE_ID]: 'Replace `null` with `undefined`.',
+			[SUGGESTION_REMOVE_MESSAGE_ID]: 'Remove `null`.'
+		},
+		fixable: true
+	}
 };
--- test/no-null.js
+++ test/no-null.js
@@ -1,16 +1,64 @@
 import test from 'ava';
 import avaRuleTester from 'eslint-ava-rule-tester';
+import {outdent} from 'outdent';
 import rule from '../rules/no-null';
+
+const ERROR_MESSAGE_ID = 'error';
+const SUGGESTION_REPLACE_MESSAGE_ID = 'replace';
+const SUGGESTION_REMOVE_MESSAGE_ID = 'remove';
 
 const ruleTester = avaRuleTester(test, {
 	parserOptions: {
+		ecmaVersion: 2020,
 		sourceType: 'module'
 	}
 });
 
-const noNullError = {
-	ruleId: 'no-null',
-	message: 'Use undefined instead of null'
+const invalidTestCase = options => {
+	const {
+		code,
+		output,
+		suggestionOutput,
+		suggestionMessageId = SUGGESTION_REPLACE_MESSAGE_ID
+	} = typeof options === 'string' ? {code: options} : options;
+
+	if (suggestionOutput) {
+		return {
+			code,
+			output: code,
+			errors: [
+				{
+					messageId: ERROR_MESSAGE_ID,
+					suggestions: [
+						{
+							messageId: suggestionMessageId,
+							output: suggestionOutput
+						}
+					]}
+			]
+		};
+	}
+
+	if (output) {
+		return {
+			code,
+			output,
+			errors: [
+				{
+					messageId: ERROR_MESSAGE_ID,
+					suggestions: undefined}
+			]
+		};
+	}
+
+	return {
+		code,
+		output: code,
+		errors: [
+			{
+				messageId: ERROR_MESSAGE_ID}
+		]
+	};
 };
 
 ruleTester.run('no-null', rule, {
@@ -23,79 +71,83 @@ ruleTester.run('no-null', rule, {
 		'Object.create()'
 	],
 	invalid: [
-		{
-			code: 'const foo = null',
-			errors: [
-				noNullError
-			]
-		},
-		{
-			code: 'if (bar === null) {}',
-			errors: [
-				noNullError
-			]
-		},
+		invalidTestCase('const foo = null'),
+		invalidTestCase('if (foo === null) {}'),
+
+		// Auto fix
+		invalidTestCase({
+			code: 'if (foo == null) {}',
+			output: 'if (foo == undefined) {}'
+		}),
+		invalidTestCase({
+			code: 'if (foo != null) {}',
+			output: 'if (foo != undefined) {}'
+		}),
+		invalidTestCase({
+			code: 'if (null == foo) {}',
+			output: 'if (undefined == foo) {}'
+		}),
+		invalidTestCase({
+			code: 'if (null != foo) {}',
+			output: 'if (undefined != foo) {}'
+		}),
+
+		// Suggestion `ReturnStatement`
+		invalidTestCase({
+			code: outdent`
+				function foo() {
+					return null;
+				}
+			`,
+			suggestionOutput: outdent`
+				function foo() {
+					return ;
+				}
+			`,
+			suggestionMessageId: SUGGESTION_REMOVE_MESSAGE_ID
+		}),
+
+		// Suggestion `VariableDeclaration`
+		invalidTestCase({
+			code: 'let foo = null;',
+			suggestionOutput: 'let foo;',
+			suggestionMessageId: SUGGESTION_REMOVE_MESSAGE_ID
+		}),
+		invalidTestCase({
+			code: 'var foo = null;',
+			suggestionOutput: 'var foo;',
+			suggestionMessageId: SUGGESTION_REMOVE_MESSAGE_ID
+		}),
+		invalidTestCase({
+			code: 'var foo = 1, bar = null, baz = 2;',
+			suggestionOutput: 'var foo = 1, bar, baz = 2;',
+			suggestionMessageId: SUGGESTION_REMOVE_MESSAGE_ID
+		}),
+		invalidTestCase({
+			code: 'const foo = null;',
+			suggestionOutput: 'const foo = undefined;'
+		}),
+
 		// Not `CallExpression`
-		{
-			code: 'new Object.create(null)',
-			errors: [
-				noNullError
-			]
-		},
+		invalidTestCase('new Object.create(null)'),
 		// Not `MemberExpression`
-		{
-			code: 'create(null)',
-			errors: [
-				noNullError
-			]
-		},
+		invalidTestCase('create(null)'),
 		// `callee.property` is not a `Identifier`
-		{
-			code: 'Object["create"](null)',
-			errors: [
-				noNullError
-			]
-		},
+		invalidTestCase('Object["create"](null)'),
 		// Computed
+		invalidTestCase('Object[create](null)'),
 		{
-			code: 'Object[create](null)',
-			errors: [
-				noNullError
-			]
+			code: 'Object[null](null)',
+			errors: [{}, {}]
 		},
 		// Not `create`
-		{
-			code: 'Object.notCreate(null)',
-			errors: [
-				noNullError
-			]
-		},
+		invalidTestCase('Object.notCreate(null)'),
 		// Not `Object`
-		{
-			code: 'NotObject.create(null)',
-			errors: [
-				noNullError
-			]
-		},
+		invalidTestCase('NotObject.create(null)'),
 		// `callee.object.type` is not a `Identifier`
-		{
-			code: 'lib.Object.create(null)',
-			errors: [
-				noNullError
-			]
-		},
+		invalidTestCase('lib.Object.create(null)'),
 		// More/Less arguments
-		{
-			code: 'Object.create(null, "")',
-			errors: [
-				noNullError
-			]
-		},
-		{
-			code: 'Object.create(...[null])',
-			errors: [
-				noNullError
-			]
-		}
+		invalidTestCase('Object.create(null, "")'),
+		invalidTestCase('Object.create(...[null])')
 	]
 });
