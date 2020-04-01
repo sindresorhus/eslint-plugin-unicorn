cd708c9995d506795aaa8ec8c74ce083bddd0b4c
fisker <lionkay@gmail.com>
2020-04-01 11:09:47 +0800

    Run integration tests on disabled rules

--- test/integration/config.js
+++ test/integration/config.js
@@ -1,4 +1,12 @@
 'use strict';
+const unicorn = require('eslint-plugin-unicorn');
+
+const enableAllRules = Object.fromEntries(
+	Object.entries(unicorn.configs.recommended.rules)
+		.filter(([id, options]) => id.startsWith('unicorn/') && options === 'off')
+		.map(([id]) => [id, 'error'])
+);
+
 module.exports = {
 	root: true,
 	parser: 'babel-eslint',
@@ -11,5 +19,22 @@ module.exports = {
 	plugins: [
 		'unicorn'
 	],
-	extends: 'plugin:unicorn/recommended'
+	extends: 'plugin:unicorn/recommended',
+	rules: {
+		...enableAllRules,
+
+		// This rule has no effect without options
+		'unicorn/string-content': [
+			'error',
+			{
+				patterns: {
+					a: '\'',
+					b: '"',
+					c: '`',
+					d: '${',
+					e: '}'
+				}
+			}
+		]
+	}
 };
