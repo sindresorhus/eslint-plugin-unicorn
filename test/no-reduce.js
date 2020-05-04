394654dad4daea5a2881276d1241294aeab905e0
fisker <lionkay@gmail.com>
2020-05-05 03:14:36 +0800

    Simplify tests

--- test/no-reduce.js
+++ test/no-reduce.js
@@ -11,13 +11,6 @@ const ruleTester = avaRuleTester(test, {
 	}
 });
 
-const babelRuleTester = avaRuleTester(test, {
-	parser: require.resolve('babel-eslint')
-});
-const typescriptRuleTester = avaRuleTester(test, {
-	parser: require.resolve('@typescript-eslint/parser')
-});
-
 const errors = [{messageId}];
 
 const tests = {
@@ -50,105 +43,32 @@ const tests = {
 		'foo.reduce(...argumentsArray)'
 	],
 	invalid: [
-		{
-			code: 'arr.reduce((total, item) => total + item)',
-			errors
-		},
-		{
-			code: 'arr.reduce((total, item) => total + item, 0)',
-			errors
-		},
-		{
-			code: 'arr.reduce(function (total, item) { return total + item }, 0)',
-			errors
-		},
-		{
-			code: 'arr.reduce(function (total, item) { return total + item })',
-			errors
-		},
-		{
-			code: 'arr.reduce((str, item) => str += item, \'\')',
-			errors
-		},
-		{
-			code: outdent`
-				arr.reduce((obj, item) => {
-				obj[item] = null;
-				return obj;
-				}, {})
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				arr.reduce((obj, item) => ({ [item]: null }), {})
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				const hyphenate = (str, char) => \`\${str}-\${char}\`;
-				["a", "b", "c"].reduce(hyphenate);
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				[].reduce.call(arr, (s, i) => s + i)
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				[].reduce.call(arr, sum);
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				[].reduce.call(sum);
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				Array.prototype.reduce.call(arr, (s, i) => s + i)
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				Array.prototype.reduce.call(arr, sum);
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				[].reduce.apply(arr, [(s, i) => s + i])
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				[].reduce.apply(arr, [sum]);
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				Array.prototype.reduce.apply(arr, [(s, i) => s + i])
-			`,
-			errors
-		},
-		{
-			code: outdent`
-				Array.prototype.reduce.apply(arr, [sum]);
-			`,
-			errors
-		}
-	]
+		'arr.reduce((total, item) => total + item)',
+		'arr.reduce((total, item) => total + item, 0)',
+		'arr.reduce(function (total, item) { return total + item }, 0)',
+		'arr.reduce(function (total, item) { return total + item })',
+		'arr.reduce((str, item) => str += item, \'\')',
+		outdent`
+			arr.reduce((obj, item) => {
+			obj[item] = null;
+			return obj;
+			}, {})
+		`,
+		'arr.reduce((obj, item) => ({ [item]: null }), {})',
+		outdent`
+			const hyphenate = (str, char) => \`\${str}-\${char}\`;
+			["a", "b", "c"].reduce(hyphenate);
+		`,
+		'[].reduce.call(arr, (s, i) => s + i)',
+		'[].reduce.call(arr, sum);',
+		'[].reduce.call(sum);',
+		'Array.prototype.reduce.call(arr, (s, i) => s + i)',
+		'Array.prototype.reduce.call(arr, sum);',
+		'[].reduce.apply(arr, [(s, i) => s + i])',
+		'[].reduce.apply(arr, [sum]);',
+		'Array.prototype.reduce.apply(arr, [(s, i) => s + i])',
+		'Array.prototype.reduce.apply(arr, [sum]);'
+	].map(code => ({code, errors}))
 };
 
 ruleTester.run('no-reduce', rule, tests);
-babelRuleTester.run('no-reduce', rule, tests);
-typescriptRuleTester.run('no-reduce', rule, tests);
