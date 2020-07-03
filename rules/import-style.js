0fcf6cdf74be60f8c53f1f933be80b1a393f92ae
futpib <futpib@gmail.com>
2020-07-03 13:22:05 +0300

    Handle import declaration

--- rules/import-style.js
+++ rules/import-style.js
@@ -1,12 +1,122 @@
 'use strict';
+const {getStringIfConstant} = require('eslint-utils');
+
 const getDocumentationUrl = require('./utils/get-documentation-url');
 
-const DEFAULT_MESSAGE_ID = 'importStyleDefault';
-const NAMESPACE_MESSAGE_ID = 'importStyleNamespace';
-const NAMED_MESSAGE_ID = 'importStyleNamed';
+const getAllowedImportStyles = (styles, moduleName) => {
+	const importStyles = styles[moduleName];
+
+	if (Array.isArray(importStyles)) {
+		return importStyles;
+	}
+
+	if (typeof importStyles === 'string') {
+		return [ importStyles ];
+	}
+
+	return null;
+}
+
+const getActualImportDeclarationStyles = importDeclaration => {
+	const { specifiers } = importDeclaration;
+
+	if (specifiers.length === 0) {
+		return [ 'unassigned' ];
+	}
+
+	const styles = new Set();
+
+	for (const specifier of specifiers) {
+		if (specifier.type === 'ImportDefaultSpecifier') {
+			styles.add('default');
+			continue;
+		}
+
+		if (specifier.type === 'ImportNamespaceSpecifier') {
+			styles.add('namespace');
+			continue;
+		}
+
+		if (specifier.type === 'ImportSpecifier') {
+			styles.add('named');
+			continue;
+		}
+	}
+
+	return [ ...styles ];
+};
+
+const joinOr = words => {
+	return words
+		.map((word, index) => {
+			if (index === (words.length - 1)) {
+				return word;
+			}
+
+			if (index === (words.length - 2)) {
+				return word + ' or';
+			}
+
+			return word + ',';
+		})
+		.join(' ');
+}
+
+const MESSAGE_ID = 'importStyle';
+
+const defaultStyles =  {
+	util: 'named',
+	path: 'default',
+	chalk: 'default',
+};
 
 const create = context => {
+	let [
+		{
+			styles = {}
+		} = {}
+	] = context.options;
+
+	styles = {
+		...defaultStyles,
+		...styles,
+	};
+
+	const report = (node, moduleName, actualImportStyles, allowedImportStyles) => {
+		const data = {
+			allowedStyles: joinOr(allowedImportStyles),
+			moduleName,
+		};
+
+		context.report({
+			node,
+			messageId: MESSAGE_ID,
+			data,
+		});
+	};
+
 	return {
+		ImportDeclaration(node) {
+			const moduleName = getStringIfConstant(node.source, context.getScope());
+
+			if (!moduleName) {
+				return;
+			}
+
+			const allowedImportStyles = getAllowedImportStyles(styles, moduleName);
+
+			if (!allowedImportStyles || allowedImportStyles.length === 0) {
+				return;
+			}
+
+			const actualImportStyles = getActualImportDeclarationStyles(node);
+
+			if (actualImportStyles.every(style => allowedImportStyles.includes(style))) {
+				return;
+			}
+
+			report(node, moduleName, actualImportStyles, allowedImportStyles);
+		},
 	};
 };
 
@@ -18,9 +128,7 @@ module.exports = {
 			url: getDocumentationUrl(__filename)
 		},
 		messages: {
-			[DEFAULT_MESSAGE_ID]: 'Use default import for module `{{module}}`.',
-			[NAMESPACE_MESSAGE_ID]: 'Use namespace import for module `{{parameter}}`.',
-			[NAMED_MESSAGE_ID]: 'Use named import for module `{{parameter}}`.',
+			[MESSAGE_ID]: 'Use {{allowedStyles}} import for module `{{moduleName}}`.',
 		}
 	}
 };
--- test/import-style.js
+++ test/import-style.js
@@ -10,36 +10,179 @@ const ruleTester = avaRuleTester(test, {
 	}
 });
 
+const options = {
+	styles: {
+		unassigned: 'unassigned',
+		default: 'default',
+		namespace: 'namespace',
+		named: 'named',
+	},
+};
+
+const unassignedError = {
+	messageId: 'importStyle',
+	data: {
+		allowedStyles: 'unassigned',
+		moduleName: 'unassigned',
+	},
+};
+
 const defaultError = {
-	messageId: 'importStyleDefault',
-	data: {module: 'default'},
+	messageId: 'importStyle',
+	data: {
+		allowedStyles: 'default',
+		moduleName: 'default'
+	},
 };
 
 const namespaceError = {
-	messageId: 'importStyleNamespace',
-	data: {module: 'namespace'},
+	messageId: 'importStyle',
+	data: {
+		allowedStyles: 'namespace',
+		moduleName: 'namespace'
+	},
 };
 
 const namedError = {
-	messageId: 'importStyleNamed',
-	data: {module: 'named'},
+	messageId: 'importStyle',
+	data: {
+		allowedStyles: 'named',
+		moduleName: 'named'
+	},
 };
+
+const addOptions = test => {
+	if (typeof test === 'string') {
+		test = {
+			code: test,
+		};
+	}
+
+	return {
+		options: [ options ],
+		...test
+	};
+}
 
 ruleTester.run('import-style', rule, {
 	valid: [
+		`require('unassigned')`,
+		`import 'unassigned'`,
+		`import('unassigned')`,
+
 		`const x = require('default')`,
 		`const {default: x} = require('default')`,
 		`import x from 'default'`,
+		outdent`
+			async () => {
+				const {default: x} = await import('default');
+			}
+		`,
 
 		`const x = require('namespace')`,
 		`import * as x from 'namespace'`,
+		outdent`
+			async () => {
+				const x = await import('namespace');
+			}
+		`,
 
 		`const {x} = require('named')`,
+		`const {x: y} = require('named')`,
 		`import {x} from 'named'`,
-	],
+		`import {x as y} from 'named'`,
+		outdent`
+			async () => {
+				const {x} = await import('named');
+			}
+		`,
+		outdent`
+			async () => {
+				const {x: y} = await import('named');
+			}
+		`,
+	].map(addOptions),
+
 	invalid: [
 		{
+			code: `const {x} = require('unassigned')`,
+			errors: [unassignedError]
+		},
+		{
+			code: `const {default: x} = require('unassigned')`,
+			errors: [unassignedError]
+		},
+		{
+			code: `import x from 'unassigned'`,
+			errors: [unassignedError]
+		},
+		{
+			code: outdent`
+				async () => {
+					const {default: x} = await import('unassigned');
+				}
+			`,
+			errors: [unassignedError]
+		},
+		{
+			code: `const x = require('unassigned')`,
+			errors: [unassignedError]
+		},
+		{
+			code: `import * as x from 'unassigned'`,
+			errors: [unassignedError]
+		},
+		{
+			code: outdent`
+				async () => {
+					const x = await import('unassigned');
+				}
+			`,
+			errors: [unassignedError]
+		},
+		{
+			code: `const {x} = require('unassigned')`,
+			errors: [unassignedError]
+		},
+		{
+			code: `const {x: y} = require('unassigned')`,
+			errors: [unassignedError]
+		},
+		{
+			code: `import {x} from 'unassigned'`,
+			errors: [unassignedError]
+		},
+		{
+			code: `import {x as y} from 'unassigned'`,
+			errors: [unassignedError]
+		},
+		{
+			code: outdent`
+				async () => {
+					const {x} = await import('unassigned');
+				}
+			`,
+			errors: [unassignedError]
+		},
+		{
+			code: outdent`
+				async () => {
+					const {x: y} = await import('unassigned');
+				}
+			`,
+			errors: [unassignedError]
+		},
+
+		{
 			code: `import * as x from 'default'`,
+			errors: [defaultError]
+		},
+		{
+			code: outdent`
+				async () => {
+					const x = await import('default');
+				}
+			`,
 			errors: [defaultError]
 		},
 		{
@@ -51,11 +194,27 @@ ruleTester.run('import-style', rule, {
 			errors: [defaultError]
 		},
 		{
-			code: `import {x} = require('default')`,
+			code: `import {x} from 'default'`,
 			errors: [defaultError]
 		},
 		{
 			code: `import {x as y} from 'default'`,
+			errors: [defaultError]
+		},
+		{
+			code: outdent`
+				async () => {
+					const {x} = await import('default');
+				}
+			`,
+			errors: [defaultError]
+		},
+		{
+			code: outdent`
+				async () => {
+					const {x: y} = await import('default');
+				}
+			`,
 			errors: [defaultError]
 		},
 
@@ -72,7 +231,31 @@ ruleTester.run('import-style', rule, {
 			errors: [namespaceError]
 		},
 		{
+			code: `const {x: y} = require('namespace')`,
+			errors: [namespaceError]
+		},
+		{
 			code: `import {x} from 'namespace'`,
+			errors: [namespaceError]
+		},
+		{
+			code: `import {x as y} from 'namespace'`,
+			errors: [namespaceError]
+		},
+		{
+			code: outdent`
+				async () => {
+					const {x} = await import('namespace');
+				}
+			`,
+			errors: [namespaceError]
+		},
+		{
+			code: outdent`
+				async () => {
+					const {x: y} = await import('namespace');
+				}
+			`,
 			errors: [namespaceError]
 		},
 
@@ -89,8 +272,24 @@ ruleTester.run('import-style', rule, {
 			errors: [namedError]
 		},
 		{
+			code: outdent`
+				async () => {
+					const {default: x} = await import('named');
+				}
+			`,
+			errors: [namedError]
+		},
+		{
 			code: `import * as x from 'named'`,
 			errors: [namedError]
 		},
-	]
+		{
+			code: outdent`
+				async () => {
+					const x = await import('named');
+				}
+			`,
+			errors: [namedError]
+		},
+	].map(addOptions)
 });
