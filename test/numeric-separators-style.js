a511e663b888a2c5afc243150f19dcf951dae352
fisker <lionkay@gmail.com>
2020-09-29 09:59:35 +0800

    `var` -> `const`

--- test/numeric-separators-style.js
+++ test/numeric-separators-style.js
@@ -18,320 +18,320 @@ const error = {
 ruleTester.run('numeric-separators-style', rule, {
 	valid: [
 		// Hexadecimal
-		'var foo = 0xAB_CD',
-		'var foo = 0xAB',
-		'var foo = 0xA',
-		'var foo = 0xA_BC_DE_F0',
-		'var foo = 0xab_e8_12',
-		'var foo = 0xe',
-		'var foo = 0Xab_e3_cd',
+		'const foo = 0xAB_CD',
+		'const foo = 0xAB',
+		'const foo = 0xA',
+		'const foo = 0xA_BC_DE_F0',
+		'const foo = 0xab_e8_12',
+		'const foo = 0xe',
+		'const foo = 0Xab_e3_cd',
 
 		// Octal
-		'var foo = 0o1234_5670',
-		'var foo = 0o7777',
-		'var foo = 0o01',
-		'var foo = 0o12_7000_0000',
-		'var foo = 0O1111_1111',
+		'const foo = 0o1234_5670',
+		'const foo = 0o7777',
+		'const foo = 0o01',
+		'const foo = 0o12_7000_0000',
+		'const foo = 0O1111_1111',
 
 		// Legacy octal
-		'var foo = 0777777',
-		'var foo = 0999999',
-		'var foo = 0111222',
+		'const foo = 0777777',
+		'const foo = 0999999',
+		'const foo = 0111222',
 
 		// Binary
-		'var foo = 0b1010_0001_1000_0101',
-		'var foo = 0b0000',
-		'var foo = 0b10',
-		'var foo = 0b1_0111_0101_0101',
-		'var foo = 0B1010',
+		'const foo = 0b1010_0001_1000_0101',
+		'const foo = 0b0000',
+		'const foo = 0b10',
+		'const foo = 0b1_0111_0101_0101',
+		'const foo = 0B1010',
 
 		// Binary with BigInt
-		'var foo = 0b1010n',
-		'var foo = 0b1010_1010n',
+		'const foo = 0b1010n',
+		'const foo = 0b1010_1010n',
 
 		// BigInt
-		'var foo = 9_223_372_036_854_775_807n',
-		'var foo = 807n',
-		'var foo = 1n',
-		'var foo = 9_372_854_807n',
-		'var foo = 9807n',
-		'var foo = 0n',
+		'const foo = 9_223_372_036_854_775_807n',
+		'const foo = 807n',
+		'const foo = 1n',
+		'const foo = 9_372_854_807n',
+		'const foo = 9807n',
+		'const foo = 0n',
 
 		// Numbers
-		'var foo = 12_345_678',
-		'var foo = 123',
-		'var foo = 1',
-		'var foo = 1234',
+		'const foo = 12_345_678',
+		'const foo = 123',
+		'const foo = 1',
+		'const foo = 1234',
 		{
-			code: 'var foo = 1_234',
+			code: 'const foo = 1_234',
 			options: [{number: {minimumDigits: 0, groupLength: 3}}]
 		},
 
 		// Decimal numbers
-		'var foo = 9807.123',
-		'var foo = 3819.123_432',
-		'var foo = 138_789.123_432_42',
-		'var foo = .000_000_1',
+		'const foo = 9807.123',
+		'const foo = 3819.123_432',
+		'const foo = 138_789.123_432_42',
+		'const foo = .000_000_1',
 
 		// Negative numbers
-		'var foo = -3000',
-		'var foo = -10_000_000',
+		'const foo = -3000',
+		'const foo = -10_000_000',
 
 		// Exponential notation
-		'var foo = 1e10_000',
-		'var foo = 39_804e1000',
-		'var foo = -123_456e-100',
-		'var foo = -100_000e-100_000',
-		'var foo = -100_000e+100_000',
-		'var foo = 3.6e12_000',
-		'var foo = 3.6E12_000',
-		'var foo = -1_200_000e5',
+		'const foo = 1e10_000',
+		'const foo = 39_804e1000',
+		'const foo = -123_456e-100',
+		'const foo = -100_000e-100_000',
+		'const foo = -100_000e+100_000',
+		'const foo = 3.6e12_000',
+		'const foo = 3.6E12_000',
+		'const foo = -1_200_000e5',
 
 		// Miscellaneous values
-		'var foo = -282_932 - (1938 / 10_000) * .1 + 18.100_000_2',
-		'var foo = NaN',
-		'var foo = Infinity',
-		'var foo = "1234567n"',
+		'const foo = -282_932 - (1938 / 10_000) * .1 + 18.100_000_2',
+		'const foo = NaN',
+		'const foo = Infinity',
+		'const foo = "1234567n"',
 
 		// Varying options
 		{
-			code: 'var foo = 10000',
+			code: 'const foo = 10000',
 			options: [{number: {minimumDigits: 6}}]
 		},
 		{
-			code: 'var foo = 100_0000_0000',
+			code: 'const foo = 100_0000_0000',
 			options: [{number: {groupLength: 4}}]
 		},
 		{
-			code: 'var foo = 0xA_B_C_D_E_1_2_3_4',
+			code: 'const foo = 0xA_B_C_D_E_1_2_3_4',
 			options: [{hexadecimal: {groupLength: 1}}]
 		},
 		{
-			code: 'var foo = 0b111',
+			code: 'const foo = 0b111',
 			options: [{number: {minimumDigits: 3, groupLength: 1}}]
 		}
 	],
 	invalid: [
 		// Hexadecimal
 		{
-			code: 'var foo = 0xA_B_CDE_F0',
+			code: 'const foo = 0xA_B_CDE_F0',
 			errors: [error],
-			output: 'var foo = 0xA_BC_DE_F0'
+			output: 'const foo = 0xA_BC_DE_F0'
 		},
 		{
-			code: 'var foo = 0xABCDEF',
+			code: 'const foo = 0xABCDEF',
 			errors: [error],
-			output: 'var foo = 0xAB_CD_EF'
+			output: 'const foo = 0xAB_CD_EF'
 		},
 		{
-			code: 'var foo = 0xA_B',
+			code: 'const foo = 0xA_B',
 			errors: [error],
-			output: 'var foo = 0xAB'
+			output: 'const foo = 0xAB'
 		},
 		{
-			code: 'var foo = 0XAB_C_D',
+			code: 'const foo = 0XAB_C_D',
 			errors: [error],
-			output: 'var foo = 0XAB_CD'
+			output: 'const foo = 0XAB_CD'
 		},
 
 		// Octal
 		{
-			code: 'var foo = 0o12_34_5670',
+			code: 'const foo = 0o12_34_5670',
 			errors: [error],
-			output: 'var foo = 0o1234_5670'
+			output: 'const foo = 0o1234_5670'
 		},
 		{
-			code: 'var foo = 0o7_7_77',
+			code: 'const foo = 0o7_7_77',
 			errors: [error],
-			output: 'var foo = 0o7777'
+			output: 'const foo = 0o7777'
 		},
 		{
-			code: 'var foo = 0o010101010101',
+			code: 'const foo = 0o010101010101',
 			errors: [error],
-			output: 'var foo = 0o0101_0101_0101'
+			output: 'const foo = 0o0101_0101_0101'
 		},
 		{
-			code: 'var foo = 0O010101010101',
+			code: 'const foo = 0O010101010101',
 			errors: [error],
-			output: 'var foo = 0O0101_0101_0101'
+			output: 'const foo = 0O0101_0101_0101'
 		},
 
 		// Binary
 		{
-			code: 'var foo = 0b10_10_0001',
+			code: 'const foo = 0b10_10_0001',
 			errors: [error],
-			output: 'var foo = 0b1010_0001'
+			output: 'const foo = 0b1010_0001'
 		},
 		{
-			code: 'var foo = 0b0_00_0',
+			code: 'const foo = 0b0_00_0',
 			errors: [error],
-			output: 'var foo = 0b0000'
+			output: 'const foo = 0b0000'
 		},
 		{
-			code: 'var foo = 0b10101010101010',
+			code: 'const foo = 0b10101010101010',
 			errors: [error],
-			output: 'var foo = 0b10_1010_1010_1010'
+			output: 'const foo = 0b10_1010_1010_1010'
 		},
 		{
-			code: 'var foo = 0B10101010101010',
+			code: 'const foo = 0B10101010101010',
 			errors: [error],
-			output: 'var foo = 0B10_1010_1010_1010'
+			output: 'const foo = 0B10_1010_1010_1010'
 		},
 
 		// BigInt
 		{
-			code: 'var foo = 1_9_223n',
+			code: 'const foo = 1_9_223n',
 			errors: [error],
-			output: 'var foo = 19_223n'
+			output: 'const foo = 19_223n'
 		},
 		{
-			code: 'var foo = 80_7n',
+			code: 'const foo = 80_7n',
 			errors: [error],
-			output: 'var foo = 807n'
+			output: 'const foo = 807n'
 		},
 		{
-			code: 'var foo = 123456789_100n',
+			code: 'const foo = 123456789_100n',
 			errors: [error],
-			output: 'var foo = 123_456_789_100n'
+			output: 'const foo = 123_456_789_100n'
 		},
 
 		// Numbers
 		{
-			code: 'var foo = 1_2_345_678',
+			code: 'const foo = 1_2_345_678',
 			errors: [error],
-			output: 'var foo = 12_345_678'
+			output: 'const foo = 12_345_678'
 		},
 		{
-			code: 'var foo = 12_3',
+			code: 'const foo = 12_3',
 			errors: [error],
-			output: 'var foo = 123'
+			output: 'const foo = 123'
 		},
 		{
-			code: 'var foo = 1234567890',
+			code: 'const foo = 1234567890',
 			errors: [error],
-			output: 'var foo = 1_234_567_890'
+			output: 'const foo = 1_234_567_890'
 		},
 
 		// Decimal numbers
 		{
-			code: 'var foo = 9807.1234567',
+			code: 'const foo = 9807.1234567',
 			errors: [error],
-			output: 'var foo = 9807.123_456_7'
+			output: 'const foo = 9807.123_456_7'
 		},
 		{
-			code: 'var foo = 3819.123_4325',
+			code: 'const foo = 3819.123_4325',
 			errors: [error],
-			output: 'var foo = 3819.123_432_5'
+			output: 'const foo = 3819.123_432_5'
 		},
 		{
-			code: 'var foo = 138789.12343_2_42',
+			code: 'const foo = 138789.12343_2_42',
 			errors: [error],
-			output: 'var foo = 138_789.123_432_42'
+			output: 'const foo = 138_789.123_432_42'
 		},
 		{
-			code: 'var foo = .000000_1',
+			code: 'const foo = .000000_1',
 			errors: [error],
-			output: 'var foo = .000_000_1'
+			output: 'const foo = .000_000_1'
 		},
 		{
-			code: 'var foo = 12345678..toString()',
+			code: 'const foo = 12345678..toString()',
 			errors: [error],
-			output: 'var foo = 12_345_678..toString()'
+			output: 'const foo = 12_345_678..toString()'
 		},
 		{
-			code: 'var foo = 12345678 .toString()',
+			code: 'const foo = 12345678 .toString()',
 			errors: [error],
-			output: 'var foo = 12_345_678 .toString()'
+			output: 'const foo = 12_345_678 .toString()'
 		},
 		{
-			code: 'var foo = .00000',
+			code: 'const foo = .00000',
 			errors: [error],
-			output: 'var foo = .000_00'
+			output: 'const foo = .000_00'
 		},
 		{
-			code: 'var foo = 0.00000',
+			code: 'const foo = 0.00000',
 			errors: [error],
-			output: 'var foo = 0.000_00'
+			output: 'const foo = 0.000_00'
 		},
 
 		// Negative numbers
 		{
-			code: 'var foo = -100000_1',
+			code: 'const foo = -100000_1',
 			errors: [error],
-			output: 'var foo = -1_000_001'
+			output: 'const foo = -1_000_001'
 		},
 
 		// Exponential notation
 		{
-			code: 'var foo = 1e10000',
+			code: 'const foo = 1e10000',
 			errors: [error],
-			output: 'var foo = 1e10_000'
+			output: 'const foo = 1e10_000'
 		},
 		{
-			code: 'var foo = 39804e10000',
+			code: 'const foo = 39804e10000',
 			errors: [error],
-			output: 'var foo = 39_804e10_000'
+			output: 'const foo = 39_804e10_000'
 		},
 		{
-			code: 'var foo = -123456e100',
+			code: 'const foo = -123456e100',
 			errors: [error],
-			output: 'var foo = -123_456e100'
+			output: 'const foo = -123_456e100'
 		},
 		{
-			code: 'var foo = -100000e-10000',
+			code: 'const foo = -100000e-10000',
 			errors: [error],
-			output: 'var foo = -100_000e-10_000'
+			output: 'const foo = -100_000e-10_000'
 		},
 		{
-			code: 'var foo = -1000e+10000',
+			code: 'const foo = -1000e+10000',
 			errors: [error],
-			output: 'var foo = -1000e+10_000'
+			output: 'const foo = -1000e+10_000'
 		},
 		{
-			code: 'var foo = -1000e+00010000',
+			code: 'const foo = -1000e+00010000',
 			errors: [error],
-			output: 'var foo = -1000e+00_010_000'
+			output: 'const foo = -1000e+00_010_000'
 		},
 		{
-			code: 'var foo = 3.6e12000',
+			code: 'const foo = 3.6e12000',
 			errors: [error],
-			output: 'var foo = 3.6e12_000'
+			output: 'const foo = 3.6e12_000'
 		},
 		{
-			code: 'var foo = -1200000e5',
+			code: 'const foo = -1200000e5',
 			errors: [error],
-			output: 'var foo = -1_200_000e5'
+			output: 'const foo = -1_200_000e5'
 		},
 		{
-			code: 'var foo = 3.65432E12000',
+			code: 'const foo = 3.65432E12000',
 			errors: [error],
-			output: 'var foo = 3.654_32E12_000'
+			output: 'const foo = 3.654_32E12_000'
 		},
 
 		// Varying options
 		{
-			code: 'var foo = 1000000',
+			code: 'const foo = 1000000',
 			options: [{number: {minimumDigits: 6}}],
 			errors: [error],
-			output: 'var foo = 1_000_000'
+			output: 'const foo = 1_000_000'
 		},
 		{
-			code: 'var foo = 10_000_000_000',
+			code: 'const foo = 10_000_000_000',
 			options: [{number: {groupLength: 4}}],
 			errors: [error],
-			output: 'var foo = 100_0000_0000'
+			output: 'const foo = 100_0000_0000'
 		},
 		{
-			code: 'var foo = 0xA_B_CD',
+			code: 'const foo = 0xA_B_CD',
 			options: [{hexadecimal: {groupLength: 1}}],
 			errors: [error],
-			output: 'var foo = 0xA_B_C_D'
+			output: 'const foo = 0xA_B_C_D'
 		},
 		{
-			code: 'var foo = 0b1_11',
+			code: 'const foo = 0b1_11',
 			options: [{number: {minimumDigits: 3, groupLength: 2}}],
 			errors: [error],
-			output: 'var foo = 0b111'
+			output: 'const foo = 0b111'
 		}
 	]
 });
@@ -344,5 +344,5 @@ const visualizeTester = visualizeRuleTester(test, {
 
 visualizeTester.run('numeric-separators-style', rule, [
 	'console.log(0XdeEdBeeFn)',
-	'var foo = 12345678..toString()'
+	'const foo = 12345678..toString()'
 ])
