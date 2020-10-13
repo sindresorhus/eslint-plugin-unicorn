f28d5399c15fc015a00d7c009941557923420a0d
fisker <lionkay@gmail.com>
2020-10-13 17:25:03 +0800

    Revert "Simplify logic"
    This reverts commit 404860c7b235fa0acb7c2db76bab40f4fc35df06.

--- rules/utils/extend-fix-range.js
+++ rules/utils/extend-fix-range.js
@@ -3,11 +3,13 @@
 /**
 Extend fix range to prevent changes from other rules
 https://github.com/eslint/eslint/pull/13748/files#diff-c692f3fde09eda7c89f1802c908511a3fb59f5d207fe95eb009cb52e46a99e84R348
+
+@param {ruleFixer} fixer The fixer to fix.
+@param {int[]} node The extended range node.
 */
-function * extendFixRange(range = []) {
-	const [start = 0, end = Infinity] = range;
-	yield {range: [start, start], text: ''};
-	yield {range: [end, end], text: ''};
+function * extendFixRange(fixer, range) {
+	yield fixer.insertTextBeforeRange(range, '');
+	yield fixer.insertTextAfterRange(range, '');
 }
 
 module.exports = extendFixRange;
