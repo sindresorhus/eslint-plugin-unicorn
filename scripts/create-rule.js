042e7388211bb57d245e4ae95517a9d062279eb0
fisker <lionkay@gmail.com>
2020-10-20 10:20:52 +0800

    Use cjs

--- scripts/create-rule.js
+++ scripts/create-rule.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import fs from "fs";
-import path from "path";
-import url from "url";
-import enquirer from "enquirer";
-import lodash from "lodash";
+'use strict';
 
-const DIRNAME = path.dirname(url.fileURLToPath(import.meta.url));
-const ROOT = path.join(DIRNAME, "..");
+const fs = require('fs');
+const path = require('path');
+const enquirer = require('enquirer');
+const {template} = require('lodash');
+
+const ROOT = path.join(__dirname, '..');
 
 function checkFiles(ruleId) {
 	const files = [
@@ -17,33 +17,33 @@ function checkFiles(ruleId) {
 
 	for (const file of files) {
 		if (fs.existsSync(path.join(ROOT, file))) {
-			throw new Error(`"${file}" already exists.`);
+			throw new Error(`“${file}” already exists.`);
 		}
 	}
 }
 
 function renderTemplate({ source, target, data }) {
-	const templateFile = path.join(DIRNAME, `template/${source}`);
+	const templateFile = path.join(__dirname, `template/${source}`);
 	const targetFile = path.join(ROOT, target);
-	const templateContent = fs.readFileSync(templateFile, "utf8")
+	const templateContent = fs.readFileSync(templateFile, 'utf8')
 
-	const compiled = lodash.template(templateContent);
+	const compiled = template(templateContent);
 	const content = compiled(data);
 	return fs.writeFileSync(targetFile, content);
 }
 
 function updateIndex(id) {
-	const RULE_START = "\t\t\trules: {\n";
-	const RULE_END = "\n\t\t\t}";
-	const RULE_INDENT = "\t".repeat(4);
+	const RULE_START = '\t\t\trules: {\n';
+	const RULE_END = '\n\t\t\t}';
+	const RULE_INDENT = '\t'.repeat(4);
 	let ruleContent = `${RULE_INDENT}'unicorn/${id}': 'error',`;
 
-	const file = path.join(ROOT, "index.js");
-	const content = fs.readFileSync(file, "utf8");
+	const file = path.join(ROOT, 'index.js');
+	const content = fs.readFileSync(file, 'utf8');
 	const [before, rest] = content.split(RULE_START);
 	const [rules, after] = rest.split(RULE_END);
 
-	let lines = rules.split("\n");
+	let lines = rules.split('\n');
 	if (!lines.every((line) => line.startsWith(RULE_INDENT))) {
 		throw 'Unexpected content in “index.js”.';
 	}
@@ -69,21 +69,21 @@ function updateIndex(id) {
 }
 
 function updateReadmeUsage({id}) {
-	const RULE_START = "\t\t\"rules\": {\n";
-	const RULE_END = "\n\t\t}";
-	const RULE_INDENT = "\t".repeat(3);
+	const RULE_START = '\t\t"rules": {\n';
+	const RULE_END = '\n\t\t}';
+	const RULE_INDENT = '\t'.repeat(3);
 	let ruleContent = `${RULE_INDENT}"unicorn/${id}": "error",`;
 
-	const file = path.join(ROOT, "readme.md");
-	const content = fs.readFileSync(file, "utf8");
+	const file = path.join(ROOT, 'readme.md');
+	const content = fs.readFileSync(file, 'utf8');
 	const [before, rest] = content.split(RULE_START);
 	const [rules, after] = rest.split(RULE_END);
 
-	let lines = rules.split("\n");
+	let lines = rules.split('\n');
 	if (!lines.every((line) => line.startsWith(RULE_INDENT))) {
-		throw 'Unexpected content in "index.js".';
+		throw 'Unexpected content in “readme.md”.';
 	}
-	const unicornRuleLines = lines.filter(line => line.startsWith(`${RULE_INDENT}"unicorn/`));
+	const unicornRuleLines = lines.filter(line => line.startsWith(`${RULE_INDENT}unicorn/`));
 	let insertIndex;
 	if (ruleContent.localeCompare(unicornRuleLines[0]) === -1) {
 		insertIndex = 0;
@@ -105,8 +105,8 @@ function updateReadmeUsage({id}) {
 }
 
 function updateReadmeRules(data) {
-	const RULE_START = "## Rules\n\n";
-	const RULE_END = "\n\n## Deprecated Rules";
+	const RULE_START = '## Rules\n\n';
+	const RULE_END = '\n\n## Deprecated Rules';
 	let ruleContent = `- [${data.id}](docs/rules/${data.id}.md) - ${data.description}`;
 	if (data.fixable === true) {
 		ruleContent += ' *(fixable)*';
@@ -114,12 +114,12 @@ function updateReadmeRules(data) {
 		ruleContent += ' *(partly fixable)*';
 	}
 
-	const file = path.join(ROOT, "readme.md");
-	const content = fs.readFileSync(file, "utf8");
+	const file = path.join(ROOT, 'readme.md');
+	const content = fs.readFileSync(file, 'utf8');
 	const [before, rest] = content.split(RULE_START);
 	const [rules, after] = rest.split(RULE_END);
 
-	const lines = rules.split("\n");
+	const lines = rules.split('\n');
 	let insertIndex;
 	if (ruleContent.localeCompare(lines[0]) === -1) {
 		insertIndex = 0;
@@ -143,60 +143,60 @@ function updateReadme(data) {
 (async () => {
 	const data = await enquirer.prompt([
 		{
-			type: "input",
-			name: "id",
-			message: "Rule name:",
+			type: 'input',
+			name: 'id',
+			message: 'Rule name:',
 			validate(value) {
 				if (!value) {
-					return "Rule name is required.";
+					return 'Rule name is required.';
 				}
 
 				if (!/^[a-z-]+$/.test(value)) {
-					return "Invalid rule name.";
+					return 'Invalid rule name.';
 				}
 
 				return true;
 			}
 		},
 		{
-			type: "input",
-			name: "description",
-			message: "Rule description:",
+			type: 'input',
+			name: 'description',
+			message: 'Rule description:',
 			validate(value) {
 				if (!value) {
-					return "Rule description is required.";
+					return 'Rule description is required.';
 				}
 
 				return true;
 			}
 		},
 		{
-			type: "select",
-			name: "fixable",
-			message: "Is it fixable?",
+			type: 'select',
+			name: 'fixable',
+			message: 'Is it fixable?',
 			choices: [
 				{
 					name: true,
-					message: "No"
+					message: 'Yes'
 				},
 				{
-					name: true,
-					message: "Yes"
+					name: 'partly',
+					name: 'Partly'
 				},
 				{
-					name: "partly",
-					name: "Partly"
+					name: false,
+					message: 'No'
 				}
 			]
 		},
 		{
-			type: "select",
-			name: "type",
-			message: "Type:",
+			type: 'select',
+			name: 'type',
+			message: 'Type:',
 			choices: [
-				"problem",
-				"suggestion",
-				"layout"
+				'problem',
+				'suggestion',
+				'layout'
 			]
 		},
 	]);
