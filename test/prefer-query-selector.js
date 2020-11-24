f0eadfbdd1cbc3758e1f93a98d9de2a2bc5380df
fisker <lionkay@gmail.com>
2020-11-24 17:07:33 +0800

    Refactor tests to only use visual test

--- test/prefer-query-selector.js
+++ test/prefer-query-selector.js
@@ -19,112 +19,30 @@ test({
 		'document.querySelectorAll("li a");',
 		'document.querySelector("li").querySelectorAll("a");'
 	],
-	invalid: [
-		{
-			code: 'document.getElementById("foo");',
-			errors: [createError('getElementById', 'querySelector')],
-			output: 'document.querySelector("#foo");'
-		},
-		{
-			code: 'document.getElementsByClassName("foo");',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')],
-			output: 'document.querySelectorAll(".foo");'
-		},
-		{
-			code: 'document.getElementsByClassName("foo bar");',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')],
-			output: 'document.querySelectorAll(".foo.bar");'
-		},
-		{
-			code: 'document.getElementsByTagName("foo");',
-			errors: [createError('getElementsByTagName', 'querySelectorAll')],
-			output: 'document.querySelectorAll("foo");'
-		},
-		{
-			code: 'document.getElementById("");',
-			errors: [createError('getElementById', 'querySelector')]
-		},
-		{
-			code: 'document.getElementById(\'foo\');',
-			errors: [createError('getElementById', 'querySelector')],
-			output: 'document.querySelector(\'#foo\');'
-		},
-		{
-			code: 'document.getElementsByClassName(\'foo\');',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')],
-			output: 'document.querySelectorAll(\'.foo\');'
-		},
-		{
-			code: 'document.getElementsByClassName(\'foo bar\');',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')],
-			output: 'document.querySelectorAll(\'.foo.bar\');'
-		},
-		{
-			code: 'document.getElementsByTagName(\'foo\');',
-			errors: [createError('getElementsByTagName', 'querySelectorAll')],
-			output: 'document.querySelectorAll(\'foo\');'
-		},
-		{
-			code: 'document.getElementsByClassName(\'\');',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')]
-		},
-		{
-			code: 'document.getElementById(`foo`);',
-			errors: [createError('getElementById', 'querySelector')],
-			output: 'document.querySelector(`#foo`);'
-		},
-		{
-			code: 'document.getElementsByClassName(`foo`);',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')],
-			output: 'document.querySelectorAll(`.foo`);'
-		},
-		{
-			code: 'document.getElementsByClassName(`foo bar`);',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')],
-			output: 'document.querySelectorAll(`.foo.bar`);'
-		},
-		{
-			code: 'document.getElementsByTagName(`foo`);',
-			errors: [createError('getElementsByTagName', 'querySelectorAll')],
-			output: 'document.querySelectorAll(`foo`);'
-		},
-		{
-			code: 'document.getElementsByTagName(``);',
-			errors: [createError('getElementsByTagName', 'querySelectorAll')]
-		},
-		{
-			code: 'document.getElementsByClassName(`${fn()}`);', // eslint-disable-line no-template-curly-in-string
-			errors: [createError('getElementsByClassName', 'querySelectorAll')]
-		},
-		{
-			code: 'document.getElementsByClassName(`foo ${undefined}`);', // eslint-disable-line no-template-curly-in-string
-			errors: [createError('getElementsByClassName', 'querySelectorAll')]
-		},
-		{
-			code: 'document.getElementsByClassName(null);',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')],
-			output: 'document.querySelectorAll(null);'
-		},
-		{
-			code: 'document.getElementsByTagName(null);',
-			errors: [createError('getElementsByTagName', 'querySelectorAll')],
-			output: 'document.querySelectorAll(null);'
-		},
-		{
-			code: 'document.getElementsByClassName(fn());',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')]
-		},
-		{
-			code: 'document.getElementsByClassName("foo" + fn());',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')]
-		},
-		{
-			code: 'document.getElementsByClassName(foo + "bar");',
-			errors: [createError('getElementsByClassName', 'querySelectorAll')]
-		}
-	]
+	invalid: []
 });
 
 test.visualize([
-	'document.getElementById("foo");'
+	'document.getElementById("foo");',
+	'document.getElementsByClassName("foo");',
+	'document.getElementsByClassName("foo bar");',
+	'document.getElementsByTagName("foo");',
+	'document.getElementById("");',
+	'document.getElementById(\'foo\');',
+	'document.getElementsByClassName(\'foo\');',
+	'document.getElementsByClassName(\'foo bar\');',
+	'document.getElementsByTagName(\'foo\');',
+	'document.getElementsByClassName(\'\');',
+	'document.getElementById(`foo`);',
+	'document.getElementsByClassName(`foo`);',
+	'document.getElementsByClassName(`foo bar`);',
+	'document.getElementsByTagName(`foo`);',
+	'document.getElementsByTagName(``);',
+	'document.getElementsByClassName(`${fn()}`);', // eslint-disable-line no-template-curly-in-string
+	'document.getElementsByClassName(`foo ${undefined}`);', // eslint-disable-line no-template-curly-in-string
+	'document.getElementsByClassName(null);',
+	'document.getElementsByTagName(null);',
+	'document.getElementsByClassName(fn());',
+	'document.getElementsByClassName("foo" + fn());',
+	'document.getElementsByClassName(foo + "bar");'
 ]);
