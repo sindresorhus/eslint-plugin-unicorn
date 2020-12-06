416e3a4086dfeb4ba79dc4c25ff9eb6a7ed08e00
Fabrice TIERCELIN <fabrice.tiercelin@gmail.com>
2020-12-06 19:47:37 +0100

    Add tests

--- test/prefer-indexof.js
+++ test/prefer-indexof.js
@@ -14,10 +14,19 @@ test({
 		'const substr = foo.substr',
 		'const findIndex = foo.findIndex',
 
-		'["foo", "bar"].indexOf()',
-		'["foo", "bar"].indexOf(0)',
-		'["foo", "bar"].indexOf(x => x === "foo", 2)',
-		'["foo", "bar"].indexOf(x => x === "foo", -2)'
+		'["foo", "bar"].findIndex()',
+		'["foo", "bar"].findIndex(0)',
+		'["foo", "bar"].findIndex(myFunction)',
+		'["foo", "bar"].findIndex(x => y === "foo")',
+		'["foo", "bar"].findIndex(x => y.x === "foo")',
+		'["foo", "bar"].findIndex(x => x + "foo" === "foo" + x)',
+		'["foo", "bar"].findIndex((x, i) => x === i)',
+		'["foo", "bar"].findIndex(x => x !== "foo")',
+		'["foo", "bar"].findIndex(x => x.includes("foo"))',
+		'["foo", "bar"].findIndex(x => x === "foo", 2)',
+		'["foo", "bar"].findIndex(x => {console.log("Hi!");return x === "foo";})',
+
+		'["foo", "bar"].indexOf(0)'
 	],
 
 	invalid: [
