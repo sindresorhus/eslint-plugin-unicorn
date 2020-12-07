0fae26cbd9999f85bb16b801fd871634a1d0e8b4
fisker <lionkay@gmail.com>
2020-12-07 14:14:07 +0800

    Rewrite `error-message`

--- rules/error-message.js
+++ rules/error-message.js
@@ -8,7 +8,7 @@ const messages = {
 	[MESSAGE_ID_EMPTY_MESSAGE]: 'Error message should not be an empty string.'
 };
 
-const errorConstructors = new Set([
+const errorConstructors = [
 	'Error',
 	'EvalError',
 	'InternalError',
@@ -17,84 +17,39 @@ const errorConstructors = new Set([
 	'SyntaxError',
 	'TypeError',
 	'URIError'
-]);
+];
 
-const isReferenceAssigned = expression => {
-	if (expression.type === 'AssignmentExpression') {
-		const assignedVariable = expression.left;
-		return assignedVariable.type === 'Identifier' && assignedVariable.name;
-	}
-
-	return false;
-};
-
-const findIdentifierValues = (identifierNode, context) => {
-	const scope = context.getScope(identifierNode);
-	const declarations = scope.set.get(identifierNode.name);
-	if (declarations === undefined) {
-		return [];
-	}
-
-	const expressions = declarations.references.map(reference => reference.identifier.parent);
-	const referenceValues = [];
-	for (const expression of expressions) {
-		if (isReferenceAssigned(expression)) {
-			referenceValues.push(expression.right);
-		} else if (expression.type === 'VariableDeclarator') {
-			referenceValues.push(expression.init);
-		}
-	}
-
-	return referenceValues;
-};
+const selector = [
+	':matches(NewExpression, CallExpression)',
+	'[callee.type="Identifier"]',
+	`:matches(${errorConstructors.map(name => `[callee.name="${name}"]`).join(', ')})`
+].join('');
 
 const isEmptyMessageString = node => {
 	return (
-		node.arguments.length > 0 &&
-		node.arguments[0].type === 'Literal' &&
-		!node.arguments[0].value
+		node.type === 'Literal' &&
+		!node.value
 	);
-};
-
-const reportError = (expressionNode, context) => {
-	const error = expressionNode.callee;
-	if (errorConstructors.has(error.name)) {
-		if (expressionNode.arguments.length === 0) {
-			context.report({
-				node: expressionNode.parent,
-				messageId: MESSAGE_ID_MISSING_MESSAGE
-			});
-		}
-
-		if (isEmptyMessageString(expressionNode)) {
-			context.report({
-				node: expressionNode.parent,
-				messageId: MESSAGE_ID_EMPTY_MESSAGE
-			});
-		}
-	}
-};
-
-const checkErrorMessage = (node, context) => {
-	if (node.type === 'Identifier') {
-		const identifierValues = findIdentifierValues(node, context);
-		for (const node of identifierValues) {
-			checkErrorMessage(node, context);
-		}
-	} else if (node.type === 'NewExpression' || node.type === 'CallExpression') {
-		reportError(node, context);
-	}
 };
 
 const create = context => {
 	const throwStatements = [];
 	return {
-		'ThrowStatement'(throwStatement) {
-			throwStatements.push(throwStatement);
-		},
-		'Program:exit'() {
-			for (const throwStatement of throwStatements) {
-				checkErrorMessage(throwStatement.argument, context);
+		[selector](node) {
+			if (node.arguments.length === 0) {
+				context.report({
+					node,
+					messageId: MESSAGE_ID_MISSING_MESSAGE
+				});
+				return;
+			}
+
+			const [message] = node.arguments;
+			if (isEmptyMessageString(message)) {
+				context.report({
+					node: message,
+					messageId: MESSAGE_ID_EMPTY_MESSAGE
+				});
 			}
 		}
 	};
