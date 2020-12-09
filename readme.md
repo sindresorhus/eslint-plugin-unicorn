c9410d4c410ce782d2947b8b29b098b16cf284a9
fisker Cheung <lionkay@gmail.com>
2020-12-09 20:29:58 +0800

    Rewrite `error-message` rule (#922)

--- readme.md
+++ readme.md
@@ -104,7 +104,7 @@ Configure it in `package.json`.
 - [catch-error-name](docs/rules/catch-error-name.md) - Enforce a specific parameter name in catch clauses. *(fixable)*
 - [consistent-function-scoping](docs/rules/consistent-function-scoping.md) - Move function definitions to the highest possible scope.
 - [custom-error-definition](docs/rules/custom-error-definition.md) - Enforce correct `Error` subclassing. *(fixable)*
-- [error-message](docs/rules/error-message.md) - Enforce passing a `message` value when throwing a built-in error.
+- [error-message](docs/rules/error-message.md) - Enforce passing a `message` value when creating a built-in error.
 - [escape-case](docs/rules/escape-case.md) - Require escape sequences to use uppercase values. *(fixable)*
 - [expiring-todo-comments](docs/rules/expiring-todo-comments.md) - Add expiration conditions to TODO comments.
 - [explicit-length-check](docs/rules/explicit-length-check.md) - Enforce explicitly comparing the `length` property of a value. *(partly fixable)*
--- rules/error-message.js
+++ rules/error-message.js
@@ -1,14 +1,17 @@
 'use strict';
+const {getStaticValue} = require('eslint-utils');
 const getDocumentationUrl = require('./utils/get-documentation-url');
 
 const MESSAGE_ID_MISSING_MESSAGE = 'constructorMissingMessage';
 const MESSAGE_ID_EMPTY_MESSAGE = 'emptyMessage';
+const MESSAGE_ID_NOT_STRING = 'message-is-not-a-string';
 const messages = {
-	[MESSAGE_ID_MISSING_MESSAGE]: 'Pass a message to the error constructor.',
-	[MESSAGE_ID_EMPTY_MESSAGE]: 'Error message should not be an empty string.'
+	[MESSAGE_ID_MISSING_MESSAGE]: 'Pass a message to the `{{constructor}}` constructor.',
+	[MESSAGE_ID_EMPTY_MESSAGE]: 'Error message should not be an empty string.',
+	[MESSAGE_ID_NOT_STRING]: 'Error message should be a string.'
 };
 
-const errorConstructors = new Set([
+const errorConstructors = [
 	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
 	'Error',
 	'EvalError',
@@ -18,84 +21,61 @@ const errorConstructors = new Set([
 	'TypeError',
 	'URIError',
 	'InternalError'
-]);
+];
 
-const isReferenceAssigned = expression => {
-	if (expression.type === 'AssignmentExpression') {
-		const assignedVariable = expression.left;
-		return assignedVariable.type === 'Identifier' && assignedVariable.name;
-	}
-
-	return false;
-};
-
-const findIdentifierValues = (identifierNode, context) => {
-	const scope = context.getScope(identifierNode);
-	const declarations = scope.set.get(identifierNode.name);
-	if (declarations === undefined) {
-		return [];
-	}
-
-	const expressions = declarations.references.map(reference => reference.identifier.parent);
-	const referenceValues = [];
-	for (const expression of expressions) {
-		if (isReferenceAssigned(expression)) {
-			referenceValues.push(expression.right);
-		} else if (expression.type === 'VariableDeclarator') {
-			referenceValues.push(expression.init);
-		}
-	}
-
-	return referenceValues;
-};
-
-const isEmptyMessageString = node => {
-	return (
-		node.arguments.length > 0 &&
-		node.arguments[0].type === 'Literal' &&
-		!node.arguments[0].value
-	);
-};
-
-const reportError = (expressionNode, context) => {
-	const error = expressionNode.callee;
-	if (errorConstructors.has(error.name)) {
-		if (expressionNode.arguments.length === 0) {
-			context.report({
-				node: expressionNode.parent,
-				messageId: MESSAGE_ID_MISSING_MESSAGE
-			});
-		}
-
-		if (isEmptyMessageString(expressionNode)) {
-			context.report({
-				node: expressionNode.parent,
-				messageId: MESSAGE_ID_EMPTY_MESSAGE
-			});
-		}
-	}
-};
-
-const checkErrorMessage = (node, context) => {
-	if (node.type === 'Identifier') {
-		const identifierValues = findIdentifierValues(node, context);
-		for (const node of identifierValues) {
-			checkErrorMessage(node, context);
-		}
-	} else if (node.type === 'NewExpression' || node.type === 'CallExpression') {
-		reportError(node, context);
-	}
-};
+const selector = [
+	':matches(NewExpression, CallExpression)',
+	'[callee.type="Identifier"]',
+	`:matches(${errorConstructors.map(name => `[callee.name="${name}"]`).join(', ')})`
+].join('');
+const noArgumentsExpressionSelector = `${selector}[arguments.length=0]`;
+const errorMessageSelector = `${selector}[arguments.length>0]`;
 
 const create = context => {
-	const throwStatements = [];
 	return {
-		'ThrowStatement'(throwStatement) {
-			throwStatements.push(throwStatement);
+		[noArgumentsExpressionSelector](node) {
+			context.report({
+				node,
+				messageId: MESSAGE_ID_MISSING_MESSAGE,
+				data: {
+					constructor: node.callee.name
+				}
+			});
 		},
-		'Program:exit'() {
-			for (const throwStatement of throwStatements) {
-				checkErrorMessage(throwStatement.argument, context);
+		[errorMessageSelector](expression) {
+			const [node] = expression.arguments;
+
+			// These types can't be string, and `getStaticValue` may don't know the value
+			// Add more types, if issue reported
+			if (node.type === 'ArrayExpression' || node.type === 'ObjectExpression') {
+				context.report({
+					node,
+					messageId: MESSAGE_ID_NOT_STRING
+				});
+				return;
+			}
+
+			const result = getStaticValue(node, context.getScope());
+
+			// We don't know the value of `message`
+			if (!result) {
+				return;
+			}
+
+			const {value} = result;
+			if (typeof value !== 'string') {
+				context.report({
+					node,
+					messageId: MESSAGE_ID_NOT_STRING
+				});
+				return;
+			}
+
+			if (value === '') {
+				context.report({
+					node,
+					messageId: MESSAGE_ID_EMPTY_MESSAGE
+				});
 			}
 		}
 	};
