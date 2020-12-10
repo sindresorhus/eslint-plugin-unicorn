85c559e6ce5ace3f40da7531ad0b14795baa0a84
fisker <lionkay@gmail.com>
2020-12-10 16:44:20 +0800

    Add many more test

--- test/prefer-array-index-of.js
+++ test/prefer-array-index-of.js
@@ -13,59 +13,65 @@ test({
 	valid: [
 		'const findIndex = foo.findIndex',
 
-		// No argument
-		'[\'foo\', \'bar\'].findIndex()',
-
-		// Too many arguments
-		'[\'foo\', \'bar\'].findIndex(x => x === \'foo\', 2)',
-
+		// Test `foo.findIndex`
+		// More/less argument(s)
+		'foo.findIndex()',
+		'foo.findIndex(function (x) {return x === 1;}, bar)',
+		'foo.findIndex(...[function (x) {return x === 1;}])',
 		// Not `CallExpression`
-		'new foo.findIndex(x => x === \'foo\');',
-
+		'new foo.findIndex(x => x === 1)',
 		// Not `MemberExpression`
-		'findIndex(x => x === \'foo\');',
-
+		'findIndex(x => x === 1)',
 		// `callee.property` is not a `Identifier`
-		'foo[\'findIndex\'](x => x === \'foo\');',
-
+		'foo["findIndex"](x => x === 1)',
 		// Computed
-		'foo[findIndex](x => x === \'foo\');',
+		'foo[findIndex](x => x === 1)',
+		// Not `findIndex`
+		'foo.notListedMethod(x => x === 1)',
 
-		// Not listed method
-		'foo.notListedMethod(x => x === \'foo\');',
+		// Test `callback` part
+		// Not function
+		'foo.findIndex(myFunction)',
+		// Not one parameter
+		'foo.findIndex((x, i) => x === i)',
+		'foo.findIndex((x, i) => {return x === i})',
+		'foo.findIndex(function(x, i) {return x === i})',
+		// Parameter is not `Identifier`
+		'foo.findIndex(({x}) => x === 1)',
+		'foo.findIndex(({x}) => {return x === 1})',
+		'foo.findIndex(function({x}) {return x === 1})',
 
-		// Wrong argument
-		'[\'foo\', \'bar\'].findIndex(0)',
+		// Test `callback` body
+		// Not only `return`
+		'foo.findIndex(({x}) => {noop();return x === 1})',
+		// Not `return`
+		'foo.findIndex(({x}) => {bar(x === 1)})',
 
-		// Unknown argument
-		'[\'foo\', \'bar\'].findIndex(myFunction)',
+		// Test `BinaryExpression`
+		// Not `BinaryExpression`
+		'foo.findIndex(x => x - 1)',
+		'foo.findIndex(x => {return x - 1})',
+		'foo.findIndex(function (x){return x - 1})',
+		// Not `===`
+		'foo.findIndex(x => x !== 1)',
+		'foo.findIndex(x => {return x !== 1})',
+		'foo.findIndex(function (x){return x !== 1})',
+		// Neither `left` nor `right` is Identifier
+		'foo.findIndex(x => 1 === 1.0)',
+		'foo.findIndex(x => {return 1 === 1.0})',
+		'foo.findIndex(function (x){return 1 === 1.0})',
 
 		// Not the same identifier
-		'[\'foo\', \'bar\'].findIndex(x => y === \'foo\')',
-
-		// Member instead of identifier
-		'[\'foo\', \'bar\'].findIndex(x => y.x === \'foo\')',
+		'foo.findIndex(x => y === 1)',
 
 		// Dynamical value
-		'[\'foo\', \'bar\'].findIndex(x => x + \'foo\' === \'foo\' + x)',
+		'foo.findIndex(x => x + "foo" === "foo" + x)',
 
-		// Changing value
-		'[\'foofoo\', \'foobar\'].findIndex(x => x === \'foo\' + x)',
-
-		// Other dynamical value
-		'[\'foo\', \'bar\'].findIndex((x, i) => x === i)',
-
-		// Wrong operator
-		'[\'foo\', \'bar\'].findIndex(x => x !== \'foo\')',
-
-		// Wrong condition
-		'[\'foo\', \'bar\'].findIndex(x => x.includes(\'foo\'))',
-
-		// Too many statements
-		'[\'foo\', \'bar\'].findIndex(x => {console.log(\'Hi!\');return x === \'foo\';})',
+		// `x` is used
+		'foo.findIndex(x => x === "foo" + x)',
 
 		// Already valid case
-		'[\'foo\', \'bar\'].indexOf(0)'
+		'foo.indexOf(0)'
 	],
 
 	invalid: [
