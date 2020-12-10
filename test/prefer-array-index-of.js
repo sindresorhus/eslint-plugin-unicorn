21300ba85e522674e0953ab31e3d4a45439bb62e
Fabrice TIERCELIN <fabrice.tiercelin@gmail.com>
2020-12-10 06:45:52 +0100

    Add more cases

--- test/prefer-array-index-of.js
+++ test/prefer-array-index-of.js
@@ -11,22 +11,58 @@ const errorsFindIndex = [
 
 test({
 	valid: [
-		'const substr = foo.substr',
 		'const findIndex = foo.findIndex',
 
-		'["foo", "bar"].findIndex()',
-		'["foo", "bar"].findIndex(0)',
-		'["foo", "bar"].findIndex(myFunction)',
-		'["foo", "bar"].findIndex(x => y === "foo")',
-		'["foo", "bar"].findIndex(x => y.x === "foo")',
-		'["foo", "bar"].findIndex(x => x + "foo" === "foo" + x)',
-		'["foo", "bar"].findIndex((x, i) => x === i)',
-		'["foo", "bar"].findIndex(x => x !== "foo")',
-		'["foo", "bar"].findIndex(x => x.includes("foo"))',
-		'["foo", "bar"].findIndex(x => x === "foo", 2)',
-		'["foo", "bar"].findIndex(x => {console.log("Hi!");return x === "foo";})',
+		// No argument
+		'[\'foo\', \'bar\'].findIndex()',
 
-		'["foo", "bar"].indexOf(0)'
+		// Too many arguments
+		'[\'foo\', \'bar\'].findIndex(x => x === \'foo\', 2)',
+
+		// Not `CallExpression`
+		'new foo.findIndex(x => x === \'foo\');',
+
+		// Not `MemberExpression`
+		'findIndex(x => x === \'foo\');',
+
+		// `callee.property` is not a `Identifier`
+		'foo[\'findIndex\'](x => x === \'foo\');',
+
+		// Computed
+		'foo[findIndex](x => x === \'foo\');',
+
+		// Not listed method
+		'foo.notListedMethod(x => x === \'foo\');',
+
+		// Wrong argument
+		'[\'foo\', \'bar\'].findIndex(0)',
+
+		// Unknown argument
+		'[\'foo\', \'bar\'].findIndex(myFunction)',
+
+		// Not the same identifier
+		'[\'foo\', \'bar\'].findIndex(x => y === \'foo\')',
+
+		// Member instead of identifier
+		'[\'foo\', \'bar\'].findIndex(x => y.x === \'foo\')',
+
+		// Dynamical value
+		'[\'foo\', \'bar\'].findIndex(x => x + \'foo\' === \'foo\' + x)',
+
+		// Other dynamical value
+		'[\'foo\', \'bar\'].findIndex((x, i) => x === i)',
+
+		// Wrong operator
+		'[\'foo\', \'bar\'].findIndex(x => x !== \'foo\')',
+
+		// Wrong condition
+		'[\'foo\', \'bar\'].findIndex(x => x.includes(\'foo\'))',
+
+		// Too many statements
+		'[\'foo\', \'bar\'].findIndex(x => {console.log(\'Hi!\');return x === \'foo\';})',
+
+		// Already valid case
+		'[\'foo\', \'bar\'].indexOf(0)'
 	],
 
 	invalid: [
