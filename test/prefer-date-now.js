77b221727e3ef85d2bfeec48170b6dae82ea0d1f
fisker <lionkay@gmail.com>
2020-12-11 12:10:33 +0800

    Fix tests

--- test/prefer-date-now.js
+++ test/prefer-date-now.js
@@ -18,8 +18,8 @@ test.visualize([
 	'const ts = (new Date()).valueOf();',
 
 	// Constructor
-	'const ts = /* 1 */ Number(/* 2 */ new / * 3 */ Date( /* 4 */ ) /* 5 */) /* 6 */',
-	'const tsBigInt = /* 1 */ BigInt(/* 2 */ new / * 3 */ Date( /* 4 */ ) /* 5 */) /* 6 */',
+	'const ts = /* 1 */ Number(/* 2 */ new /* 3 */ Date( /* 4 */ ) /* 5 */) /* 6 */',
+	'const tsBigInt = /* 1 */ BigInt(/* 2 */ new /* 3 */ Date( /* 4 */ ) /* 5 */) /* 6 */',
 
 	// `UnaryExpression`
 	'const ts = +new Date;',
