47a0a62203e2d8dfa432eca3825ff8967ecdb5dd
fisker <lionkay@gmail.com>
2020-12-11 10:51:02 +0800

    Implement

--- rules/prefer-date-now.js
+++ rules/prefer-date-now.js
@@ -1,26 +1,55 @@
 'use strict';
 const getDocumentationUrl = require('./utils/get-documentation-url');
+const methodSelector = require('./utils/method-selector');
 
-const MESSAGE_ID = 'prefer-date-now';
+const MESSAGE_ID_METHOD = 'prefer-date-now-over-methods';
+const MESSAGE_ID_NUMBER = 'prefer-date-now-over-number-data-object';
 const messages = {
-	[MESSAGE_ID]: 'Prefer `{{replacement}}` over `{{value}}`.'
+	[MESSAGE_ID_METHOD]: 'Prefer `Date.now()` over `Date#{{name}}()`.',
+	[MESSAGE_ID_NUMBER]: 'Prefer `Date.now()` over `Number(new Date())`.'
 };
+
+const createNewDateSelector = path => {
+	const prefix = property ? `${property}.` : '';
+	return [
+		`[${prefix}type="NewExpression"]`,
+		`[${prefix}callee.type="Identifier"]`,
+		`[${prefix}callee.name="Date"]`,
+		`[${prefix}callee.arguments.length=0]`
+	].join('');
+};
+const methodsSelector = [
+	methodSelector({
+		names: ['getTime', 'valueOf'],
+		length: 0
+	}),
+	createNewDateSelector('callee.object'),
+].join('');
+const numberSelector = [
+	'CallExpression',
+	'[callee.type="Identifier"]',
+	'[callee.name="Number"]',
+	'[arguments.length=1]',
+	createNewDateSelector('arguments.0')
+].join('');
+
 
 const create = context => {
 	return {
-		Literal(node) {
-			if (node.value !== 'unicorn') {
-				return;
-			}
-
+		[methodsSelector](node) {
+			const method = node.callee.property;
+			context.report({
+				node: method,
+				messageId: MESSAGE_ID_METHOD,
+				data: {method: method.name},
+				fix: fixer => fixer.replaceText(node, 'Date.now()')
+			});
+		}
+		[numberSelector](node) {
 			context.report({
 				node,
-				messageId: MESSAGE_ID,
-				data: {
-					value: 'unicorn',
-					replacement: '🦄'
-				},
-				fix: fixer => fixer.replaceText(node, '\'🦄\'')
+				messageId: MESSAGE_ID_NUMBER,
+				fix: fixer => fixer.replaceText(node, 'Date.now()')
 			});
 		}
 	}
