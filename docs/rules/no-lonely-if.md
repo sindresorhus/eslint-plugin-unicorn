383979f1fc45792c372c018d8cab65c56258f8f0
fisker <lionkay@gmail.com>
2020-12-14 09:48:37 +0800

    Run `create-rule`

---
+++ docs/rules/no-lonely-if.md
@@ -0,0 +1,17 @@
+# Disallow `if` statements as the only statement in `if` blocks without `else`.
+
+<!-- More detailed description. Remove this comment. -->
+
+This rule is fixable.
+
+## Fail
+
+```js
+const foo = 'unicorn';
+```
+
+## Pass
+
+```js
+const foo = '🦄';
+```
---
+++ rules/no-lonely-if.js
@@ -0,0 +1,42 @@
+'use strict';
+const getDocumentationUrl = require('./utils/get-documentation-url');
+
+const MESSAGE_ID = 'no-lonely-if';
+const messages = {
+	[MESSAGE_ID]: 'Prefer `{{replacement}}` over `{{value}}`.'
+};
+
+const create = context => {
+	return {
+		Literal(node) {
+			if (node.value !== 'unicorn') {
+				return;
+			}
+
+			context.report({
+				node,
+				messageId: MESSAGE_ID,
+				data: {
+					value: 'unicorn',
+					replacement: '🦄'
+				},
+				fix: fixer => fixer.replaceText(node, '\'🦄\'')
+			});
+		}
+	}
+};
+
+const schema = [];
+
+module.exports = {
+	create,
+	meta: {
+		type: 'suggestion',
+		docs: {
+			url: getDocumentationUrl(__filename)
+		},
+		fixable: 'code',
+		schema,
+		messages
+	}
+};
