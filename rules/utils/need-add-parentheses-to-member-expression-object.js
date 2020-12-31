3e68c1d5e6a0e84ce23fba44f9a77fd5c499d959
Sindre Sorhus <sindresorhus@gmail.com>
2021-01-01 06:26:30 +0700

    Update need-add-parentheses-to-member-expression-object.js

--- rules/utils/need-add-parentheses-to-member-expression-object.js
+++ rules/utils/need-add-parentheses-to-member-expression-object.js
@@ -3,17 +3,16 @@
 const {isNotOpeningParenToken, isNotClosingParenToken} = require('eslint-utils');
 
 /**
-Check if need add parentheses to node, when it's used as `object` of `MemberExpression`.
+Check if parentheses needs to be added to a `node` when it's used as an `object` of `MemberExpression`.
 
-@param {Node} node The AST node to check.
-@param {SourceCode} sourceCode The source code object to get text.
+@param {Node} node - The AST node to check.
+@param {SourceCode} sourceCode - The source code object to get text.
 @returns {boolean}
 */
-
 function needAddParenthesesToMemberExpressionObject(node, sourceCode) {
 	switch (node.type) {
-		// This is not full list, some other node like `FunctionDeclaration` don't need parentheses,
-		// but not possible to be in the place we are checking at this point
+		// This is not a full list. Some other nodes like `FunctionDeclaration` don't need parentheses,
+		// but it's not possible to be in the place we are checking at this point.
 		case 'Identifier':
 		case 'MemberExpression':
 		case 'CallExpression':
