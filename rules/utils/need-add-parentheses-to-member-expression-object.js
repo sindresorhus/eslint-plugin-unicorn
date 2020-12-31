b27b35c8d1ce23862ede4a7d2b5b1fda25e53af1
fisker <lionkay@gmail.com>
2020-12-31 16:46:49 +0800

    Fix code style

--- rules/utils/need-add-parentheses-to-member-expression-object.js
+++ rules/utils/need-add-parentheses-to-member-expression-object.js
@@ -9,33 +9,33 @@ Check if need add parentheses to node, when it's used as `object` of `MemberExpr
 */
 
 function needAddParenthesesToMemberExpressionObject(node, sourceCode) {
-	const {type} = node;
-
-	switch (type) {
+	switch (node.type) {
 		// This is not full list, some other node like `FunctionDeclaration` don't need parentheses,
 		// but not possible to be in the place we are checking at this point
-		case "Identifier":
-		case "MemberExpression":
-		case "CallExpression":
-		case "ChainExpression":
-		case "TemplateLiteral":
+		case 'Identifier':
+		case 'MemberExpression':
+		case 'CallExpression':
+		case 'ChainExpression':
+		case 'TemplateLiteral':
 			return false;
-		case "NewExpression": {
+
+		case 'NewExpression': {
 			// `new Foo.bar` is different with `new Foo().bar`
 			return !sourceCode.getText(node).endsWith(')');
 		}
-		case "Literal": {
+
+		case 'Literal': {
 			/* istanbul ignore next */
-			if (typeof node.value === 'number') {
-				if (/^\d+$/.test(node.raw)) {
-					return true;
-				}
+			if (typeof node.value === 'number' && /^\d+$/.test(node.raw)) {
+				return true;
 			}
+
 			return false;
 		}
-	}
 
-	return true;
+		default:
+			return true;
+	}
 }
 
 module.exports = needAddParenthesesToMemberExpressionObject;
