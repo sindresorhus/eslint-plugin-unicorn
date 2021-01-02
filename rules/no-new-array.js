775a4c4d2a2168eb62d4528f35dbe6b205b0fa15
fisker <lionkay@gmail.com>
2021-01-02 20:18:18 +0800

    Implement rule

--- rules/no-new-array.js
+++ rules/no-new-array.js
@@ -1,32 +1,111 @@
 'use strict';
+const {isParenthesized, getStaticValue} = require('eslint-utils');
 const getDocumentationUrl = require('./utils/get-documentation-url');
 
-const MESSAGE_ID = 'no-new-array';
+const MESSAGE_ID_ERROR = 'error';
+const MESSAGE_ID_LENGTH = 'array-length';
+const MESSAGE_ID_FIRST_ELEMENT = 'first-element';
+const MESSAGE_ID_SPREAD = 'spread';
 const messages = {
-	[MESSAGE_ID]: 'Prefer `{{replacement}}` over `{{value}}`.'
+	[MESSAGE_ID_ERROR]: 'Do not use `new Array()`.',
+	[MESSAGE_ID_LENGTH]: 'The argument is length of array.',
+	[MESSAGE_ID_FIRST_ELEMENT]: 'The argument is first element of array.',
+	[MESSAGE_ID_SPREAD]: 'Spread the argument.'
 };
+const newArraySelector = [
+	'NewExpression',
+	'[callee.type="Identifier"]',
+	'[callee.name="Array"]',
+	'[arguments.length<2]'
+].join('');
 
-const create = context => {
+function create(context) {
+	const sourceCode = context.getSourceCode();
+
+	const reportEmptyArray = node => {
+		context.report({
+			node,
+			messageId: MESSAGE_ID_ERROR,
+			fix: fixer => fixer.replaceText(node, '[]')
+		});
+	};
+
+	const reportSpreadElementArray = node => {
+		context.report({
+			node,
+			messageId: MESSAGE_ID_ERROR,
+			suggest: [
+				{
+					messageId: MESSAGE_ID_SPREAD,
+					fix: fixer => fixer.replaceText(node, `[${sourceCode.getText(node.arguments[0])}]`)
+				}
+			]
+		});
+	};
+
+	const getParenthesizedText = node => {
+		const text = sourceCode.getText(node);
+		return isParenthesized(node, sourceCode) ? `(${text})` : text;
+	};
+
 	return {
-		Literal(node) {
-			if (node.value !== 'unicorn') {
+		[newArraySelector](node) {
+			// Empty array
+			if (node.arguments.length === 0) {
+				reportEmptyArray(node);
 				return;
 			}
 
+			const [argumentNode] = node.arguments;
+			// We are not sure how many `arguments` passed
+			if (argumentNode.type === 'SpreadElement') {
+				reportSpreadElementArray(node);
+				return;
+			}
+
+			const result = getStaticValue(argumentNode, context.getScope());
+			const text = getParenthesizedText(argumentNode);
+
+			// `new Array(unknown)`
+			if (result === null) {
+				context.report({
+					node,
+					messageId: MESSAGE_ID_ERROR,
+					suggest: [
+						{
+							messageId: MESSAGE_ID_LENGTH,
+							fix: fixer => fixer.replaceText(node, `Array.from({length: ${text}})`)
+						},
+						{
+							messageId: MESSAGE_ID_FIRST_ELEMENT,
+							fix: fixer => fixer.replaceText(node, `[${text}]`)
+						}
+					]
+				});
+				return;
+			}
+
+			const {value} = result;
+
+			// `new Array(number)`
+			if (typeof value === 'number') {
+				context.report({
+					node,
+					messageId: MESSAGE_ID_ERROR,
+					fix: fixer => fixer.replaceText(node, `Array.from({length: ${text}}`)
+				});
+				return;
+			}
+
+			// `new Array(notANumber)`
 			context.report({
 				node,
-				messageId: MESSAGE_ID,
-				data: {
-					value: 'unicorn',
-					replacement: '🦄'
-				},
-				fix: fixer => fixer.replaceText(node, '\'🦄\'')
+				messageId: MESSAGE_ID_ERROR,
+				fix: fixer => fixer.replaceText(node, `[${text}]`)
 			});
 		}
-	}
-};
-
-const schema = [];
+	};
+}
 
 module.exports = {
 	create,
@@ -36,7 +115,6 @@ module.exports = {
 			url: getDocumentationUrl(__filename)
 		},
 		fixable: 'code',
-		schema,
 		messages
 	}
 };
--- test/no-new-array.js
+++ test/no-new-array.js
@@ -11,7 +11,7 @@ const suggestionCase = ({code, suggestions}) => ({
 	output: code,
 	errors: [
 		{
-			messageId: MESSAGE_ID,
+			messageId: MESSAGE_ID_ERROR,
 			suggestions
 		}
 	]
@@ -26,10 +26,10 @@ test({
 		'const array = Array(1, 2)',
 
 		// `unicorn/new-for-builtins` cases
-		'const array = Array(1)',
+		'const array = Array(1)'
 	],
 	invalid: [
-		{
+		suggestionCase({
 			code: 'const array = new Array(foo)',
 			suggestions: [
 				{
@@ -41,47 +41,49 @@ test({
 					output: 'const array = [foo]'
 				}
 			]
-		},
-		{
-			code: 'const array = new Array(...[foo])',
-			suggestions: [
-				{
-					messageId: MESSAGE_ID_LENGTH,
-					output: 'const array = Array.from({length: foo})'
-				},
-				{
-					messageId: MESSAGE_ID_FIRST_ELEMENT,
-					output: 'const array = [foo]'
-				}
-			]
-		},
-		{
-			code: 'const array = new Array(...foo)',
-			suggestions: [
-				{
-					messageId: MESSAGE_ID_SPREAD,
-					output: 'const array = [...foo]'
-				}
-			]
-		},
-		{
-			code: 'const array = new Array(...[...foo])',
-			suggestions: [
-				{
-					messageId: MESSAGE_ID_SPREAD,
-					output: 'const array = [...[...foo]]'
-				}
-			]
-		},
+		}),
+		...[
+			'...[foo]',
+			'...foo',
+			'...[...foo]',
+			// The following cases we can know the result, but we are not auto-fixing them
+			'...[1]',
+			'...["1"]',
+			'...[1, "1"]'
+		].map(argumentText => {
+			const code = `const array = new Array(${argumentText})`;
+			return {
+				code,
+				output: code,
+				errors: [
+					{
+						messageId: MESSAGE_ID_ERROR,
+						suggestions: [
+							{
+								messageId: MESSAGE_ID_SPREAD,
+								output: `const array = [${argumentText}]`
+							}
+						]
+					}
+				]
+			};
+		})
 	]
 });
 
 test.visualize([
 	'const array = new Array()',
+	'const array = new Array',
 	'const array = new Array(1)',
+	// This is actually `[]`, but we fix to `Array.from({length: zero})`
+	outdent`
+		const zero = 0;
+		const array = new Array(zero);
+	`,
+	'const array = new Array(1.5)',
+	'const array = new Array(Number("1"))',
 	'const array = new Array("1")',
-	'const array = new Array(...[1])',
-	'const array = new Array(...["1"])',
-	'const array = new Array(...[1])',
-	'const array = new Array(...[1, "1"])'
+	'const array = new Array(null)',
+	'const array = new Array(("1"))',
+	'const array = new Array((0, 1))'
 ]);
