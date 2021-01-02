d1be90d1116e77ea1f1e03aab4337f0625b1accd
Sindre Sorhus <sindresorhus@gmail.com>
2021-01-02 14:28:16 +0700

    Update is-logical-expression.js

--- rules/utils/is-logical-expression.js
+++ rules/utils/is-logical-expression.js
@@ -3,10 +3,8 @@
 /**
 Check if the given node is a true logical expression or not.
 
-The three binary expressions logical-or (`||`), logical-and (`&&`), and coalesce (`??`) are known as `ShortCircuitExpression`.
-But ESTree represents those by `LogicalExpression` node.
+The three binary expressions logical-or (`||`), logical-and (`&&`), and coalesce (`??`) are known as `ShortCircuitExpression`, but ESTree represents these by the `LogicalExpression` node type. This function rejects coalesce expressions of `LogicalExpression` node type.
 
-This function rejects coalesce expressions of `LogicalExpression` node.
 @param {Node} node - The node to check.
 @returns {boolean} `true` if the node is `&&` or `||`.
 @see https://tc39.es/ecma262/#prod-ShortCircuitExpression
