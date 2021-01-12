a9aeb22926ef52476fa1556c5dc950e17613f487
Elliot Maisl <elliot.maisl@gmail.com>
2021-01-12 16:53:36 +0100

    `prefer-math-trunc`: Use suggestion instead of fix for x | 0

--- rules/prefer-math-trunc.js
+++ rules/prefer-math-trunc.js
@@ -4,9 +4,11 @@ const getDocumentationUrl = require('./utils/get-documentation-url');
 
 const MESSAGE_ID_BITWISE = 'bitwise';
 const MESSAGE_ID_BITWISE_NOT = 'bitwiseNot';
+const SUGGESTION_MESSAGE_ID = 'suggestion';
 const messages = {
 	[MESSAGE_ID_BITWISE]: 'Use `Math.trunc` instead of `{{operator}} {{value}}`.',
-	[MESSAGE_ID_BITWISE_NOT]: 'Use `Math.trunc` instead of `~~`.'
+	[MESSAGE_ID_BITWISE_NOT]: 'Use `Math.trunc` instead of `~~`.',
+	[SUGGESTION_MESSAGE_ID]: 'Replace `| 0` with `Math.trunc`.'
 };
 
 const createBitwiseNotSelector = (level, isNegative) => {
@@ -56,15 +58,26 @@ const create = context => {
 				}
 			};
 
-			if (!isAssignment || !hasSideEffect(left, sourceCode)) {
-				problem.fix = fixer => {
-					let fixed = mathTruncFunctionCall(left);
-					if (isAssignment) {
-						fixed = `${sourceCode.getText(left)} = ${fixed}`;
-					}
+			const fix = fixer => {
+				let fixed = mathTruncFunctionCall(left);
+				if (isAssignment) {
+					fixed = `${sourceCode.getText(left)} = ${fixed}`;
+				}
 
-					return fixer.replaceText(node, fixed);
-				};
+				return fixer.replaceText(node, fixed);
+			};
+
+			if (!isAssignment || !hasSideEffect(left, sourceCode)) {
+				if (operator === '|') {
+					problem.suggest = [
+						{
+							messageId: SUGGESTION_MESSAGE_ID,
+							fix
+						}
+					];
+				} else {
+					problem.fix = fix;
+				}
 			}
 
 			context.report(problem);
