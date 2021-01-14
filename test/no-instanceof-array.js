713b23fb9e0a202a54b1cec91eeb2cc8be47f64d
fisker <lionkay@gmail.com>
2021-01-14 18:58:45 +0800

    Refactor tests

--- test/no-instanceof-array.js
+++ test/no-instanceof-array.js
@@ -1,12 +1,6 @@
 import {test} from './utils/test.js';
 
-const errors = [
-	{
-		messageId: 'no-instanceof-array'
-	}
-];
-
-test({
+test.visualize({
 	valid: [
 		'Array.isArray(arr)',
 		'arr instanceof Object',
@@ -18,39 +12,11 @@ test({
 		'"arr instanceof Array"'
 	],
 	invalid: [
-		{
-			code: 'arr instanceof Array',
-			output: 'Array.isArray(arr)',
-			errors
-		},
-		{
-			code: '[] instanceof Array',
-			output: 'Array.isArray([])',
-			errors
-		},
-		{
-			code: '[1,2,3] instanceof Array === true',
-			output: 'Array.isArray([1,2,3]) === true',
-			errors
-		},
-		{
-			code: 'fun.call(1, 2, 3) instanceof Array',
-			output: 'Array.isArray(fun.call(1, 2, 3))',
-			errors
-		},
-		{
-			code: 'obj.arr instanceof Array',
-			output: 'Array.isArray(obj.arr)',
-			errors
-		},
-		{
-			code: 'foo.bar[2] instanceof Array',
-			output: 'Array.isArray(foo.bar[2])',
-			errors
-		}
+		'arr instanceof Array',
+		'[] instanceof Array',
+		'[1,2,3] instanceof Array === true',
+		'fun.call(1, 2, 3) instanceof Array',
+		'obj.arr instanceof Array',
+		'foo.bar[2] instanceof Array'
 	]
 });
-
-test.visualize([
-	'if (arr instanceof Array) {}'
-]);
