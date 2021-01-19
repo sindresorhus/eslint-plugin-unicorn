a8f9cc8249c0655a053093c80703608fc1e0debd
fisker <lionkay@gmail.com>
2021-01-19 17:04:12 +0800

    Only reverse when use `.entries()`

--- rules/no-array-for-each.js
+++ rules/no-array-for-each.js
@@ -49,13 +49,11 @@ function getFixFunction(callExpression, sourceCode, functionInfo) {
 	const {returnStatements} = functionInfo.get(callback);
 
 	const getForOfLoopHeadText = () => {
-		const parametersText = parameters
-			.map(parameter => sourceCode.getText(parameter))
-			.reverse();
+		const parametersText = parameters.map(parameter => sourceCode.getText(parameter));
 		const useEntries = parameters.length === 2;
 
 		let text = 'for (const ';
-		text += useEntries ? `[${parametersText.join(', ')}]` : parametersText[0];
+		text += useEntries ? `[${parametersText.reverse().join(', ')}]` : parametersText[0];
 
 		text += ' of ';
 
