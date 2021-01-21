1a029a51cf4ec9dadad1109593851812e7c056b2
fisker <lionkay@gmail.com>
2021-01-21 15:47:05 +0800

    Add test

--- test/prefer-destructuring-in-parameters.js
+++ test/prefer-destructuring-in-parameters.js
@@ -9,6 +9,7 @@ test.snapshot({
 		'const foo = (bar, baz) => foo.x',
 		'const foo = function bar(baz) {return bar.name}',
 		'const foo = ({bar}) => bar',
+		'const foo = ({bar}) => bar.x',
 		'const foo = bar => bar[2]',
 		'const foo = bar => bar[1.5]',
 		'const foo = bar => bar[-1]',
