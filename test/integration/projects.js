44e52b378a41ec1be1e80adca3a47e038e47e5ba
fisker <lionkay@gmail.com>
2021-01-27 15:35:04 +0800

    Disable integration tests

--- test/integration/projects.js
+++ test/integration/projects.js
@@ -142,8 +142,9 @@ module.exports = [
 	// #1030
 	'https://github.com/astrofox-io/astrofox',
 	// #1075
-	'https://github.com/untitled-labs/metabase-custom',
-	'https://github.com/TheThingsNetwork/lorawan-stack'
+	// These two project use `decorator`, try to enable when we use `@babel/eslint-parser`
+	// 'https://github.com/untitled-labs/metabase-custom',
+	// 'https://github.com/TheThingsNetwork/lorawan-stack'
 ].map(project => {
 	if (typeof project === 'string') {
 		project = {repository: project};
