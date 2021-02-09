0dd4ae9ae48448aff4570e28bcfc12ee45b903e9
fisker Cheung <lionkay@gmail.com>
2021-02-09 15:17:02 +0800

    Pass `method` and `replacement`

--- test/shared/simple-array-search-rule-tests.js
+++ test/shared/simple-array-search-rule-tests.js
@@ -1,123 +1,123 @@
-'use strict';
-const {outdent} = require('outdent');
+`use strict`;
+const {outdent} = require(`outdent`);
 
 function snapshotTests({method, replacement}) {
 	return {
 		valid: [
-			'const findIndex = foo.findIndex',
+			`const ${method} = foo.${method}`,
 
 			// Test `foo.findIndex`
 			// More/less argument(s)
-			'foo.findIndex()',
-			'foo.findIndex(function (x) {return x === 1;}, bar)',
-			'foo.findIndex(...[function (x) {return x === 1;}])',
+			`foo.${method}()`,
+			`foo.${method}(function (x) {return x === 1;}, bar)`,
+			`foo.${method}(...[function (x) {return x === 1;}])`,
 			// Not `CallExpression`
-			'new foo.findIndex(x => x === 1)',
+			`new foo.${method}(x => x === 1)`,
 			// Not `MemberExpression`
-			'findIndex(x => x === 1)',
+			`${method}(x => x === 1)`,
 			// `callee.property` is not a `Identifier`
-			'foo["findIndex"](x => x === 1)',
+			`foo["${method}"](x => x === 1)`,
 			// Computed
-			'foo[findIndex](x => x === 1)',
+			`foo[${method}](x => x === 1)`,
 			// Not `findIndex`
-			'foo.notListedMethod(x => x === 1)',
+			`foo.notListedMethod(x => x === 1)`,
 
 			// Test `callback` part
 			// Not function
-			'foo.findIndex(myFunction)',
+			`foo.${method}(myFunction)`,
 			// Not one parameter
-			'foo.findIndex((x, i) => x === i)',
-			'foo.findIndex((x, i) => {return x === i})',
-			'foo.findIndex(function(x, i) {return x === i})',
+			`foo.${method}((x, i) => x === i)`,
+			`foo.${method}((x, i) => {return x === i})`,
+			`foo.${method}(function(x, i) {return x === i})`,
 			// Parameter is not `Identifier`
-			'foo.findIndex(({x}) => x === 1)',
-			'foo.findIndex(({x}) => {return x === 1})',
-			'foo.findIndex(function({x}) {return x === 1})',
+			`foo.${method}(({x}) => x === 1)`,
+			`foo.${method}(({x}) => {return x === 1})`,
+			`foo.${method}(function({x}) {return x === 1})`,
 			// `generator`
-			'foo.findIndex(function * (x) {return x === 1})',
+			`foo.${method}(function * (x) {return x === 1})`,
 			// `async`
-			'foo.findIndex(async (x) => x === 1)',
-			'foo.findIndex(async (x) => {return x === 1})',
-			'foo.findIndex(async function(x) {return x === 1})',
+			`foo.${method}(async (x) => x === 1)`,
+			`foo.${method}(async (x) => {return x === 1})`,
+			`foo.${method}(async function(x) {return x === 1})`,
 
 			// Test `callback` body
 			// Not only `return`
-			'foo.findIndex(({x}) => {noop();return x === 1})',
+			`foo.${method}(({x}) => {noop();return x === 1})`,
 			// Not `return`
-			'foo.findIndex(({x}) => {bar(x === 1)})',
+			`foo.${method}(({x}) => {bar(x === 1)})`,
 
 			// Test `BinaryExpression`
 			// Not `BinaryExpression`
-			'foo.findIndex(x => x - 1)',
-			'foo.findIndex(x => {return x - 1})',
-			'foo.findIndex(function (x){return x - 1})',
+			`foo.${method}(x => x - 1)`,
+			`foo.${method}(x => {return x - 1})`,
+			`foo.${method}(function (x){return x - 1})`,
 			// Not `===`
-			'foo.findIndex(x => x !== 1)',
-			'foo.findIndex(x => {return x !== 1})',
-			'foo.findIndex(function (x){return x !== 1})',
+			`foo.${method}(x => x !== 1)`,
+			`foo.${method}(x => {return x !== 1})`,
+			`foo.${method}(function (x){return x !== 1})`,
 			// Neither `left` nor `right` is Identifier
-			'foo.findIndex(x => 1 === 1.0)',
-			'foo.findIndex(x => {return 1 === 1.0})',
-			'foo.findIndex(function (x){return 1 === 1.0})',
+			`foo.${method}(x => 1 === 1.0)`,
+			`foo.${method}(x => {return 1 === 1.0})`,
+			`foo.${method}(function (x){return 1 === 1.0})`,
 			// Both `left` and `right` are same as `parameter`
-			'foo.findIndex(x => x === x)',
+			`foo.${method}(x => x === x)`,
 
 			// Not the same identifier
-			'foo.findIndex(x => y === 1)',
+			`foo.${method}(x => y === 1)`,
 
 			// Dynamical value
-			'foo.findIndex(x => x + "foo" === "foo" + x)',
+			`foo.${method}(x => x + "foo" === "foo" + x)`,
 
 			// Parameter is used
-			'foo.findIndex(x => x === "foo" + x)',
+			`foo.${method}(x => x === "foo" + x)`,
 			// Parameter is used in a deeper scope
-			'foo.findIndex(x => x === (function (){return x === "1"})())',
+			`foo.${method}(x => x === (function (){return x === "1"})())`,
 			// FunctionName is used
-			'foo.findIndex(function fn(x) {return x === fn(y)})',
+			`foo.${method}(function fn(x) {return x === fn(y)})`,
 			// `arguments` is used
-			'foo.findIndex(function(x) {return x === arguments.length})',
+			`foo.${method}(function(x) {return x === arguments.length})`,
 			// `this` is used
-			'foo.findIndex(function(x) {return x === this.length})',
+			`foo.${method}(function(x) {return x === this.length})`,
 
 			// Already valid case
-			'foo.indexOf(0)'
+			`foo.${replacement}(0)`
 		],
 
 		invalid: [
-			'values.findIndex(x => x === "foo")',
-			'values.findIndex(x => "foo" === x)',
-			'values.findIndex(x => {return x === "foo";})',
-			'values.findIndex(function (x) {return x === "foo";})',
+			`values.${method}(x => x === "foo")`,
+			`values.${method}(x => "foo" === x)`,
+			`values.${method}(x => {return x === "foo";})`,
+			`values.${method}(function (x) {return x === "foo";})`,
 			outdent`
 				// 1
 				(0, values)
 					// 2
-					./* 3 */findIndex /* 3 */ (
+					./* 3 */${method} /* 3 */ (
 						/* 4 */
 						x /* 5 */ => /* 6 */ x /* 7 */ === /* 8 */ "foo" /* 9 */
 					) /* 10 */
 			`,
 			outdent`
-				foo.findIndex(function (element) {
-					return element === bar.findIndex(x => x === 1);
+				foo.${method}(function (element) {
+					return element === bar.${method}(x => x === 1);
 				});
 			`,
-			'values.findIndex(x => x === (0, "foo"))',
-			'values.findIndex((x => x === (0, "foo")))',
+			`values.${method}(x => x === (0, "foo"))`,
+			`values.${method}((x => x === (0, "foo")))`,
 			// `this`/`arguments` in arrow functions
 			outdent`
 				function fn() {
-					foo.findIndex(x => x === arguments.length)
+					foo.${method}(x => x === arguments.length)
 				}
 			`,
 			outdent`
 				function fn() {
-					foo.findIndex(x => x === this[1])
+					foo.${method}(x => x === this[1])
 				}
 			`,
-			'values.findIndex(x => x === foo())',
+			`values.${method}(x => x === foo())`,
 			outdent`
-				foo.findIndex(function a(x) {
+				foo.${method}(function a(x) {
 					return x === (function (a) {
 						return a(this) === arguments[1]
 					}).call(thisObject, anotherFunctionNamedA, secondArgument)
@@ -134,12 +134,12 @@ function typescriptTests({method, replacement}) {
 			{
 				code: outdent`
 					function foo() {
-						return (bar as string).findIndex(x => x === "foo");
+						return (bar as string).${method}(x => x === "foo");
 					}
 				`,
 				output: outdent`
 					function foo() {
-						return (bar as string).indexOf("foo");
+						return (bar as string).${replacement}("foo");
 					}
 				`,
 				errors: 1
