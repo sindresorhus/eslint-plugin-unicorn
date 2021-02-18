52554dc67be56a1622fbfc080319b332d720caf1
fisker Cheung <lionkay@gmail.com>
2021-02-18 17:41:42 +0800

    Fix

--- test/get-documentation-url.mjs
+++ test/get-documentation-url.mjs
@@ -6,8 +6,8 @@ import path from 'path';
 import getDocumentationUrl from '../rules/utils/get-documentation-url.js';
 
 const require = createRequire(import.meta.url);
-
 const packageJson = require('../package.json');
+const filename = url.fileURLToPath(import.meta.url).replace(/\.mjs$/, '.js');
 
 test('returns the URL of the a named rule\'s documentation', t => {
 	const url = `https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v${packageJson.version}/docs/rules/foo.md`;
