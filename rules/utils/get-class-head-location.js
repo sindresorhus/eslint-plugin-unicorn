a926dd3664f5ba06fcc62efc302f5cf45444a0fa
Sindre Sorhus <sindresorhus@gmail.com>
2021-02-21 02:16:44 +0700

    Update get-class-head-location.js

--- rules/utils/get-class-head-location.js
+++ rules/utils/get-class-head-location.js
@@ -1,12 +1,13 @@
 'use strict';
 
 /**
-* @typedef {line: number, column: number} Position
-*
-* Get the location of the given class node for reporting.
-* @param {Node} node The class node to get.
-* @param {SourceCode} sourceCode The source code object to get tokens.
-* @returns {start: Position, end: Position} The location of the class node for reporting.
+@typedef {line: number, column: number} Position
+
+Get the location of the given class node for reporting.
+
+@param {Node} node - The class node to get.
+@param {SourceCode} sourceCode - The source code object to get tokens.
+@returns {start: Position, end: Position} The location of the class node for reporting.
 */
 function getClassHeadLocation(node, sourceCode) {
 	const {loc, body} = node;
