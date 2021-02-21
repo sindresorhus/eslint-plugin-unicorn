98717ee623da933010f8296892b9c3ba154c3227
fisker <lionkay@gmail.com>
2021-02-21 14:57:55 +0800

    Fix JSDoc

--- rules/utils/get-class-head-location.js
+++ rules/utils/get-class-head-location.js
@@ -7,7 +7,7 @@ Get the location of the given class node for reporting.
 
 @param {Node} node - The class node to get.
 @param {SourceCode} sourceCode - The source code object to get tokens.
-@returns {start: Position, end: Position} The location of the class node for reporting.
+@returns {{start: Position, end: Position}} The location of the class node for reporting.
 */
 function getClassHeadLocation(node, sourceCode) {
 	const {loc, body} = node;
