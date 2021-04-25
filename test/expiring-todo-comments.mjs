c4473a369ab8590d85a29c0da4bc4c56d45319ea
fisker Cheung <lionkay@gmail.com>
2021-04-25 16:57:45 +0800

    Fix duplicated key

--- test/expiring-todo-comments.mjs
+++ test/expiring-todo-comments.mjs
@@ -203,7 +203,6 @@ test({
 			code: '// TODO [2200-12-12, 2200-12-12]: Multiple dates',
 			output: null,
 			errors: [avoidMultipleDatesError('2200-12-12, 2200-12-12', 'Multiple dates')],
-			output: '// TODO [2200-12-12, 2200-12-12]: Multiple dates'
 		},
 		{
 			code: '// TODO [>1]: if your package.json version is >1',
