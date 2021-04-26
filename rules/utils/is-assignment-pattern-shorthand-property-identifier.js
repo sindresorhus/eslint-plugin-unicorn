1434de98bc4acb62a14a707690704ce625d8a42b
fisker Cheung <lionkay@gmail.com>
2021-04-26 10:50:57 +0800

    Style

--- rules/utils/is-assignment-pattern-shorthand-property-identifier.js
+++ rules/utils/is-assignment-pattern-shorthand-property-identifier.js
@@ -8,7 +8,7 @@ module.exports = identifier =>
 	identifier.parent.parent.type === 'Property' &&
 	(
 		identifier === identifier.parent.parent.key ||
-		// In `@babel/eslint-parser` the `variable.reference.identifier` 
+		// In `@babel/eslint-parser` the `variable.reference.identifier`
 		// is neither reference of `identifier.parent.key` nor `identifier.parent.value`
 		// https://github.com/babel/babel/issues/13205
 		hasSameRange(identifier, identifier.parent.parent.key)
--- rules/utils/is-shorthand-property-identifier.js
+++ rules/utils/is-shorthand-property-identifier.js
@@ -7,7 +7,7 @@ module.exports = identifier =>
 	identifier.parent.shorthand &&
 	(
 		identifier === identifier.parent.key ||
-		// In `@babel/eslint-parser` the `variable.reference.identifier` 
+		// In `@babel/eslint-parser` the `variable.reference.identifier`
 		// is neither reference of `identifier.parent.key` nor `identifier.parent.value`
 		// https://github.com/babel/babel/issues/13205
 		hasSameRange(identifier, identifier.parent.key)
