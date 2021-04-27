1276db52f1788d7bcc385adac631fb8a2aad2a12
fisker Cheung <lionkay@gmail.com>
2021-04-27 11:46:44 +0800

    Remove workaround in `isShorthandPropertyIdentifier` (#1214)

--- rules/utils/is-shorthand-property-identifier.js
+++ rules/utils/is-shorthand-property-identifier.js
@@ -1,14 +1,8 @@
 'use strict';
 
-const hasSameRange = require('./has-same-range');
-
-module.exports = identifier =>
+const isShorthandPropertyIdentifier = identifier =>
 	identifier.parent.type === 'Property' &&
 	identifier.parent.shorthand &&
-	(
-		identifier === identifier.parent.key ||
-		// In `@babel/eslint-parser` the `variable.reference.identifier`
-		// is neither reference of `identifier.parent.key` nor `identifier.parent.value`
-		// https://github.com/babel/babel/issues/13205
-		hasSameRange(identifier, identifier.parent.key)
-	);
+	identifier === identifier.parent.value;
+
+module.exports = isShorthandPropertyIdentifier;
