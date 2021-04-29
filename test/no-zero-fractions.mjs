d62d8407e7f51f2f9f6bc1798e4437a32f28dadf
fisker Cheung <lionkay@gmail.com>
2021-04-29 17:35:52 +0800

    Rewrite tests

--- test/no-zero-fractions.mjs
+++ test/no-zero-fractions.mjs
@@ -2,16 +2,7 @@ import {getTester} from './utils/test.mjs';
 
 const {test} = getTester(import.meta);
 
-const MESSAGE_ZERO_FRACTION = 'zero-fraction';
-const MESSAGE_DANGLING_DOT = 'dangling-dot';
-const errorZeroFraction = {
-	messageId: MESSAGE_ZERO_FRACTION
-};
-const errorDanglingDot = {
-	messageId: MESSAGE_DANGLING_DOT
-};
-
-test({
+test.snapshot({
 	valid: [
 		'const foo = "123.1000"',
 		'foo("123.1000")',
@@ -25,83 +16,20 @@ test({
 		'const foo = 1e3'
 	],
 	invalid: [
-		{
-			code: 'const foo = 1.0',
-			output: 'const foo = 1',
-			errors: [errorZeroFraction]
-		},
-		{
-			code: 'const foo = 1.0 + 1',
-			output: 'const foo = 1 + 1',
-			errors: [errorZeroFraction]
-		},
-		{
-			code: 'foo(1.0 + 1)',
-			output: 'foo(1 + 1)',
-			errors: [errorZeroFraction]
-		},
-		{
-			code: 'const foo = 1.00',
-			output: 'const foo = 1',
-			errors: [errorZeroFraction]
-		},
-		{
-			code: 'const foo = 1.00000',
-			output: 'const foo = 1',
-			errors: [errorZeroFraction]
-		},
-		{
-			code: 'const foo = -1.0',
-			output: 'const foo = -1',
-			errors: [errorZeroFraction]
-		},
-		{
-			code: 'const foo = 123123123.0',
-			output: 'const foo = 123123123',
-			errors: [errorZeroFraction]
-		},
-		{
-			code: 'const foo = 123.11100000000',
-			output: 'const foo = 123.111',
-			errors: [errorZeroFraction]
-		},
-		{
-			code: 'const foo = 1.',
-			output: 'const foo = 1',
-			errors: [errorDanglingDot]
-		},
-		{
-			code: 'const foo = +1.',
-			output: 'const foo = +1',
-			errors: [errorDanglingDot]
-		},
-		{
-			code: 'const foo = -1.',
-			output: 'const foo = -1',
-			errors: [errorDanglingDot]
-		},
-		{
-			code: 'const foo = 1.e10',
-			output: 'const foo = 1e10',
-			errors: [errorDanglingDot]
-		},
-		{
-			code: 'const foo = +1.e-10',
-			output: 'const foo = +1e-10',
-			errors: [errorDanglingDot]
-		},
-		{
-			code: 'const foo = -1.e+10',
-			output: 'const foo = -1e+10',
-			errors: [errorDanglingDot]
-		}
-	]
-});
-
-test.snapshot({
-	valid: [],
-	invalid: [
 		'const foo = 1.0',
+		'const foo = 1.0 + 1',
+		'foo(1.0 + 1)',
+		'const foo = 1.00',
+		'const foo = 1.00000',
+		'const foo = -1.0',
+		'const foo = 123123123.0',
+		'const foo = 123.11100000000',
+		'const foo = 1.',
+		'const foo = +1.',
+		'const foo = -1.',
+		'const foo = 1.e10',
+		'const foo = +1.e-10',
+		'const foo = -1.e+10',
 		'const foo = (1.).toString()'
 	]
 });
