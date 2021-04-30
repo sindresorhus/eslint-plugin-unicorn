5be500f222ae4d593f0ff2b2cf03a76f86b68952
fisker Cheung <lionkay@gmail.com>
2021-04-30 15:21:57 +0800

    Simplify rule & improve report location

--- rules/no-zero-fractions.js
+++ rules/no-zero-fractions.js
@@ -1,6 +1,6 @@
 'use strict';
 const getDocumentationUrl = require('./utils/get-documentation-url');
-const {isNumber, parseNumber, parseFloat} = require('./utils/numeric');
+const {isNumber} = require('./utils/numeric');
 
 const MESSAGE_ZERO_FRACTION = 'zero-fraction';
 const MESSAGE_DANGLING_DOT = 'dangling-dot';
@@ -9,31 +9,37 @@ const messages = {
 	[MESSAGE_DANGLING_DOT]: 'Don\'t use a dangling dot in the number.'
 };
 
-const format = text => {
-	// Legacy octal number `0777` and prefixed number `0o1234` can't has dot.
-	const {number, mark, sign, power} = parseNumber(text);
-	const {integer, dot, fractional} = parseFloat(number);
-	return {
-		hasDanglingDot: !fractional,
-		formatted: integer + (dot + fractional).replace(/[0_.]+$/g, '') + mark + sign + power
-	};
-}
-
 const create = context => {
 	return {
 		Literal: node => {
-			const {raw} = node;
-			if (!isNumber(node) || !raw.includes('.')) {
+			if (!isNumber(node)) {
 				return;
 			}
 
-			const {hasDanglingDot, formatted} = format(raw);
+			// Legacy octal number `0777` and prefixed number `0o1234` can't has dot.
+			const {raw} = node;
+			const match = raw.match(/^(?<before>[\d_]*)(?<dotAndFraction>\.[\d_]*)(?<after>.*)$/);
+			if (!match) {
+				return;
+			}
+
+			const {before, dotAndFraction, after} = match.groups;
+			const formatted = before + dotAndFraction.replace(/[0_.]+$/g, '') + after;
+
 			if (formatted === raw) {
 				return;
 			}
 
+			const hasDanglingDot = dotAndFraction.length === 1;
+			// End of fractional
+			const end = node.range[0] + before.length + dotAndFraction.length;
+			const start = end - (raw.length - formatted.length);
+			const sourceCode = context.getSourceCode();
 			context.report({
-				node,
+				loc: {
+					start: sourceCode.getLocFromIndex(start),
+					end: sourceCode.getLocFromIndex(end)
+				},
 				messageId: hasDanglingDot ? MESSAGE_DANGLING_DOT : MESSAGE_ZERO_FRACTION,
 				fix: fixer => fixer.replaceText(node, formatted)
 			});
