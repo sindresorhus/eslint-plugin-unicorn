f21045181a109deea9a8a4bf0a510586660d98ea
fisker Cheung <lionkay@gmail.com>
2021-05-12 18:58:20 +0800

    Sort big objects by ESLint

---
+++ rules/shared/default-abbreviations.js
@@ -0,0 +1,211 @@
+/* eslint sort-keys: ["error", "asc", {natural: true}] */
+'use strict';
+module.exports = {
+	acc: {
+		accumulator: true
+	},
+	arg: {
+		argument: true
+	},
+	args: {
+		arguments: true
+	},
+	arr: {
+		array: true
+	},
+	attr: {
+		attribute: true
+	},
+	btn: {
+		button: true
+	},
+	attrs: {
+		attributes: true
+	},
+	cb: {
+		callback: true
+	},
+	conf: {
+		config: true
+	},
+	ctx: {
+		context: true
+	},
+	cur: {
+		current: true
+	},
+	curr: {
+		current: true
+	},
+	db: {
+		database: true
+	},
+	dest: {
+		destination: true
+	},
+	dev: {
+		development: true
+	},
+	dir: {
+		directory: true,
+		direction: true
+	},
+	dirs: {
+		directories: true
+	},
+	doc: {
+		document: true
+	},
+	docs: {
+		documentation: true,
+		documents: true
+	},
+	e: {
+		error: true,
+		event: true
+	},
+	el: {
+		element: true
+	},
+	elem: {
+		element: true
+	},
+	env: {
+		environment: true
+	},
+	envs: {
+		environments: true
+	},
+	err: {
+		error: true
+	},
+	ev: {
+		event: true
+	},
+	evt: {
+		event: true
+	},
+	ext: {
+		extension: true
+	},
+	exts: {
+		extensions: true
+	},
+	fn: {
+		function: true
+	},
+	func: {
+		function: true
+	},
+	i: {
+		index: true
+	},
+	idx: {
+		index: true
+	},
+	j: {
+		index: true
+	},
+	len: {
+		length: true
+	},
+	lib: {
+		library: true
+	},
+	mod: {
+		module: true
+	},
+	msg: {
+		message: true
+	},
+	num: {
+		number: true
+	},
+	obj: {
+		object: true
+	},
+	opts: {
+		options: true
+	},
+	param: {
+		parameter: true
+	},
+	params: {
+		parameters: true
+	},
+	pkg: {
+		package: true
+	},
+	prev: {
+		previous: true
+	},
+	prod: {
+		production: true
+	},
+	prop: {
+		property: true
+	},
+	props: {
+		properties: true
+	},
+	ref: {
+		reference: true
+	},
+	refs: {
+		references: true
+	},
+	rel: {
+		related: true,
+		relationship: true,
+		relative: true
+	},
+	req: {
+		request: true
+	},
+	res: {
+		response: true,
+		result: true
+	},
+	ret: {
+		returnValue: true
+	},
+	retval: {
+		returnValue: true
+	},
+	sep: {
+		separator: true
+	},
+	src: {
+		source: true
+	},
+	stdDev: {
+		standardDeviation: true
+	},
+	str: {
+		string: true
+	},
+	tbl: {
+		table: true
+	},
+	temp: {
+		temporary: true
+	},
+	tit: {
+		title: true
+	},
+	tmp: {
+		temporary: true
+	},
+	val: {
+		value: true
+	},
+	var: {
+		variable: true
+	},
+	vars: {
+		variables: true
+	},
+	ver: {
+		version: true
+	}
+};
