fa68b6f702016f7db58c9b731951994e50b12f6b
fisker Cheung <lionkay@gmail.com>
2021-05-17 14:02:03 +0800

    `empty-brace-spaces`: Support experimental syntax (#1276)

--- rules/empty-brace-spaces.js
+++ rules/empty-brace-spaces.js
@@ -1,4 +1,5 @@
 'use strict';
+const {isOpeningBraceToken} = require('eslint-utils');
 const getDocumentationUrl = require('./utils/get-documentation-url');
 
 const MESSAGE_ID = 'empty-brace-spaces';
@@ -10,17 +11,44 @@ const selector = `:matches(${
 	[
 		'BlockStatement[body.length=0]',
 		'ClassBody[body.length=0]',
-		'ObjectExpression[properties.length=0]'
+		'ObjectExpression[properties.length=0]',
+		// Experimental https://github.com/tc39/proposal-record-tuple
+		'RecordExpression[properties.length=0]',
+		// Experimental https://github.com/tc39/proposal-class-static-block
+		'StaticBlock[body.length=0]'
 	].join(', ')
 })`;
 
+/** @param {import('eslint').Rule.RuleContext} context */
 const create = context => {
 	const sourceCode = context.getSourceCode();
 	return {
 		[selector](node) {
-			let [start, end] = node.range;
-			start += 1;
-			end -= 1;
+			let startOffset = 1;
+			let endOffset = -1;
+
+			switch (node.type) {
+				case 'RecordExpression': {
+					startOffset = 2;
+					const [start] = node.range;
+					const firstTwoCharacters = sourceCode.text.slice(start, start + 2);
+					if (firstTwoCharacters === '{|') {
+						endOffset = -2;
+					}
+
+					break;
+				}
+
+				case 'StaticBlock': {
+					const openingBraceToken = sourceCode.getFirstToken(node, isOpeningBraceToken);
+					startOffset = openingBraceToken.range[1] - node.range[0];
+					break;
+				}
+				// No default
+			}
+
+			const start = node.range[0] + startOffset;
+			const end = node.range[1] + endOffset;
 
 			if (!/^\s+$/.test(sourceCode.text.slice(start, end))) {
 				return;
