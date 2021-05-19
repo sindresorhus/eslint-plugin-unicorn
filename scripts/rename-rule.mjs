c1d08801fab235a6067b44c99bdb56612e4ceead
fisker Cheung <lionkay@gmail.com>
2021-05-19 10:59:23 +0800

    Style

--- scripts/rename-rule.mjs
+++ scripts/rename-rule.mjs
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import fs, { promises as fsAsync } from 'node:fs';
-import unicorn from '../index.js';
+import fs, {promises as fsAsync} from 'node:fs';
 import enquirer from 'enquirer';
+import unicorn from '../index.js';
 
 const rules = Object.keys(unicorn.rules);
-const resolveFile = (file) => new URL(`../${file}`, import.meta.url);
+const resolveFile = file => new URL(`../${file}`, import.meta.url);
 
 function checkFiles(ruleId) {
 	const files = [
@@ -12,7 +12,7 @@ function checkFiles(ruleId) {
 		`rules/${ruleId}.js`,
 		`test/${ruleId}.mjs`,
 		`test/snapshots/${ruleId}.mjs.md`,
-		`test/snapshots/${ruleId}.mjs.snap`,
+		`test/snapshots/${ruleId}.mjs.snap`
 	];
 
 	for (const file of files) {
@@ -26,25 +26,17 @@ async function renameFile(source, target) {
 	source = resolveFile(source);
 	target = resolveFile(target);
 
-	if (!fs.existsSync(source)) {
-		return;
+	if (fs.existsSync(source)) {
+		await fsAsync.rename(source, target);
 	}
-
-	await fsAsync.rename(source, target);
 }
 
 async function renameRule(from, to) {
 	await renameFile(`docs/rules/${from}.md`, `docs/rules/${to}.md`);
 	await renameFile(`rules/${from}.js`, `rules/${to}.js`);
 	await renameFile(`test/${from}.mjs`, `test/${to}.mjs`);
-	await renameFile(
-		`test/snapshots/${from}.mjs.md`,
-		`test/snapshots/${to}.mjs.md`
-	);
-	await renameFile(
-		`test/snapshots/${from}.mjs.snap`,
-		`test/snapshots/${to}.mjs.snap`
-	);
+	await renameFile(`test/snapshots/${from}.mjs.md`, `test/snapshots/${to}.mjs.md`);
+	await renameFile(`test/snapshots/${from}.mjs.snap`, `test/snapshots/${to}.mjs.snap`);
 
 	for (const file of [
 		'readme.md',
@@ -52,31 +44,31 @@ async function renameRule(from, to) {
 		`docs/rules/${to}.md`,
 		`rules/${to}.js`,
 		`test/${to}.mjs`,
-		`test/snapshots/${to}.mjs.md`,
-	].map((file) => resolveFile(file))) {
+		`test/snapshots/${to}.mjs.md`
+	].map(file => resolveFile(file))
+	) {
 		if (!fs.existsSync(file)) {
 			continue;
 		}
 
+		// eslint-disable-next-line no-await-in-loop
 		let text = await fsAsync.readFile(file, 'utf8');
 		text = text.replaceAll(from, to);
+		// eslint-disable-next-line no-await-in-loop
 		await fsAsync.writeFile(file, text);
 	}
-
-	await import('./generate-rules-table.mjs');
-	await import('./generate-usage-example.mjs');
 }
 
 (async () => {
 	const originalRuleId = await new enquirer.AutoComplete({
 		message: 'Select the rule you want rename:',
 		limit: 10,
-		choices: rules,
+		choices: rules
 	}).run();
 
 	const ruleId = await new enquirer.Input({
 		message: 'New name:',
-		initial: originalRuleId,
+		initial: originalRuleId
 	}).run();
 
 	if (!ruleId || originalRuleId === ruleId) {
@@ -90,8 +82,7 @@ async function renameRule(from, to) {
 
 	checkFiles(ruleId);
 	renameRule(originalRuleId, ruleId);
-
-})().catch((error) => {
+})().catch(error => {
 	console.error(error);
 	process.exit(1);
 });
