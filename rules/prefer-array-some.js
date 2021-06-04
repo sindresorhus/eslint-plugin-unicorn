06476d75143d4eed69109175e4b1cfb2c466a44e
fisker Cheung <lionkay@gmail.com>
2021-06-04 16:41:56 +0800

    Dot

--- rules/prefer-array-some.js
+++ rules/prefer-array-some.js
@@ -101,7 +101,7 @@ module.exports = {
 	meta: {
 		type: 'suggestion',
 		docs: {
-			description: 'Prefer `.some(…)` over `.filter(…).length` check and `.find(…)`',
+			description: 'Prefer `.some(…)` over `.filter(…).length` check and `.find(…)`.',
 			url: getDocumentationUrl(__filename),
 			suggestion: true
 		},
