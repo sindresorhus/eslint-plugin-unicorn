8a74d73e33935fc979db0167758ebc102ac8229f
fisker Cheung <lionkay@gmail.com>
2021-06-08 10:06:18 +0800

    Fix

--- rules/prefer-add-event-listener.js
+++ rules/prefer-add-event-listener.js
@@ -1,7 +1,7 @@
 'use strict';
 const {isParenthesized} = require('eslint-utils');
 const getDocumentationUrl = require('./utils/get-documentation-url.js');
-const domEventsJson = require('./utils/dom-events.json.js');
+const domEventsJson = require('./utils/dom-events.json');
 const {STATIC_REQUIRE_SOURCE_SELECTOR} = require('./selectors/index.js');
 
 const MESSAGE_ID = 'prefer-add-event-listener';
