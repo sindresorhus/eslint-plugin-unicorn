e7b43003be974ee2a618c0f087805f1b2a9088be
fisker Cheung <lionkay@gmail.com>
2021-06-15 17:05:10 +0800

    `error-message`: Support `AggregateError` (#1351)

--- rules/error-message.js
+++ rules/error-message.js
@@ -2,42 +2,50 @@
 const {getStaticValue} = require('eslint-utils');
 const {callOrNewExpressionSelector} = require('./selectors/index.js');
 
-const MESSAGE_ID_MISSING_MESSAGE = 'constructorMissingMessage';
-const MESSAGE_ID_EMPTY_MESSAGE = 'emptyMessage';
+const MESSAGE_ID_MISSING_MESSAGE = 'missing-message';
+const MESSAGE_ID_EMPTY_MESSAGE = 'message-is-empty-string';
 const MESSAGE_ID_NOT_STRING = 'message-is-not-a-string';
 const messages = {
-	[MESSAGE_ID_MISSING_MESSAGE]: 'Pass a message to the `{{constructor}}` constructor.',
+	[MESSAGE_ID_MISSING_MESSAGE]: 'Pass a message to the `{{constructorName}}` constructor.',
 	[MESSAGE_ID_EMPTY_MESSAGE]: 'Error message should not be an empty string.',
 	[MESSAGE_ID_NOT_STRING]: 'Error message should be a string.'
 };
 
-const errorConstructors = [
-	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
-	'Error',
-	'EvalError',
-	'RangeError',
-	'ReferenceError',
-	'SyntaxError',
-	'TypeError',
-	'URIError',
-	'InternalError'
-];
-const noArgumentsExpressionSelector = callOrNewExpressionSelector({names: errorConstructors, length: 0});
-const errorMessageSelector = callOrNewExpressionSelector({names: errorConstructors, min: 1});
+const selector = callOrNewExpressionSelector({
+	names: [
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+		'Error',
+		'EvalError',
+		'RangeError',
+		'ReferenceError',
+		'SyntaxError',
+		'TypeError',
+		'URIError',
+		'InternalError',
+		'AggregateError'
+	]
+});
 
 const create = context => {
 	return {
-		[noArgumentsExpressionSelector](node) {
-			return {
-				node,
-				messageId: MESSAGE_ID_MISSING_MESSAGE,
-				data: {
-					constructor: node.callee.name
-				}
-			};
-		},
-		[errorMessageSelector](expression) {
-			const [node] = expression.arguments;
+		[selector](expression) {
+			const constructorName = expression.callee.name;
+			const messageArgumentIndex = constructorName === 'AggregateError' ? 1 : 0;
+			const callArguments = expression.arguments;
+
+			// If message is `SpreadElement` or there is `SpreadElement` before message
+			if (callArguments.some((node, index) => index <= messageArgumentIndex && node.type === 'SpreadElement')) {
+				return;
+			}
+
+			const node = callArguments[messageArgumentIndex];
+			if (!node) {
+				return {
+					node: expression,
+					messageId: MESSAGE_ID_MISSING_MESSAGE,
+					data: {constructorName}
+				};
+			}
 
 			// These types can't be string, and `getStaticValue` may don't know the value
 			// Add more types, if issue reported
@@ -48,14 +56,14 @@ const create = context => {
 				};
 			}
 
-			const result = getStaticValue(node, context.getScope());
+			const staticResult = getStaticValue(node, context.getScope());
 
 			// We don't know the value of `message`
-			if (!result) {
+			if (!staticResult) {
 				return;
 			}
 
-			const {value} = result;
+			const {value} = staticResult;
 			if (typeof value !== 'string') {
 				return {
 					node,
