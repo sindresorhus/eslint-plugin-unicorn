9c6f2c63d5aadf436d4343a6ca09238fe38f7d41
fisker Cheung <lionkay@gmail.com>
2021-06-30 19:16:21 +0800

    Improve report location

--- rules/no-instanceof-array.js
+++ rules/no-instanceof-array.js
@@ -15,25 +15,30 @@ const selector = [
 	'[right.name="Array"]'
 ].join('');
 
-const create = context => ({
-	[selector]: node => ({
-		node,
-		messageId: MESSAGE_ID,
-		/** @param {import('eslint').Rule.RuleFixer} fixer */
-		* fix(fixer) {
+const create = context => {
+	const sourceCode = context.getSourceCode();
+
+	return {
+		[selector](node) {
 			const {left, right} = node;
-			const sourceCode = context.getSourceCode();
-
-			const range = getParenthesizedRange(left, sourceCode);
-			yield fixer.insertTextBeforeRange(range, 'Array.isArray(');
-			yield fixer.insertTextAfterRange(range, ')');
-
 			const instanceofToken = sourceCode.getTokenAfter(left, isInstanceofToken);
-			yield * replaceNodeOrTokenAndSpacesBefore(instanceofToken, '', fixer, sourceCode);
-			yield * replaceNodeOrTokenAndSpacesBefore(right, '', fixer, sourceCode);
+
+			return {
+				node: instanceofToken,
+				messageId: MESSAGE_ID,
+				/** @param {import('eslint').Rule.RuleFixer} fixer */
+				* fix(fixer) {
+					const range = getParenthesizedRange(left, sourceCode);
+					yield fixer.insertTextBeforeRange(range, 'Array.isArray(');
+					yield fixer.insertTextAfterRange(range, ')');
+
+					yield * replaceNodeOrTokenAndSpacesBefore(instanceofToken, '', fixer, sourceCode);
+					yield * replaceNodeOrTokenAndSpacesBefore(right, '', fixer, sourceCode);
+				}
+			};
 		}
-	})
-});
+	};
+};
 
 module.exports = {
 	create,
