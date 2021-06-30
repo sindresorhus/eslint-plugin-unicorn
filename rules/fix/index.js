69d78f8da7e984dbb453bc246ce6ae9e26a2975f
fisker Cheung <lionkay@gmail.com>
2021-06-30 20:42:20 +0800

    Style

--- rules/fix/index.js
+++ rules/fix/index.js
@@ -1,15 +1,16 @@
-"use strict";
+'use strict';
 
 module.exports = {
 	// Utilities
-	extendFixRange: require("./extend-fix-range.js"),
+	extendFixRange: require('./extend-fix-range.js'),
 
-	appendArgument: require("./append-argument.js"),
-	removeArgument: require("./remove-argument.js"),
-	switchNewExpressionToCallExpression: require("./switch-new-expression-to-call-expression.js"),
-	removeMemberExpressionProperty: require("./remove-member-expression-property.js"),
-	removeMethodCall: require("./remove-method-call.js"),
-	replaceTemplateElement: require("./replace-template-element.js"),
-	replaceReferenceIdentifier: require("./replace-reference-identifier.js"),
-	renameVariable: require("./rename-variable.js")
+	appendArgument: require('./append-argument.js'),
+	removeArgument: require('./remove-argument.js'),
+	switchNewExpressionToCallExpression: require('./switch-new-expression-to-call-expression.js'),
+	removeMemberExpressionProperty: require('./remove-member-expression-property.js'),
+	removeMethodCall: require('./remove-method-call.js'),
+	replaceTemplateElement: require('./replace-template-element.js'),
+	replaceReferenceIdentifier: require('./replace-reference-identifier.js'),
+	renameVariable: require('./rename-variable.js'),
+	replaceNodeOrTokenAndSpacesBefore: require('./replace-node-or-token-and-spaces-before.js')
 };
