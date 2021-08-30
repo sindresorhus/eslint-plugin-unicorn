f19f7214fbdd487cebbad533bcad20db4c01b015
Michael Novotny <manovotny@gmail.com>
2021-08-30 11:43:39 -0500

    Fixes lint errors.

--- rules/no-empty-file.js
+++ rules/no-empty-file.js
@@ -4,14 +4,14 @@ const messages = {
 	[MESSAGE_ID]: 'Empty files are not allowed.',
 };
 
+const isEmpty = node =>
+	node.type === 'EmptyStatement'
+	|| (node.type === 'ExpressionStatement' && node.expression.value === 'use strict')
+	|| (node.type === 'BlockStatement' && node.body.every(currentNode => isEmpty(currentNode)));
+
 const create = () => ({
 	Program(node) {
-		const isEmpty = (currentNode) =>
-			currentNode.type === 'EmptyStatement'
-			|| (currentNode.type === 'ExpressionStatement' && currentNode.expression.value === 'use strict')
-			|| currentNode.type === 'BlockStatement' && currentNode.body.every(isEmpty);
-
-		if (node.body.every(isEmpty)) {
+		if (node.body.every(currentNode => isEmpty(currentNode))) {
 			return {
 				node,
 				messageId: MESSAGE_ID,
