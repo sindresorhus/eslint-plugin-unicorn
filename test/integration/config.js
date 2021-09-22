7ed1a0ff38608e04741f247e19c9d375c7100f16
fisker Cheung <lionkay@gmail.com>
2021-09-22 15:13:48 +0800

    Use 'plugin:unicorn/all' in integration test (#1519)

--- test/integration/config.js
+++ test/integration/config.js
@@ -1,11 +1,4 @@
 'use strict';
-const unicorn = require('eslint-plugin-unicorn');
-
-const enableAllRules = Object.fromEntries(
-	Object.entries(unicorn.configs.recommended.rules)
-		.filter(([id]) => id.startsWith('unicorn/'))
-		.map(([id]) => [id, 'error']),
-);
 
 module.exports = {
 	root: true,
@@ -31,10 +24,8 @@ module.exports = {
 	plugins: [
 		'unicorn',
 	],
-	extends: 'plugin:unicorn/recommended',
+	extends: 'plugin:unicorn/all',
 	rules: {
-		...enableAllRules,
-
 		// This rule crashing on replace string inside `jsx` or `Unicode escape sequence`
 		'unicorn/string-content': 'off',
 	},
