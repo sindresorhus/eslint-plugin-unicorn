5f4c440fd0705952483a8d7dc360d36e5c441944
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2021-10-11 07:21:45 +0100

    Add `template-indent` rule (#1478)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- configs/recommended.js
+++ configs/recommended.js
@@ -96,6 +96,7 @@ module.exports = {
 		'unicorn/require-number-to-fixed-digits-argument': 'error',
 		'unicorn/require-post-message-target-origin': 'error',
 		'unicorn/string-content': 'off',
+		'unicorn/template-indent': 'warn',
 		'unicorn/throw-new-error': 'error',
 		...require('./conflicting-rules.js').rules,
 	},
