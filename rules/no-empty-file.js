d9f76cd8e51b3b90704290e4b608a5a77645738c
Michael Novotny <manovotny@gmail.com>
2021-11-12 12:05:14 -0600

    Splits triple-slash directive check into it's own check for better performance.

--- rules/no-empty-file.js
+++ rules/no-empty-file.js
@@ -22,8 +22,6 @@ const hasTripeSlashDirectives = comments =>
 /** @param {import('eslint').Rule.RuleContext} context */
 const create = context => {
 	const filename = context.getPhysicalFilename().toLowerCase();
-	const sourceCode = context.getSourceCode();
-	const comments = sourceCode.getAllComments();
 
 	if (!/\.(?:js|mjs|cjs|ts|mts|cts)$/.test(filename)) {
 		return {};
@@ -31,7 +29,14 @@ const create = context => {
 
 	return {
 		Program(node) {
-			if (!isEmpty(node) || hasTripeSlashDirectives(comments)) {
+			if (!isEmpty(node)) {
+				return;
+			}
+
+			const sourceCode = context.getSourceCode();
+			const comments = sourceCode.getAllComments();
+
+			if (hasTripeSlashDirectives(comments)) {
 				return;
 			}
 
