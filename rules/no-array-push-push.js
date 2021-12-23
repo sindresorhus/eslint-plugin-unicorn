10ad6994b699029af290cb5487d156df11c3872d
fisker Cheung <lionkay@gmail.com>
2021-12-24 07:30:07 +0800

    `no-array-push-push`: Ignore `process.{stdin,stdout,stderr}` (#1654)

--- rules/no-array-push-push.js
+++ rules/no-array-push-push.js
@@ -43,7 +43,15 @@ function create(context) {
 		ignore: [],
 		...context.options[0],
 	};
-	const ignoredObjects = ['stream', 'this', 'this.stream', ...ignore];
+	const ignoredObjects = [
+		'stream',
+		'this',
+		'this.stream',
+		'process.stdin',
+		'process.stdout',
+		'process.stderr',
+		...ignore,
+	];
 	const sourceCode = context.getSourceCode();
 
 	return {
