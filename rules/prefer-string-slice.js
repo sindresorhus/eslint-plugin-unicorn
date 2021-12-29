a972de9ce1723b020750caf7349b7ccdcb0e7139
fisker Cheung <lionkay@gmail.com>
2021-12-29 14:03:27 +0800

    Simplify extract `getFixArguments`

--- rules/prefer-string-slice.js
+++ rules/prefer-string-slice.js
@@ -34,6 +34,113 @@ const isLengthProperty = node => (
 	&& node.property.name === 'length'
 );
 
+function getFixArguments(node, context) {
+	const sourceCode = context.getSourceCode();
+	const method = node.callee.property.name;
+
+	let sliceArguments;
+
+	if (method === 'substr') {
+		const objectNode = node.callee.object;
+		const argumentNodes = node.arguments;
+
+		const firstArgument = argumentNodes[0] ? sourceCode.getText(argumentNodes[0]) : undefined;
+		const secondArgument = argumentNodes[1] ? sourceCode.getText(argumentNodes[1]) : undefined;
+
+		switch (argumentNodes.length) {
+			case 0: {
+				sliceArguments = [];
+				break;
+			}
+
+			case 1: {
+				sliceArguments = [firstArgument];
+				break;
+			}
+
+			case 2: {
+				if (firstArgument === '0') {
+					sliceArguments = [firstArgument];
+					if (isLiteralNumber(secondArgument) || isLengthProperty(argumentNodes[1])) {
+						sliceArguments.push(secondArgument);
+					} else if (typeof getNumericValue(argumentNodes[1]) === 'number') {
+						sliceArguments.push(Math.max(0, getNumericValue(argumentNodes[1])));
+					} else {
+						sliceArguments.push(`Math.max(0, ${secondArgument})`);
+					}
+				} else if (
+					isLiteralNumber(argumentNodes[0])
+					&& isLiteralNumber(argumentNodes[1])
+				) {
+					sliceArguments = [
+						firstArgument,
+						argumentNodes[0].value + argumentNodes[1].value,
+					];
+				} else if (
+					isNumber(argumentNodes[0], context.getScope())
+					&& isNumber(argumentNodes[1], context.getScope())
+				) {
+					sliceArguments = [firstArgument, firstArgument + ' + ' + secondArgument];
+				}
+
+				break;
+			}
+			// No default
+		}
+	} else {
+		const objectNode = node.callee.object;
+		const argumentNodes = node.arguments;
+
+		const firstArgument = argumentNodes[0] ? sourceCode.getText(argumentNodes[0]) : undefined;
+		const secondArgument = argumentNodes[1] ? sourceCode.getText(argumentNodes[1]) : undefined;
+
+		const firstNumber = argumentNodes[0] ? getNumericValue(argumentNodes[0]) : undefined;
+
+		switch (argumentNodes.length) {
+			case 0: {
+				sliceArguments = [];
+				break;
+			}
+
+			case 1: {
+				if (firstNumber !== undefined) {
+					sliceArguments = [Math.max(0, firstNumber)];
+				} else if (isLengthProperty(argumentNodes[0])) {
+					sliceArguments = [firstArgument];
+				} else {
+					sliceArguments = [`Math.max(0, ${firstArgument})`];
+				}
+
+				break;
+			}
+
+			case 2: {
+				const secondNumber = getNumericValue(argumentNodes[1]);
+
+				if (firstNumber !== undefined && secondNumber !== undefined) {
+					sliceArguments = firstNumber > secondNumber
+						? [Math.max(0, secondNumber), Math.max(0, firstNumber)]
+						: [Math.max(0, firstNumber), Math.max(0, secondNumber)];
+				} else if (firstNumber === 0 || secondNumber === 0) {
+					sliceArguments = [0, `Math.max(0, ${firstNumber === 0 ? secondArgument : firstArgument})`];
+				} else {
+				// As values aren't Literal, we can not know whether secondArgument will become smaller than the first or not, causing an issue:
+				//   .substring(0, 2) and .substring(2, 0) returns the same result
+				//   .slice(0, 2) and .slice(2, 0) doesn't return the same result
+				// There's also an issue with us now knowing whether the value will be negative or not, due to:
+				//   .substring() treats a negative number the same as it treats a zero.
+				// The latter issue could be solved by wrapping all dynamic numbers in Math.max(0, <value>), but the resulting code would not be nice
+				}
+
+				break;
+			}
+			// No default
+		}
+	}
+
+	return sliceArguments;
+}
+
 /** @param {import('eslint').Rule.RuleContext} context */
 const create = context => {
 	const sourceCode = context.getSourceCode();
@@ -48,52 +155,7 @@ const create = context => {
 				messageId: MESSAGE_ID_SUBSTR,
 			};
 
-			const firstArgument = argumentNodes[0] ? sourceCode.getText(argumentNodes[0]) : undefined;
-			const secondArgument = argumentNodes[1] ? sourceCode.getText(argumentNodes[1]) : undefined;
-
-			let sliceArguments;
-
-			switch (argumentNodes.length) {
-				case 0: {
-					sliceArguments = [];
-					break;
-				}
-
-				case 1: {
-					sliceArguments = [firstArgument];
-					break;
-				}
-
-				case 2: {
-					if (firstArgument === '0') {
-						sliceArguments = [firstArgument];
-						if (isLiteralNumber(secondArgument) || isLengthProperty(argumentNodes[1])) {
-							sliceArguments.push(secondArgument);
-						} else if (typeof getNumericValue(argumentNodes[1]) === 'number') {
-							sliceArguments.push(Math.max(0, getNumericValue(argumentNodes[1])));
-						} else {
-							sliceArguments.push(`Math.max(0, ${secondArgument})`);
-						}
-					} else if (
-						isLiteralNumber(argumentNodes[0])
-						&& isLiteralNumber(argumentNodes[1])
-					) {
-						sliceArguments = [
-							firstArgument,
-							argumentNodes[0].value + argumentNodes[1].value,
-						];
-					} else if (
-						isNumber(argumentNodes[0], context.getScope())
-						&& isNumber(argumentNodes[1], context.getScope())
-					) {
-						sliceArguments = [firstArgument, firstArgument + ' + ' + secondArgument];
-					}
-
-					break;
-				}
-				// No default
-			}
-
+			const sliceArguments = getFixArguments(node, context);
 			if (sliceArguments) {
 				const objectText = getParenthesizedText(objectNode, sourceCode);
 				const optionalMemberSuffix = node.callee.optional ? '?' : '';
@@ -114,54 +176,7 @@ const create = context => {
 				messageId: MESSAGE_ID_SUBSTRING,
 			};
 
-			const firstArgument = argumentNodes[0] ? sourceCode.getText(argumentNodes[0]) : undefined;
-			const secondArgument = argumentNodes[1] ? sourceCode.getText(argumentNodes[1]) : undefined;
-
-			const firstNumber = argumentNodes[0] ? getNumericValue(argumentNodes[0]) : undefined;
-
-			let sliceArguments;
-
-			switch (argumentNodes.length) {
-				case 0: {
-					sliceArguments = [];
-					break;
-				}
-
-				case 1: {
-					if (firstNumber !== undefined) {
-						sliceArguments = [Math.max(0, firstNumber)];
-					} else if (isLengthProperty(argumentNodes[0])) {
-						sliceArguments = [firstArgument];
-					} else {
-						sliceArguments = [`Math.max(0, ${firstArgument})`];
-					}
-
-					break;
-				}
-
-				case 2: {
-					const secondNumber = getNumericValue(argumentNodes[1]);
-
-					if (firstNumber !== undefined && secondNumber !== undefined) {
-						sliceArguments = firstNumber > secondNumber
-							? [Math.max(0, secondNumber), Math.max(0, firstNumber)]
-							: [Math.max(0, firstNumber), Math.max(0, secondNumber)];
-					} else if (firstNumber === 0 || secondNumber === 0) {
-						sliceArguments = [0, `Math.max(0, ${firstNumber === 0 ? secondArgument : firstArgument})`];
-					} else {
-					// As values aren't Literal, we can not know whether secondArgument will become smaller than the first or not, causing an issue:
-					//   .substring(0, 2) and .substring(2, 0) returns the same result
-					//   .slice(0, 2) and .slice(2, 0) doesn't return the same result
-					// There's also an issue with us now knowing whether the value will be negative or not, due to:
-					//   .substring() treats a negative number the same as it treats a zero.
-					// The latter issue could be solved by wrapping all dynamic numbers in Math.max(0, <value>), but the resulting code would not be nice
-					}
-
-					break;
-				}
-				// No default
-			}
-
+			const sliceArguments = getFixArguments(node, context);
 			if (sliceArguments) {
 				const objectText = getParenthesizedText(objectNode, sourceCode);
 				const optionalMemberSuffix = node.callee.optional ? '?' : '';
