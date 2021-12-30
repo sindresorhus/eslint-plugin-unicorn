7a4a436408f2b95e7d902e357bd5937df0042088
fisker Cheung <lionkay@gmail.com>
2021-12-30 16:04:04 +0800

    Refactor

--- rules/prefer-string-slice.js
+++ rules/prefer-string-slice.js
@@ -1,6 +1,6 @@
 'use strict';
 const {getStaticValue} = require('eslint-utils');
-const {getParenthesizedText} = require('./utils/parentheses.js');
+const {getParenthesizedText, getParenthesizedRange} = require('./utils/parentheses.js');
 const {methodCallSelector} = require('./selectors/index.js');
 const isNumber = require('./utils/is-number.js');
 const {replaceArgument} = require('./fix/index.js');
@@ -39,115 +39,107 @@ const isLengthProperty = node => (
 	&& node.property.name === 'length'
 );
 
-function * fixArguments({node, fixer, context, abort}) {
+function * fixSubstrArguments({node, fixer, context, abort}) {
 	const argumentNodes = node.arguments;
+	const [firstArgument, secondArgument] = argumentNodes;
 
-	if (argumentNodes.length === 0) {
+	if (!secondArgument) {
 		return;
 	}
 
-	if (
-		argumentNodes.length > 2
-		|| argumentNodes.some(node => node.type === 'SpreadElement')
-	) {
-		return abort();
-	}
-
-	const sourceCode = context.getSourceCode();
 	const scope = context.getScope();
-	const [firstArgument, secondArgument] = argumentNodes;
-	const [firstArgumentText, secondArgumentText] = argumentNodes.map(node => node ? getParenthesizedText(node, sourceCode) : '');
+	const sourceCode = context.getSourceCode();
+	const firstArgumentStaticResult = getStaticValue(firstArgument, scope);
+	const secondArgumentRange = getParenthesizedRange(secondArgument, sourceCode);
+	const replaceSecondArgument = text => replaceArgument(fixer, secondArgument, text, sourceCode);
 
-	const method = node.callee.property.name;
-	const replaceArgumentByIndex = (index, text) =>
-		replaceArgument(fixer, argumentNodes[index], text, sourceCode);
-	const replaceSecondArgument = text => replaceArgumentByIndex(1, text);
-
-	if (method === 'substr') {
-		switch (argumentNodes.length) {
-			case 1: {
-				return;
-			}
-
-			case 2: {
-				const firstArgumentStaticResult = getStaticValue(firstArgument, scope);
-				if (firstArgumentStaticResult && firstArgumentStaticResult.value === 0) {
-					if (isLiteralNumber(secondArgument) || isLengthProperty(secondArgument)) {
-						return;
-					}
-
-					if (typeof getNumericValue(secondArgument) === 'number') {
-						yield replaceSecondArgument(Math.max(0, getNumericValue(secondArgument)));
-						return;
-					}
-
-					yield replaceSecondArgument(`Math.max(0, ${secondArgumentText})`);
-					return;
-				}
-
-				if (argumentNodes.every(node => isLiteralNumber(node))) {
-					yield replaceSecondArgument(firstArgument.value + secondArgument.value);
-					return;
-				}
-
-				if (argumentNodes.every(node => isNumber(node, context.getScope()))) {
-					yield replaceSecondArgument(firstArgumentText + ' + ' + secondArgumentText);
-					return;
-				}
-
-				break;
-			}
-			// No default
+	if (firstArgumentStaticResult && firstArgumentStaticResult.value === 0) {
+		if (isLiteralNumber(secondArgument) || isLengthProperty(secondArgument)) {
+			return;
 		}
-	} else if (method === 'substring') {
-		const firstNumber = firstArgument ? getNumericValue(firstArgument) : undefined;
-		const replaceFirstArgument = text => replaceArgumentByIndex(0, text);
-		switch (argumentNodes.length) {
-			case 1: {
-				if (firstNumber !== undefined) {
-					yield replaceFirstArgument(Math.max(0, firstNumber));
-					return;
-				}
 
-				if (isLengthProperty(firstArgument)) {
-					return;
-				}
-
-				yield replaceFirstArgument(`Math.max(0, ${firstArgumentText})`);
-				return;
-			}
-
-			case 2: {
-				const secondNumber = getNumericValue(secondArgument);
-
-				if (firstNumber !== undefined && secondNumber !== undefined) {
-					const argumentsText = [Math.max(0, firstNumber), Math.max(0, secondNumber)];
-					if (firstNumber > secondNumber) {
-						argumentsText.reverse();
-					}
-
-					yield * argumentsText.map((text, index) => replaceArgumentByIndex(index, text));
-					return;
-				}
-
-				if (firstNumber === 0 || secondNumber === 0) {
-					yield replaceFirstArgument(0);
-					yield replaceSecondArgument(`Math.max(0, ${firstNumber === 0 ? secondArgumentText : firstArgumentText})`);
-					return;
-				}
-
-				// As values aren't Literal, we can not know whether secondArgument will become smaller than the first or not, causing an issue:
-				//   .substring(0, 2) and .substring(2, 0) returns the same result
-				//   .slice(0, 2) and .slice(2, 0) doesn't return the same result
-				// There's also an issue with us now knowing whether the value will be negative or not, due to:
-				//   .substring() treats a negative number the same as it treats a zero.
-				// The latter issue could be solved by wrapping all dynamic numbers in Math.max(0, <value>), but the resulting code would not be nice
-
-				break;
-			}
-			// No default
+		if (typeof getNumericValue(secondArgument) === 'number') {
+			yield replaceSecondArgument(Math.max(0, getNumericValue(secondArgument)));
+			return;
 		}
+
+		yield fixer.insertTextBeforeRange(secondArgumentRange, 'Math.max(0, ');
+		yield fixer.insertTextAfterRange(secondArgumentRange, ')');
+		return;
 	}
+
+	if (argumentNodes.every(node => isLiteralNumber(node))) {
+		yield replaceSecondArgument(firstArgument.value + secondArgument.value);
+		return;
+	}
+
+	if (argumentNodes.every(node => isNumber(node, context.getScope()))) {
+		const firstArgumentText = getParenthesizedText(firstArgument, sourceCode);
+
+		yield fixer.insertTextBeforeRange(secondArgumentRange, `${firstArgumentText} + `);
+		return;
+	}
+
+	return abort();
+}
+
+function * fixSubstringArguments({node, fixer, context, abort}) {
+	const sourceCode = context.getSourceCode();
+	const [firstArgument, secondArgument] = node.arguments;
+
+	const firstNumber = firstArgument ? getNumericValue(firstArgument) : undefined;
+	const firstArgumentText = getParenthesizedText(firstArgument, sourceCode);
+	const replaceFirstArgument = text => replaceArgument(fixer, firstArgument, text, sourceCode);
+
+	if (!secondArgument) {
+		if (isLengthProperty(firstArgument)) {
+			return;
+		}
+
+		if (firstNumber !== undefined) {
+			yield replaceFirstArgument(Math.max(0, firstNumber));
+			return;
+		}
+
+		const firstArgumentRange = getParenthesizedRange(firstArgument, sourceCode);
+		yield fixer.insertTextBeforeRange(firstArgumentRange, 'Math.max(0, ');
+		yield fixer.insertTextAfterRange(firstArgumentRange, ')');
+		return;
+	}
+
+	const secondNumber = getNumericValue(secondArgument);
+	const secondArgumentText = getParenthesizedText(secondArgument, sourceCode);
+	const replaceSecondArgument = text => replaceArgument(fixer, secondArgument, text, sourceCode);
+
+	if (firstNumber !== undefined && secondNumber !== undefined) {
+		const argumentsValue = [Math.max(0, firstNumber), Math.max(0, secondNumber)];
+		if (firstNumber > secondNumber) {
+			argumentsValue.reverse();
+		}
+
+		if (argumentsValue[0] !== firstNumber) {
+			yield replaceFirstArgument(argumentsValue[0]);
+		}
+
+		if (argumentsValue[1] !== secondNumber) {
+			yield replaceSecondArgument(argumentsValue[1]);
+		}
+
+		return;
+	}
+
+	if (firstNumber === 0 || secondNumber === 0) {
+		yield replaceFirstArgument(0);
+		yield replaceSecondArgument(`Math.max(0, ${firstNumber === 0 ? secondArgumentText : firstArgumentText})`);
+		return;
+	}
+
+	// As values aren't Literal, we can not know whether secondArgument will become smaller than the first or not, causing an issue:
+	//   .substring(0, 2) and .substring(2, 0) returns the same result
+	//   .slice(0, 2) and .slice(2, 0) doesn't return the same result
+	// There's also an issue with us now knowing whether the value will be negative or not, due to:
+	//   .substring() treats a negative number the same as it treats a zero.
+	// The latter issue could be solved by wrapping all dynamic numbers in Math.max(0, <value>), but the resulting code would not be nice
 
 	return abort();
 }
@@ -155,12 +147,27 @@ function * fixArguments({node, fixer, context, abort}) {
 /** @param {import('eslint').Rule.RuleContext} context */
 const create = context => ({
 	[selector](node) {
+		const method = node.callee.property.name;
+
 		return {
 			node,
-			messageId: node.callee.property.name,
+			messageId: method,
 			* fix(fixer, {abort}) {
-				yield * fixArguments({node, fixer, context, abort});
 				yield fixer.replaceText(node.callee.property, 'slice');
+
+				if (node.arguments.length === 0) {
+					return;
+				}
+
+				if (
+					node.arguments.length > 2
+					|| node.arguments.some(node => node.type === 'SpreadElement')
+				) {
+					return abort();
+				}
+
+				const fixArguments = method === 'substr' ? fixSubstrArguments : fixSubstringArguments;
+				yield * fixArguments({node, fixer, context, abort});
 			},
 		};
 	},
