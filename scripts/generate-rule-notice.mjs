b8221e604fba82c2e14a364be2fadf66f7aab97c
fisker Cheung <lionkay@gmail.com>
2022-01-04 16:34:00 +0800

    Add script

---
+++ scripts/generate-rule-notice.mjs
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+// Automatically regenerates the rule notice.
+
+import fs from 'node:fs/promises';
+import eslintPluginUnicorn from '../index.js';
+import outdent from 'outdent';
+import {getRuleNotice} from './utils.mjs';
+
+const rules = Object.entries(eslintPluginUnicorn.rules).filter(([, rule]) => !rule.meta.deprecated).map(([id]) => id);
+
+const RULE_NOTICE_START_MARK = '<!-- RULE_NOTICE_START -->';
+const RULE_NOTICE_END_MARK = '<!-- RULE_NOTICE_END -->';
+
+async function updateNotice(id) {
+	const documentationFile = new URL(`../docs/rules/${id}.md`, import.meta.url);
+	const original = await fs.readFile(documentationFile, 'utf8');
+	const notice = getRuleNotice(id);
+	const startMarkIndex = original.indexOf(RULE_NOTICE_START_MARK);
+	const endMarkIndex = original.indexOf(RULE_NOTICE_END_MARK);
+
+	if (startMarkIndex === -1) {
+		throw new Error('Rule notice start mark lost.');
+	}
+
+	if (endMarkIndex === -1) {
+		throw new Error('Rule notice end mark lost.');
+	}
+
+	const before = original.slice(0, startMarkIndex + RULE_NOTICE_START_MARK.length)
+	const after = original.slice(endMarkIndex);
+	const content = outdent`
+		${before}
+		${notice}
+		${after}
+	`;
+
+	if (content !== original) {
+		await fs.writeFile(documentationFile, content);
+	}
+}
+
+for (const id of rules) {
+	await updateNotice(id);
+}
---
+++ scripts/utils.mjs
@@ -0,0 +1,37 @@
+import eslintPluginUnicorn from '../index.js';
+
+const {rules, configs} = eslintPluginUnicorn;
+// Const recommendedRules = new Set(Object.keys(configs.rules));
+
+export function getRecommendedRules() {
+	const rules = Object.entries(configs.recommended.rules).filter(([, config]) => ['error', 'warn'].includes(config))
+		.map(([id]) => id.slice('unicorn/'.length));
+
+	return new Set(rules);
+}
+
+const MESSAGES = {
+	recommended: '✅ *This rule is part of the [recommended](https://github.com/sindresorhus/eslint-plugin-unicorn#recommended-config) config.*',
+	fixable: '🔧 *This rule is [auto-fixable](https://eslint.org/docs/user-guide/command-line-interface#fixing-problems).*',
+	fixableAndHasSuggestions: '🔧💡 *This rule is [auto-fixable](https://eslint.org/docs/user-guide/command-line-interface#fixing-problems) and provides [suggestions](https://eslint.org/docs/developer-guide/working-with-rules#providing-suggestions).*',
+	hasSuggestions: '💡 *This rule provides [suggestions](https://eslint.org/docs/developer-guide/working-with-rules#providing-suggestions).*',
+};
+
+const recommendedRules = getRecommendedRules();
+export function getRuleNotice(id) {
+	const notices = [];
+
+	if (recommendedRules.has(id)) {
+		notices.push(MESSAGES.recommended);
+	}
+
+	const {meta} = rules[id];
+
+	if (meta.fixable) {
+		notices.push(meta.hasSuggestions ? MESSAGES.fixableAndHasSuggestions : MESSAGES.fixable);
+	} else if (meta.hasSuggestions) {
+		notices.push(MESSAGES.hasSuggestions);
+	}
+
+	return notices.join('\n\n');
+}
