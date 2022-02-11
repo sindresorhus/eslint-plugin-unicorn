9de8a44c2a4972373ddf0b19bfae4a7493857e10
fisker Cheung <lionkay@gmail.com>
2022-02-11 22:04:26 +0800

    Add an internal ESLint rule `prefer-negative-boolean-attribute` (#1725)

---
+++ scripts/internal-rules/prefer-negative-boolean-attribute.js
@@ -0,0 +1,44 @@
+'use strict';
+const path = require('path');
+
+const messageId = path.basename(__filename, '.js');
+
+const shouldReport = (string, value) => {
+	const index = string.indexOf(`=${value}]`);
+
+	if (index === -1) {
+		return false;
+	}
+
+	return string[index - 1] !== '!';
+};
+
+module.exports = {
+	create(context) {
+		return {
+			'TemplateElement, Literal'(node) {
+				const string = node.value;
+				if (typeof string !== 'string') {
+					return;
+				}
+
+				for (const value of [true, false]) {
+					if (shouldReport(string, value)) {
+						context.report({
+							node,
+							messageId,
+							data: {
+								preferred: String(!value),
+							},
+						});
+					}
+				}
+			},
+		};
+	},
+	meta: {
+		messages: {
+			[messageId]: 'Prefer use `[…!={{preferred}}]` in esquery selector.',
+		},
+	},
+};
