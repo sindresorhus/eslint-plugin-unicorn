51166f471c9b1774e48cec1d0130e9d7da5fba5d
fisker Cheung <lionkay@gmail.com>
2022-04-01 18:38:32 +0800

    Add `prefer-native-coercion-functions` rule (#1767)

--- configs/recommended.js
+++ configs/recommended.js
@@ -87,6 +87,7 @@ module.exports = {
 		'unicorn/prefer-modern-dom-apis': 'error',
 		'unicorn/prefer-modern-math-apis': 'error',
 		'unicorn/prefer-module': 'error',
+		'unicorn/prefer-native-coercion-functions': 'error',
 		'unicorn/prefer-negative-index': 'error',
 		'unicorn/prefer-node-protocol': 'error',
 		'unicorn/prefer-number-properties': 'error',
