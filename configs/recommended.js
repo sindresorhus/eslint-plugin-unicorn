060638691142c280ffadc7d012ce202d9386cc2c
fisker Cheung <lionkay@gmail.com>
2022-04-29 22:10:43 +0800

    Drop support for Node.js v12

--- configs/recommended.js
+++ configs/recommended.js
@@ -105,7 +105,6 @@ module.exports = {
 		'unicorn/prefer-string-trim-start-end': 'error',
 		'unicorn/prefer-switch': 'error',
 		'unicorn/prefer-ternary': 'error',
-		// TODO: Enable this by default when targeting Node.js 14.
 		'unicorn/prefer-top-level-await': 'off',
 		'unicorn/prefer-type-error': 'error',
 		'unicorn/prevent-abbreviations': 'error',
