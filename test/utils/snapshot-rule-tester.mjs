9166534bcbbeb42267c7cc970f80eac20125632f
fisker Cheung <lionkay@gmail.com>
2022-05-26 19:26:15 +0800

    Fix linting

--- test/utils/snapshot-rule-tester.mjs
+++ test/utils/snapshot-rule-tester.mjs
@@ -159,6 +159,7 @@ class SnapshotRuleTester {
 			const {code, options, filename} = testCase;
 			const verifyConfig = getVerifyConfig(ruleId, config, testCase);
 			defineParser(linter, verifyConfig.parser);
+			// eslint-disable-next-line unicorn/consistent-function-scoping - bug
 			const runVerify = code => verify(linter, code, verifyConfig, {filename});
 
 			test(
