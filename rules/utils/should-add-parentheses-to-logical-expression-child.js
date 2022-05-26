98ccadfb91815ddaac843d6d2f938abee7b0d364
fisker Cheung <lionkay@gmail.com>
2022-05-26 19:49:46 +0800

    Coverage

--- rules/utils/should-add-parentheses-to-logical-expression-child.js
+++ rules/utils/should-add-parentheses-to-logical-expression-child.js
@@ -8,6 +8,7 @@ Check if parentheses should be added to a `node` when it's used as child of `Log
 */
 function shouldAddParenthesesToLogicalExpressionChild(node, {operator, property}) {
 	// We are not using this, but we can improve this function with it
+	/* c8 ignore next 3 */
 	if (!property) {
 		throw new Error('`property` is required.');
 	}
