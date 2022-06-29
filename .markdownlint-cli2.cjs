8128ec6d7831a15715bb2decd85ca7f937340bca
fisker Cheung <lionkay@gmail.com>
2022-06-29 11:16:36 +0800

    Switch `markdownlint-cli` to `markdownlint-cli2`

---
+++ .markdownlint-cli2.cjs
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+	config: {
+		'line-length': false,
+		'no-duplicate-heading': false,
+		'no-hard-tabs': false,
+		'ul-style': {
+			'style': 'dash'
+		}
+	},
+	ignores: [
+		'**/node_modules/**',
+		'test/snapshots/**',
+		'test/integration/fixtures/**',
+	],
+}
