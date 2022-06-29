9be8a5df0d92988a830754346dbd617574a58f1d
fisker Cheung <lionkay@gmail.com>
2022-06-29 11:21:06 +0800

    Fix js file style

--- .markdownlint-cli2.cjs
+++ .markdownlint-cli2.cjs
@@ -6,12 +6,12 @@ module.exports = {
 		'no-duplicate-heading': false,
 		'no-hard-tabs': false,
 		'ul-style': {
-			'style': 'dash'
-		}
+			style: 'dash',
+		},
 	},
 	ignores: [
 		'**/node_modules/**',
 		'test/snapshots/**',
 		'test/integration/fixtures/**',
 	],
-}
+};
