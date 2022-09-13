ff1cacf11881397eee36fc4c4ea3a52c3c7d2e26
jjangga0214 <bnbcmindnpass@gmail.com>
2022-09-13 13:53:59 +0900

    refactor: import dup

--- index.mjs
+++ index.mjs
@@ -1,31 +1,7 @@
-import createDeprecatedRules from './rules/utils/create-deprecated-rules.js';
-import {loadRules} from './rules/utils/rule.js';
-
-const deprecatedRules = createDeprecatedRules({
-	// {ruleId: ReplacementRuleId | ReplacementRuleId[]}, if no replacement, use `{ruleId: []}`
-	'import-index': [],
-	'no-array-instanceof': 'unicorn/no-instanceof-array',
-	'no-fn-reference-in-iterator': 'unicorn/no-array-callback-reference',
-	'no-reduce': 'unicorn/no-array-reduce',
-	'prefer-dataset': 'unicorn/prefer-dom-node-dataset',
-	'prefer-event-key': 'unicorn/prefer-keyboard-event-key',
-	'prefer-exponentiation-operator': 'prefer-exponentiation-operator',
-	'prefer-flat-map': 'unicorn/prefer-array-flat-map',
-	'prefer-node-append': 'unicorn/prefer-dom-node-append',
-	'prefer-node-remove': 'unicorn/prefer-dom-node-remove',
-	'prefer-object-has-own': 'prefer-object-has-own',
-	'prefer-replace-all': 'unicorn/prefer-string-replace-all',
-	'prefer-starts-ends-with': 'unicorn/prefer-string-starts-ends-with',
-	'prefer-text-content': 'unicorn/prefer-dom-node-text-content',
-	'prefer-trim-start-end': 'unicorn/prefer-string-trim-start-end',
-	'regex-shorthand': 'unicorn/better-regex',
-});
+import {rules} from './index.js';
 
 const plugin = {
-	rules: {
-		...loadRules(),
-		...deprecatedRules,
-	},
+	rules,
 };
 
 export default plugin;
