81dd4a189a56b49d6177d82078a372a776480b1b
fisker Cheung <lionkay@gmail.com>
2022-09-19 21:37:40 +0800

    Fix

--- rules/fix/add-parenthesizes-to-return-or-throw-expression.js
+++ rules/fix/add-parenthesizes-to-return-or-throw-expression.js
@@ -1,15 +1,23 @@
+const {isSemicolonToken} = require('eslint-utils')
+
 function * addParenthesizesToReturnOrThrowExpression(fixer, node, effectedNodeOrToken, sourceCode) {
 	if (node.type !== 'ReturnStatement' && node.type !== 'ThrowStatement') {
 		return;
 	}
 
 	const returnOrThrowToken = sourceCode.getFirstToken(node);
-	if (returnOrThrowToken.loc.start.line === effectedNodeOrToken.loc.start.line) {
+	if (returnOrThrowToken.loc.start.line !== effectedNodeOrToken.loc.start.line) {
 		return;
 	}
 
-	yield fixer.insertTextAfter(returnToken, ' (');
-	yield fixer.insertTextAfter(newExpression, ')');
+	yield fixer.insertTextAfter(returnOrThrowToken, ' (');
+
+	const lastToken = sourceCode.getLastToken(node);
+	if (isSemicolonToken(lastToken)) {
+		yield fixer.insertTextBefore(lastToken, ')');
+	} else {
+		yield fixer.insertTextAfter(node, ')');
+	}
 }
 
 module.exports = addParenthesizesToReturnOrThrowExpression
