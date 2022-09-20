f0e5c9579c8349fbc8da6b5280e2b24966753daa
fisker Cheung <lionkay@gmail.com>
2022-09-20 18:47:41 +0800

    Fix

--- test/integration/run-eslint.mjs
+++ test/integration/run-eslint.mjs
@@ -56,8 +56,8 @@ async function runEslint(project) {
 				babelOptions: {
 					babelrc: false,
 					configFile: false,
-					allowReturnOutsideFunction: true,
 					parserOpts: {
+						allowReturnOutsideFunction: true,
 						plugins: [
 							'jsx',
 							'exportDefaultFrom',
