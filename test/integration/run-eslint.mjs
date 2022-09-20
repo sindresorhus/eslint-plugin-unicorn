8c63c8a98b82ed0f551219d0e7663e7605dab927
fisker Cheung <lionkay@gmail.com>
2022-09-20 11:14:32 +0800

    New integration test

---
+++ test/integration/run-eslint.mjs
@@ -0,0 +1,98 @@
+import {ESLint} from 'eslint';
+import {codeFrameColumns} from '@babel/code-frame';
+import eslintPluginUnicorn from '../../index.js';
+
+class UnicornIntegrationTestError extends AggregateError {
+	name = 'UnicornIntegrationTestError';
+
+	constructor(project, errors) {
+		super(errors, `Error thrown when linting '${project.name}' project.`);
+
+		this.project = project;
+	}
+}
+
+class UnicornEslintFatalError extends SyntaxError {
+	name = 'UnicornEslintFatalError';
+
+	constructor(message, file) {
+		super(message.message);
+
+		this.eslintMessage = message;
+		this.eslintFile = file;
+	}
+
+	get details() {
+		const {source} = this.eslintFile;
+		const {line, column, message} = this.eslintMessage;
+
+		return codeFrameColumns(
+			source,
+			{start: {line, column}},
+			{message, highlightCode: true},
+		);
+	}
+}
+
+async function runEslint(project) {
+	const eslint = new ESLint({
+		cwd: project.location,
+		baseConfig: eslintPluginUnicorn.configs.all,
+		useEslintrc: false,
+		extensions: ['.js', '.mjs', '.cjs', '.ts', '.mts', '.cts', '.jsx', '.tsx', '.vue'],
+		plugins: {
+			unicorn: eslintPluginUnicorn,
+		},
+		fix: true,
+		overrideConfig: {
+			parser: '@babel/eslint-parser',
+			parserOptions: {
+				requireConfigFile: false,
+				babelOptions: {
+					babelrc: false,
+					configFile: false,
+					parserOpts: {
+						plugins: [
+							'jsx',
+							'doExpressions',
+							'exportDefaultFrom',
+						],
+					},
+				},
+			},
+			ignorePatterns: project.ignore,
+			rules: {
+				// This rule crashing on replace string inside `jsx` or `Unicode escape sequence`
+				'unicorn/string-content': 'off',
+			},
+			overrides: [
+				{
+					files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
+					parser: '@typescript-eslint/parser',
+				},
+				{
+					files: ['*.vue'],
+					parser: 'vue-eslint-parser',
+				},
+			],
+		},
+	});
+
+	const result = await eslint.lintFiles('.');
+
+	const errors = result
+		.filter(file => file.fatalErrorCount > 0)
+		.flatMap(
+			file => file.messages
+				.filter(message => message.fatal)
+				.map(message => new UnicornEslintFatalError(message, file)),
+		);
+
+	if (errors.length === 0) {
+		return;
+	}
+
+	throw new UnicornIntegrationTestError(project, errors);
+}
+
+export default runEslint;
