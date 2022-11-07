62adff03c62826a3a90092e5a37eb1b3f1bdaa90
fisker Cheung <lionkay@gmail.com>
2022-11-07 17:19:36 +0800

    Valid tests

--- test/prefer-set-size.mjs
+++ test/prefer-set-size.mjs
@@ -5,15 +5,16 @@ const {test} = getTester(import.meta);
 
 test.snapshot({
 	valid: [
-		// 'new Set(foo).size',
-		// 'for (const foo of bar) console.log([...foo].length)',
-		// '[...new Set(array), foo].length',
-		// '[foo, ...new Set(array), ].length',
-		// '[...new Set(array)].notLength',
-		// '[...new Set(array)].length()',
-		// '[...new Set(array)]?.length',
-		// '[...new Set(array)][length]',
-		// '[...new Set(array)]["length"]',
+		'new Set(foo).size',
+		'for (const foo of bar) console.log([...foo].length)',
+		'[...new Set(array), foo].length',
+		'[foo, ...new Set(array), ].length',
+		'[...new Set(array)].notLength',
+		'[...new Set(array)]?.length',
+		'[...new Set(array)][length]',
+		'[...new Set(array)]["length"]',
+		'[...new NotSet(array)].length',
+		'[...Set(array)].length',
 	],
 	invalid: [
 		'[...new Set(array)].length',
