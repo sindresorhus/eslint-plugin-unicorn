84700c5a30754d2cac5f3590856b4920884be8d9
fisker Cheung <lionkay@gmail.com>
2022-11-09 16:53:17 +0800

    Add basic fix

--- rules/no-negated-condition.js
+++ rules/no-negated-condition.js
@@ -4,7 +4,8 @@ https://github.com/eslint/eslint/blob/5c39425fc55ecc0b97bbd07ac22654c0eb4f789c/l
 */
 'use strict';
 const {matches} = require('./selectors/index.js');
-const {} = require('./fix/index.js');
+const { removeParentheses } = require('./fix/index.js');
+const { getParenthesizedRange, getParenthesizedText } = require('./utils/parentheses.js');
 
 const MESSAGE_ID= 'no-negated-condition';
 const messages = {
@@ -23,13 +24,59 @@ const selector = [
 	]),
 ].join('');
 
+function * convertNegatedCondition(fixer, node, sourceCode) {
+	const {test} = node;
+	if (test.type === 'UnaryExpression') {
+		const token = sourceCode.getFirstToken(test);
+		yield * removeParentheses(test, fixer, sourceCode);
+		yield fixer.remove(token);
+		return;
+	}
+
+	const token = sourceCode.getTokenAfter(
+		test.left,
+		token => token.type === "Punctuator" && token.value === test.operator,
+	);
+
+	yield fixer.replaceText(token, '=' + token.value.slice(1));
+}
+
+function * swapConsequentAndAlternate(fixer, node, sourceCode) {
+	const [consequent, alternate] = [
+		node.consequent,
+		node.alternate
+	].map(node => {
+		const range = getParenthesizedRange(node, sourceCode);
+		return {
+			range,
+			text: sourceCode.text.slice(...range),
+		}
+	});
+
+	if (consequent.text === alternate.text) {
+		return;
+	}
+
+	yield fixer.replaceTextRange(consequent.range, alternate.text);
+	yield fixer.replaceTextRange(alternate.range, consequent.text);
+}
+
 /** @param {import('eslint').Rule.RuleContext} context */
 const create = context => {
 	return {
 		[selector](node) {
 			return {
 				node: node.test,
-				messageId: MESSAGE_ID
+				messageId: MESSAGE_ID,
+				/** @param {import('eslint').Rule.RuleFixer} fixer */
+				* fix(fixer) {
+					const sourceCode = context.getSourceCode();
+					yield * convertNegatedCondition(fixer, node, sourceCode);
+					yield * swapConsequentAndAlternate(fixer, node, sourceCode);
+
+					// TODO: Handle edge cases: ASI, space, return argument, parentheses
+				}
+
 				// /** @param {import('eslint').Rule.RuleFixer} fixer */
 				// fix: fixer => fixer.replaceText(node, '\'🦄\''),
 			};
