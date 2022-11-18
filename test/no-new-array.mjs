f64a4a873307d588ae81c9fa50c443c98b5ed348
fisker Cheung <lionkay@gmail.com>
2022-11-18 20:14:44 +0800

    Use snapshot tester in `no-new-array` (#1974)

--- test/no-new-array.mjs
+++ test/no-new-array.mjs
@@ -3,22 +3,7 @@ import {getTester} from './utils/test.mjs';
 
 const {test} = getTester(import.meta);
 
-const MESSAGE_ID_ERROR = 'error';
-const MESSAGE_ID_LENGTH = 'array-length';
-const MESSAGE_ID_ONLY_ELEMENT = 'only-element';
-const MESSAGE_ID_SPREAD = 'spread';
-
-const suggestionCase = ({code, suggestions}) => ({
-	code,
-	errors: [
-		{
-			messageId: MESSAGE_ID_ERROR,
-			suggestions,
-		},
-	],
-});
-
-test({
+test.snapshot({
 	valid: [
 		'const array = Array.from({length: 1})',
 
@@ -31,100 +16,6 @@ test({
 		// `unicorn/new-for-builtins` cases
 		'const array = Array(1)',
 	],
-	invalid: [
-		suggestionCase({
-			code: 'const array = new Array(foo)',
-			suggestions: [
-				{
-					messageId: MESSAGE_ID_LENGTH,
-					output: 'const array = Array.from({length: foo})',
-				},
-				{
-					messageId: MESSAGE_ID_ONLY_ELEMENT,
-					output: 'const array = [foo]',
-				},
-			],
-		}),
-		suggestionCase({
-			code: 'const array = new Array(length)',
-			suggestions: [
-				{
-					messageId: MESSAGE_ID_LENGTH,
-					output: 'const array = Array.from({length})',
-				},
-				{
-					messageId: MESSAGE_ID_ONLY_ELEMENT,
-					output: 'const array = [length]',
-				},
-			],
-		}),
-		suggestionCase({
-			code: outdent`
-				const foo = []
-				new Array(bar).forEach(baz)
-			`,
-			suggestions: [
-				{
-					messageId: MESSAGE_ID_LENGTH,
-					output: outdent`
-						const foo = []
-						Array.from({length: bar}).forEach(baz)
-					`,
-				},
-				{
-					messageId: MESSAGE_ID_ONLY_ELEMENT,
-					output: outdent`
-						const foo = []
-						;[bar].forEach(baz)
-					`,
-				},
-			],
-		}),
-		...[
-			'...[foo]',
-			'...foo',
-			'...[...foo]',
-			// The following cases we can know the result, but we are not auto-fixing them
-			'...[1]',
-			'...["1"]',
-			'...[1, "1"]',
-		].map(argumentText => {
-			const code = `const array = new Array(${argumentText})`;
-			return {
-				code,
-				errors: [
-					{
-						messageId: MESSAGE_ID_ERROR,
-						suggestions: [
-							{
-								messageId: MESSAGE_ID_SPREAD,
-								output: `const array = [${argumentText}]`,
-							},
-						],
-					},
-				],
-			};
-		}),
-		suggestionCase({
-			code: outdent`
-				const foo = []
-				new Array(...bar).forEach(baz)
-			`,
-			suggestions: [
-				{
-					messageId: MESSAGE_ID_SPREAD,
-					output: outdent`
-						const foo = []
-						;[...bar].forEach(baz)
-					`,
-				},
-			],
-		}),
-	],
-});
-
-test.snapshot({
-	valid: [],
 	invalid: [
 		'const array = new Array(1)',
 		// This is actually `[]`, but we fix to `Array.from({length: zero})`
@@ -196,5 +87,24 @@ test.snapshot({
 		'new Array(unknown ? foo : 1)',
 		'new Array(unknown ? 1 : foo)',
 		'new Array(++foo)',
+		'const array = new Array(foo)',
+		'const array = new Array(length)',
+		outdent`
+			const foo = []
+			new Array(bar).forEach(baz)
+		`,
+		...[
+			'...[foo]',
+			'...foo',
+			'...[...foo]',
+			// The following cases we can know the result, but we are not auto-fixing them
+			'...[1]',
+			'...["1"]',
+			'...[1, "1"]',
+		].map(argumentText => `const array = new Array(${argumentText})`),
+		outdent`
+			const foo = []
+			new Array(...bar).forEach(baz)
+		`,
 	],
 });
