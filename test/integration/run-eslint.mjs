bde528c5fa3c65d66153481bf0c8babd36b51be4
fisker Cheung <lionkay@gmail.com>
2023-02-01 20:14:08 +0800

    Fix

--- test/integration/run-eslint.mjs
+++ test/integration/run-eslint.mjs
@@ -64,7 +64,7 @@ async function runEslint(project) {
 						plugins: [
 							'jsx',
 							'exportDefaultFrom',
-							...babelPlugins,
+							...project.babelPlugins,
 						],
 					},
 				},
