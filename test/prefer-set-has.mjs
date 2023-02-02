1467cca5d8f876fd8887ea3a4d3d4e7c562c32a7
fisker Cheung <lionkay@gmail.com>
2023-02-02 16:12:32 +0800

    Fix test

--- test/prefer-set-has.mjs
+++ test/prefer-set-has.mjs
@@ -345,19 +345,23 @@ test.snapshot({
 			}
 		`),
 		// Not MemberExpression
-		...methodsReturnsArray.map(method => outdent`
-			const foo = ${method}();
-			function unicorn() {
-				return foo.includes(1);
-			}
-		`),
+		...methodsReturnsArray
+			.filter(method => method !== 'with')
+			.map(method => outdent`
+				const foo = ${method}();
+				function unicorn() {
+					return foo.includes(1);
+				}
+			`),
 		// Computed
-		...methodsReturnsArray.map(method => outdent`
-			const foo = bar[${method}]();
-			function unicorn() {
-				return foo.includes(1);
-			}
-		`),
+		...methodsReturnsArray
+			.filter(method => method !== 'with')
+			.map(method => outdent`
+				const foo = bar[${method}]();
+				function unicorn() {
+					return foo.includes(1);
+				}
+			`),
 		// Not `Identifier`
 		...methodsReturnsArray.map(method => outdent`
 			const foo = bar["${method}"]();
