c4d4be637f3b7a51827620c682d94f5470744cd0
fisker Cheung <lionkay@gmail.com>
2023-02-02 10:57:42 +0800

    Add `fix-snapshot-test`

---
+++ scripts/internal-rules/fix-snapshot-test.js
@@ -0,0 +1,91 @@
+'use strict';
+const {
+	isCommaToken,
+} = require('@eslint-community/eslint-utils');
+const {matches, methodCallSelector} = require('../../rules/selectors/index.js');
+const toLocation = require('../../rules/utils/to-location.js');
+
+const MESSAGE_ID_DISALLOWED_PROPERTY = 'disallow-property'
+const MESSAGE_ID_NO_SINGLE_CODE_OBJECT = 'use-string'
+const messages = {
+	[MESSAGE_ID_DISALLOWED_PROPERTY]: '"{{name}}" not allowed.',
+	[MESSAGE_ID_NO_SINGLE_CODE_OBJECT]: 'Use string instead of object with "code".'
+}
+
+// Top-level `test.snapshot({invalid: []})`
+const selector = [
+	'Program > ExpressionStatement.body > .expression',
+	// `test.snapshot()`
+	methodCallSelector({
+		argumentsLength: 1,
+		object: 'test',
+		method: 'snapshot',
+	}),
+	' > ObjectExpression.arguments:first-child',
+	/*
+	test.snapshot({
+		invalid: [], <- Property
+	})
+	*/
+	' > Property.properties',
+	'[computed!=true]',
+	'[method!=true]',
+	'[shorthand!=true]',
+	'[kind="init"]',
+	'[key.type="Identifier"]',
+	'[key.name="invalid"]',
+
+
+	' > ArrayExpression.value',
+	' > ObjectExpression.elements',
+	' > Property.properties[computed!=true][key.type="Identifier"]',
+].join('');
+
+function * removeObjectProperty(node, fixer, sourceCode) {
+	yield fixer.remove(node)
+	const nextToken = sourceCode.getTokenAfter(node);
+	if (isCommaToken(nextToken)) {
+		yield fixer.remove(nextToken)
+	}
+}
+
+module.exports = {
+	create(context) {
+		const sourceCode = context.getSourceCode();
+
+		return {
+			[selector](propertyNode) {
+				const {key} = propertyNode;
+
+				switch (key.name) {
+					case 'errors':
+					case 'output': {
+						context.report({
+							node: key,
+							messageId: MESSAGE_ID_DISALLOWED_PROPERTY,
+							data: {name: key.name},
+							fix: fixer => removeObjectProperty(propertyNode, fixer, sourceCode)
+						});
+						break;
+					}
+					case 'code': {
+						const testCase = propertyNode.parent;
+						if (testCase.properties.length === 1) {
+							context.report({
+								node: testCase,
+								messageId: MESSAGE_ID_NO_SINGLE_CODE_OBJECT,
+								fix: fixer => fixer.replaceText(testCase, sourceCode.getText(propertyNode.value))
+							});
+						}
+						break;
+					}
+					// No default
+				}
+			},
+		};
+	},
+	meta: {
+		fixable: 'code',
+		messages,
+	},
+};
--- scripts/internal-rules/index.js
+++ scripts/internal-rules/index.js
@@ -1,18 +1,47 @@
 'use strict';
 
+const path = require('node:path');
 const pluginName = 'internal-rules';
+const TEST_DIRECTORIES = [
+	path.join(__dirname, '../../test')
+]
+const RULES_DIRECTORIES = [
+	__dirname,
+	path.join(__dirname, '../../rules')
+]
 
 const rules = [
-	'prefer-negative-boolean-attribute',
-	'prefer-disallow-over-forbid',
+	{id: 'fix-snapshot-test', directories: TEST_DIRECTORIES},
+	{id: 'prefer-disallow-over-forbid', directories: RULES_DIRECTORIES},
+	{id: 'prefer-negative-boolean-attribute', directories: RULES_DIRECTORIES},
 ];
 
+const isFileInsideDirectory = (filename, directory) => filename.startsWith(directory + path.sep);
+
 module.exports = {
-	rules: Object.fromEntries(rules.map(id => [id, require(`./${id}.js`)])),
+	rules: Object.fromEntries(
+		rules.map(({id, directories}) => {
+			const rule = require(`./${id}.js`);
+			return [
+				id,
+				{
+					...rule,
+					create(context) {
+						const filename = context.getPhysicalFilename();
+						if (directories.every(directory => !isFileInsideDirectory(filename, directory))) {
+							return {}
+						}
+
+						return rule.create(context);
+					}
+				}
+			];
+		})
+	),
 	configs: {
 		all: {
 			plugins: [pluginName],
-			rules: Object.fromEntries(rules.map(id => [`${pluginName}/${id}`, 'error'])),
+			rules: Object.fromEntries(rules.map(({id}) => [`${pluginName}/${id}`, 'error'])),
 		},
 	},
 };
