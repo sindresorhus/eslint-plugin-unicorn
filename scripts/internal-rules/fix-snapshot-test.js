0871f580e6f74b4f4c89f9c236550db999a298ab
fisker Cheung <lionkay@gmail.com>
2023-02-02 11:33:04 +0800

    Don't auto fix by default

--- scripts/internal-rules/fix-snapshot-test.js
+++ scripts/internal-rules/fix-snapshot-test.js
@@ -1,4 +1,5 @@
 'use strict';
+const assert = require('node:assert');
 const {
 	isCommaToken,
 } = require('@eslint-community/eslint-utils');
@@ -6,9 +7,11 @@ const {methodCallSelector} = require('../../rules/selectors/index.js');
 
 const MESSAGE_ID_DISALLOWED_PROPERTY = 'disallow-property';
 const MESSAGE_ID_NO_SINGLE_CODE_OBJECT = 'use-string';
+const MESSAGE_ID_REMOVE_FIX_MARK_COMMENT = 'remove-fix-mark';
 const messages = {
-	[MESSAGE_ID_DISALLOWED_PROPERTY]: '"{{name}}" not allowed.',
-	[MESSAGE_ID_NO_SINGLE_CODE_OBJECT]: 'Use string instead of object with "code".',
+	[MESSAGE_ID_DISALLOWED_PROPERTY]: '"{{name}}" not allowed.{{autoFixEnableTip}}',
+	[MESSAGE_ID_NO_SINGLE_CODE_OBJECT]: 'Use string instead of object with "code".{{autoFixEnableTip}}',
+	[MESSAGE_ID_REMOVE_FIX_MARK_COMMENT]: 'This comment should be removed.',
 };
 
 // Top-level `test.snapshot({invalid: []})`
@@ -49,6 +52,24 @@ function * removeObjectProperty(node, fixer, sourceCode) {
 	}
 }
 
+// The fix deletes lots of code, disabled by default
+function getFixComment(propertyNode, sourceCode) {
+	const snapshotTestCall = propertyNode.parent.parent.parent.parent.parent;
+	assert.ok(snapshotTestCall.type === 'CallExpression');
+	const comment = sourceCode.getTokenBefore(snapshotTestCall, {includeComments: true});
+
+	if (
+		(comment?.type === 'Block' || comment?.type === 'Line')
+		&& comment.value.trim().toLowerCase() === 'fix'
+		&& (
+			comment.loc.start.line === snapshotTestCall.loc.start.line
+			|| comment.loc.start.line === snapshotTestCall.loc.start.line - 1
+		)
+	) {
+		return comment;
+	}
+}
+
 module.exports = {
 	create(context) {
 		const sourceCode = context.getSourceCode();
@@ -56,6 +77,10 @@ module.exports = {
 		return {
 			[selector](propertyNode) {
 				const {key} = propertyNode;
+				const fixMarkComment = getFixComment(propertyNode, sourceCode);
+				const autoFixEnableTip = fixMarkComment
+					? ''
+					: ' Put /* fix */ before `test.snapshot()` to enable auto-fix.';
 
 				switch (key.name) {
 					case 'errors':
@@ -63,8 +88,11 @@ module.exports = {
 						context.report({
 							node: key,
 							messageId: MESSAGE_ID_DISALLOWED_PROPERTY,
-							data: {name: key.name},
-							fix: fixer => removeObjectProperty(propertyNode, fixer, sourceCode),
+							data: {name: key.name, autoFixEnableTip},
+							fix: fixMarkComment && sourceCode.getCommentsInside(propertyNode).length === 0
+								? fixer => removeObjectProperty(propertyNode, fixer, sourceCode)
+								: undefined
+							,
 						});
 						break;
 					}
@@ -72,15 +100,21 @@ module.exports = {
 					case 'code': {
 						const testCase = propertyNode.parent;
 						if (testCase.properties.length === 1) {
+							const commentsCount = sourceCode.getCommentsInside(testCase).length
+								- sourceCode.getCommentsInside(propertyNode).length
 							context.report({
 								node: testCase,
+								data: {autoFixEnableTip},
 								messageId: MESSAGE_ID_NO_SINGLE_CODE_OBJECT,
-								fix: fixer => fixer.replaceText(testCase, sourceCode.getText(propertyNode.value)),
+								fix: fixMarkComment && commentsCount === 0
+									? fixer => fixer.replaceText(testCase, sourceCode.getText(propertyNode.value))
+									: undefined,
 							});
 						}
 
 						break;
 					}
+
 					// No default
 				}
 			},
