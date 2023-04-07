80fa81650ca248ad4d3d63fec9e6aa9de8b3a118
fisker <lionkay@gmail.com>
2023-04-07 21:02:37 +0800

    `prefer-dom-node-append`: Check optional chaining

--- rules/utils/is-value-not-usable.js
+++ rules/utils/is-value-not-usable.js
@@ -1,3 +1,8 @@
 'use strict';
 
-module.exports = ({parent}) => !parent || parent.type === 'ExpressionStatement';
+module.exports = (node) =>
+	node.parent.type === 'ExpressionStatement'
+	|| (
+		node.parent.type === 'ChainExpression'
+		&& node.parent.parent.type === 'ExpressionStatement'
+	);
