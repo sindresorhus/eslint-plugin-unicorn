ef9ab3f4bc42cd20651ad4bed4ceb9baf5ef2e5b
fisker Cheung <lionkay@gmail.com>
2023-04-11 17:41:04 +0800

    Fix

--- rules/error-message.js
+++ rules/error-message.js
@@ -28,7 +28,7 @@ const selector = callOrNewExpressionSelector([
 /** @param {import('eslint').Rule.RuleContext} context */
 const create = context => ({
 	[selector](expression) {
-		const scope = context.getSourceCode().getScope(node);
+		const scope = context.getSourceCode().getScope(expression);
 		if (isShadowed(scope, expression.callee)) {
 			return;
 		}
