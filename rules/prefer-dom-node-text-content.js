2fa9941577fe4c372320d85822f6f950890ce9de
fisker Cheung <lionkay@gmail.com>
2023-05-12 13:31:28 +0800

    Add `isMemberExpression` to replace selectors (#2093)

--- rules/prefer-dom-node-text-content.js
+++ rules/prefer-dom-node-text-content.js
@@ -1,5 +1,5 @@
 'use strict';
-const {memberExpressionSelector} = require('./selectors/index.js');
+const {isMemberExpression} = require('./ast/index.js');
 
 const ERROR = 'error';
 const SUGGESTION = 'suggestion';
@@ -8,7 +8,6 @@ const messages = {
 	[SUGGESTION]: 'Switch to `.textContent`.',
 };
 
-const memberExpressionPropertySelector = `${memberExpressionSelector({property: 'innerText', includeOptional: true})} > .property`;
 const destructuringSelector = [
 	'ObjectPattern',
 	' > ',
@@ -22,7 +21,18 @@ const destructuringSelector = [
 
 /** @param {import('eslint').Rule.RuleContext} context */
 const create = () => ({
-	[memberExpressionPropertySelector](node) {
+	MemberExpression(memberExpression) {
+		if (
+			!isMemberExpression(memberExpression, {
+				property: 'innerText',
+				computed: false,
+			})
+		) {
+			return;
+		}
+
+		const node = memberExpression.property;
+
 		return {
 			node,
 			messageId: ERROR,
