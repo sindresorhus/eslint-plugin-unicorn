468782c59749674c438b5042ed7992db33618ddd
fisker <lionkay@gmail.com>
2023-05-15 18:22:35 +0800

    `prefer-array-flat-map`: Use simple selector

--- rules/prefer-array-flat-map.js
+++ rules/prefer-array-flat-map.js
@@ -1,6 +1,6 @@
 'use strict';
 const {isNodeMatches} = require('./utils/is-node-matches.js');
-const {methodCallSelector, matches} = require('./selectors/index.js');
+const {isMethodCall} = require('./ast/index.js');
 const {removeMethodCall} = require('./fix/index.js');
 
 const MESSAGE_ID = 'prefer-array-flat-map';
@@ -8,20 +8,35 @@ const messages = {
 	[MESSAGE_ID]: 'Prefer `.flatMap(…)` over `.map(…).flat()`.',
 };
 
-const selector = [
-	methodCallSelector('flat'),
-	matches([
-		'[arguments.length=0]',
-		'[arguments.length=1][arguments.0.type="Literal"][arguments.0.raw="1"]',
-	]),
-	methodCallSelector({path: 'callee.object', method: 'map'}),
-].join('');
-
 const ignored = ['React.Children', 'Children'];
 
 /** @param {import('eslint').Rule.RuleContext} context */
 const create = context => ({
-	[selector](flatCallExpression) {
+	CallExpression(callExpression) {
+		if (!(
+			isMethodCall(callExpression, {
+				method: 'flat',
+				optionalCall: false,
+				optionalMember: false,
+			})
+			&& (
+				callExpression.arguments.length === 0
+				|| (
+					callExpression.arguments.length === 1
+					&& callExpression.arguments[0].type === 'Literal'
+					&& callExpression.arguments[0].raw === '1'
+				)
+			)
+			&& isMethodCall(callExpression.callee.object, {
+				method: 'map',
+				optionalCall: false,
+				optionalMember: false,
+			})
+		)) {
+			return;
+		}
+
+		const flatCallExpression = callExpression;
 		const mapCallExpression = flatCallExpression.callee.object;
 		if (isNodeMatches(mapCallExpression.callee.object, ignored)) {
 			return;
