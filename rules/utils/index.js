80c81bb295adb39aa66fcc2671aff919ccc484a1
fisker Cheung <lionkay@gmail.com>
2023-05-17 13:22:48 +0800

    Refactor to use simple selectors (#2119)

--- rules/utils/index.js
+++ rules/utils/index.js
@@ -12,15 +12,18 @@ const {
 	isObjectPrototypeProperty,
 } = require('./array-or-object-prototype-property.js');
 const {isNodeMatches, isNodeMatchesNameOrPath} = require('./is-node-matches.js');
+const {isBooleanNode, getBooleanAncestor} = require('./boolean.js');
 
 module.exports = {
 	escapeString: require('./escape-string.js'),
+	getBooleanAncestor,
 	getParentheses,
 	getParenthesizedRange,
 	getParenthesizedText,
 	getParenthesizedTimes,
 	getVariableIdentifiers: require('./get-variable-identifiers.js'),
 	isArrayPrototypeProperty,
+	isBooleanNode,
 	isNodeMatches,
 	isNodeMatchesNameOrPath,
 	isNodeValueNotDomNode: require('./is-node-value-not-dom-node.js'),
@@ -29,6 +32,7 @@ module.exports = {
 	isOnSameLine: require('./is-on-same-line.js'),
 	isParenthesized,
 	isSameIdentifier: require('./is-same-identifier.js'),
+	isSameReference: require('./is-same-reference.js'),
 	isShadowed: require('./is-shadowed.js'),
 	isValueNotUsable: require('./is-value-not-usable.js'),
 	needsSemicolon: require('./needs-semicolon.js'),
