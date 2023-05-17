ac8578f9b0d70f75827b2600c73a0288a755231c
fisker <lionkay@gmail.com>
2023-05-17 13:56:32 +0800

    Clean

--- rules/selectors/index.js
+++ rules/selectors/index.js
@@ -5,7 +5,6 @@ module.exports = {
 	matches: require('./matches-any.js'),
 	not: require('./negation.js'),
 
-	memberExpressionSelector: require('./member-expression-selector.js'),
 	methodCallSelector: require('./method-call-selector.js'),
 	callExpressionSelector: require('./call-or-new-expression-selector.js').callExpressionSelector,
 	newExpressionSelector: require('./call-or-new-expression-selector.js').newExpressionSelector,
