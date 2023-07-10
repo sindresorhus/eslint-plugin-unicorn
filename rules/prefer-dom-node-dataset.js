716c1baf073b053aa05eb9156119aa51d92bf420
fisker <lionkay@gmail.com>
2023-07-10 20:34:17 +0800

    Refactor

--- rules/prefer-dom-node-dataset.js
+++ rules/prefer-dom-node-dataset.js
@@ -10,42 +10,15 @@ const messages = {
 
 const dashToCamelCase = string => string.replace(/-[a-z]/g, s => s[1].toUpperCase());
 
-/** @param {import('eslint').Rule.RuleContext} context */
-const create = context => ({
-	CallExpression(node) {
-		if (!(
-			(
-				isMethodCall(node, {
-					method: 'setAttribute',
-					argumentsLength: 2,
-					optionalCall: false,
-					optionalMember: false,
-				})
-				|| isMethodCall(node, {
-					methods: ['getAttribute', 'removeAttribute', 'hasAttribute'],
-					argumentsLength: 1,
-					optionalCall: false,
-					optionalMember: false,
-				})
-			)
-			&& isStringLiteral(node.arguments[0])
-		)) {
-			return;
-		}
+function getFix({context, callExpression}) {
+	const method = callExpression.callee.property.name;
+	const [nameNode] = callExpression.arguments;
+	const name = dashToCamelCase(nameNode.value.toLowerCase().slice(5));
 
-		const [nameNode] = node.arguments;
-		const attributeName = nameNode.value.toLowerCase();
-
-		if (!attributeName.startsWith('data-')) {
-			return;
-		}
-
-		const method = node.callee.property.name;
-		const name = dashToCamelCase(attributeName.slice(5));
-
+	return (fixer) => {
 		const {sourceCode} = context;
 		let text = '';
-		const datasetText = `${sourceCode.getText(node.callee.object)}.dataset`;
+		const datasetText = `${sourceCode.getText(callExpression.callee.object)}.dataset`;
 		switch (method) {
 			case 'setAttribute':
 			case 'getAttribute':
@@ -53,7 +26,7 @@ const create = context => ({
 				text = isIdentifierName(name) ? `.${name}` : `[${escapeString(name, nameNode.raw.charAt(0))}]`;
 				text = `${datasetText}${text}`;
 				if (method === 'setAttribute') {
-					text += ` = ${sourceCode.getText(node.arguments[1])}`;
+					text += ` = ${sourceCode.getText(callExpression.arguments[1])}`;
 				} else if (method === 'removeAttribute') {
 					text = `delete ${text}`;
 				}
@@ -72,11 +45,44 @@ const create = context => ({
 			// No default
 		}
 
+		return fixer.replaceText(callExpression, text)
+	};
+}
+
+/** @param {import('eslint').Rule.RuleContext} context */
+const create = context => ({
+	CallExpression(callExpression) {
+		if (!(
+			(
+				isMethodCall(callExpression, {
+					method: 'setAttribute',
+					argumentsLength: 2,
+					optionalCall: false,
+					optionalMember: false,
+				})
+				|| isMethodCall(callExpression, {
+					methods: ['getAttribute', 'removeAttribute', 'hasAttribute'],
+					argumentsLength: 1,
+					optionalCall: false,
+					optionalMember: false,
+				})
+			)
+			&& isStringLiteral(callExpression.arguments[0])
+		)) {
+			return;
+		}
+
+		const attributeName = callExpression.arguments[0].value.toLowerCase();
+
+		if (!attributeName.startsWith('data-')) {
+			return;
+		}
+
 		return {
-			node,
+			node: callExpression,
 			messageId: MESSAGE_ID,
-			data: {method},
-			fix: fixer => fixer.replaceText(node, text),
+			data: {method: callExpression.callee.property.name},
+			fix: getFix({context, callExpression}),
 		};
 	},
 });
