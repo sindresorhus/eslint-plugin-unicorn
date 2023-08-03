bf03beaf2633a4f93bae0c46ef622ee316de6e92
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2023-08-03 11:32:03 -0700

    fix: lint errors

--- configs/unopinionated.js
+++ configs/unopinionated.js
@@ -5,11 +5,11 @@ module.exports = {
 	...baseConfigs,
 	rules: {
 		...Object.fromEntries(Object.entries(rules)
-		.filter(([ruleId, severity]) => severity === 'error')
+			.filter(([_ruleId, severity]) => severity === 'error'),
 		),
 		'unicorn/no-array-for-each': 'off',
 		'unicorn/no-array-reduce': 'off',
 		'unicorn/no-null': 'off',
-		'unicorn/prefer-switch': 'off'
+		'unicorn/prefer-switch': 'off',
 	},
 };
