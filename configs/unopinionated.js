c2ca0f3e49497b4605622d8877bb23e7d29cb69d
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2023-08-02 19:47:39 -0700

    added opinionated preset

---
+++ configs/unopinionated.js
@@ -0,0 +1,15 @@
+'use strict';
+const {rules, ...baseConfigs} = require('./recommended.js');
+
+module.exports = {
+	...baseConfigs,
+	rules: {
+		...Object.fromEntries(Object.entries(rules)
+		.filter(([ruleId, severity]) => severity === 'error')
+		),
+		'unicorn/no-array-for-each': 'off',
+		'unicorn/no-array-reduce': 'off',
+		'unicorn/no-null': 'off',
+		'unicorn/prefer-switch': 'off'
+	},
+};
