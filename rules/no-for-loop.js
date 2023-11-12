b3d70f015bef06da1639fad2a08fc7acbb77987b
virtuallyunknown <dechobarca@gmail.com>
2023-11-12 10:00:12 +0200

    fix: lint errors

--- rules/no-for-loop.js
+++ rules/no-for-loop.js
@@ -421,7 +421,7 @@ module.exports = {
 			description: 'Do not use a `for` loop that can be replaced with a `for-of` loop.',
 		},
 		fixable: 'code',
-		messages,
 		hasSuggestions: true,
+		messages,
 	},
 };
