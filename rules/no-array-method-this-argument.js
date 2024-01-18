797caee5e0206156be980dbcb1b41dbebbdc4ff2
fisker Cheung <lionkay@gmail.com>
2024-01-19 00:53:35 +0800

    `no-array-method-this-argument`: Check `Array.from()` (#2262)

--- rules/no-array-method-this-argument.js
+++ rules/no-array-method-this-argument.js
@@ -7,11 +7,13 @@ const {isNodeMatches} = require('./utils/is-node-matches.js');
 const {isNodeValueNotFunction} = require('./utils/index.js');
 const {isMethodCall} = require('./ast/index.js');
 
-const ERROR = 'error';
+const ERROR_PROTOTYPE_METHOD = 'error-prototype-method';
+const ERROR_STATIC_METHOD = 'error-static-method';
 const SUGGESTION_BIND = 'suggestion-bind';
 const SUGGESTION_REMOVE = 'suggestion-remove';
 const messages = {
-	[ERROR]: 'Do not use the `this` argument in `Array#{{method}}()`.',
+	[ERROR_PROTOTYPE_METHOD]: 'Do not use the `this` argument in `Array#{{method}}()`.',
+	[ERROR_STATIC_METHOD]: 'Do not use the `this` argument in `Array.{{method}}()`.',
 	[SUGGESTION_REMOVE]: 'Remove the second argument.',
 	[SUGGESTION_BIND]: 'Use a bound function.',
 };
@@ -70,107 +72,141 @@ const ignored = [
 	'underscore.some',
 ];
 
-function removeThisArgument(callExpression, sourceCode) {
-	return fixer => removeArgument(fixer, callExpression.arguments[1], sourceCode);
+function removeThisArgument(thisArgumentNode, sourceCode) {
+	return fixer => removeArgument(fixer, thisArgumentNode, sourceCode);
 }
 
-function useBoundFunction(callExpression, sourceCode) {
+function useBoundFunction(callbackNode, thisArgumentNode, sourceCode) {
 	return function * (fixer) {
-		yield removeThisArgument(callExpression, sourceCode)(fixer);
+		yield removeThisArgument(thisArgumentNode, sourceCode)(fixer);
 
-		const [callback, thisArgument] = callExpression.arguments;
-
-		const callbackParentheses = getParentheses(callback, sourceCode);
+		const callbackParentheses = getParentheses(callbackNode, sourceCode);
 		const isParenthesized = callbackParentheses.length > 0;
 		const callbackLastToken = isParenthesized
 			? callbackParentheses.at(-1)
-			: callback;
+			: callbackNode;
 		if (
 			!isParenthesized
-			&& shouldAddParenthesesToMemberExpressionObject(callback, sourceCode)
+			&& shouldAddParenthesesToMemberExpressionObject(callbackNode, sourceCode)
 		) {
 			yield fixer.insertTextBefore(callbackLastToken, '(');
 			yield fixer.insertTextAfter(callbackLastToken, ')');
 		}
 
-		const thisArgumentText = getParenthesizedText(thisArgument, sourceCode);
+		const thisArgumentText = getParenthesizedText(thisArgumentNode, sourceCode);
 		// `thisArgument` was a argument, no need add extra parentheses
 		yield fixer.insertTextAfter(callbackLastToken, `.bind(${thisArgumentText})`);
 	};
+}
+
+function getProblem({
+	sourceCode,
+	callExpression,
+	callbackNode,
+	thisArgumentNode,
+	messageId,
+}) {
+	const problem = {
+		node: thisArgumentNode,
+		messageId,
+		data: {
+			method: callExpression.callee.property.name,
+		},
+	};
+
+	const isArrowCallback = callbackNode.type === 'ArrowFunctionExpression';
+	if (isArrowCallback) {
+		const thisArgumentHasSideEffect = hasSideEffect(thisArgumentNode, sourceCode);
+		if (thisArgumentHasSideEffect) {
+			problem.suggest = [
+				{
+					messageId: SUGGESTION_REMOVE,
+					fix: removeThisArgument(thisArgumentNode, sourceCode),
+				},
+			];
+		} else {
+			problem.fix = removeThisArgument(thisArgumentNode, sourceCode);
+		}
+
+		return problem;
+	}
+
+	problem.suggest = [
+		{
+			messageId: SUGGESTION_REMOVE,
+			fix: removeThisArgument(thisArgumentNode, sourceCode),
+		},
+		{
+			messageId: SUGGESTION_BIND,
+			fix: useBoundFunction(callbackNode, thisArgumentNode, sourceCode),
+		},
+	];
+
+	return problem;
 }
 
 /** @param {import('eslint').Rule.RuleContext} context */
 const create = context => {
 	const {sourceCode} = context;
 
-	return {
-		CallExpression(callExpression) {
-			if (
-				!isMethodCall(callExpression, {
-					methods: [
-						'every',
-						'filter',
-						'find',
-						'findLast',
-						'findIndex',
-						'findLastIndex',
-						'flatMap',
-						'forEach',
-						'map',
-						'some',
-					],
-					argumentsLength: 2,
-					optionalCall: false,
-					optionalMember: false,
-				})
-				|| isNodeMatches(callExpression.callee, ignored)
-				|| isNodeValueNotFunction(callExpression.arguments[0])
-			) {
-				return;
-			}
+	// Prototype methods
+	context.on('CallExpression', callExpression => {
+		if (
+			!isMethodCall(callExpression, {
+				methods: [
+					'every',
+					'filter',
+					'find',
+					'findLast',
+					'findIndex',
+					'findLastIndex',
+					'flatMap',
+					'forEach',
+					'map',
+					'some',
+				],
+				argumentsLength: 2,
+				optionalCall: false,
+				optionalMember: false,
+			})
+			|| isNodeMatches(callExpression.callee, ignored)
+			|| isNodeValueNotFunction(callExpression.arguments[0])
+		) {
+			return;
+		}
 
-			const {callee} = callExpression;
-			const method = callee.property.name;
-			const [callback, thisArgument] = callExpression.arguments;
+		return getProblem({
+			sourceCode,
+			callExpression,
+			callbackNode: callExpression.arguments[0],
+			thisArgumentNode: callExpression.arguments[1],
+			messageId: ERROR_PROTOTYPE_METHOD,
+		});
+	});
 
-			const problem = {
-				node: thisArgument,
-				messageId: ERROR,
-				data: {method},
-			};
+	// `Array.from()`
+	context.on('CallExpression', callExpression => {
+		if (
+			!isMethodCall(callExpression, {
+				object: 'Array',
+				method: 'from',
+				argumentsLength: 3,
+				optionalCall: false,
+				optionalMember: false,
+			})
+			|| isNodeValueNotFunction(callExpression.arguments[1])
+		) {
+			return;
+		}
 
-			const thisArgumentHasSideEffect = hasSideEffect(thisArgument, sourceCode);
-			const isArrowCallback = callback.type === 'ArrowFunctionExpression';
-
-			if (isArrowCallback) {
-				if (thisArgumentHasSideEffect) {
-					problem.suggest = [
-						{
-							messageId: SUGGESTION_REMOVE,
-							fix: removeThisArgument(callExpression, sourceCode),
-						},
-					];
-				} else {
-					problem.fix = removeThisArgument(callExpression, sourceCode);
-				}
-
-				return problem;
-			}
-
-			problem.suggest = [
-				{
-					messageId: SUGGESTION_REMOVE,
-					fix: removeThisArgument(callExpression, sourceCode),
-				},
-				{
-					messageId: SUGGESTION_BIND,
-					fix: useBoundFunction(callExpression, sourceCode),
-				},
-			];
-
-			return problem;
-		},
-	};
+		return getProblem({
+			sourceCode,
+			callExpression,
+			callbackNode: callExpression.arguments[1],
+			thisArgumentNode: callExpression.arguments[2],
+			messageId: ERROR_STATIC_METHOD,
+		});
+	});
 };
 
 /** @type {import('eslint').Rule.RuleModule} */
