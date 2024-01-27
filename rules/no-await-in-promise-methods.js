d7d4dccd4cf1bea0f229bd6a59c9e7fb1defa0c2
fisker <lionkay@gmail.com>
2024-01-28 05:07:53 +0800

    Report problems ONE by ONE

--- rules/no-await-in-promise-methods.js
+++ rules/no-await-in-promise-methods.js
@@ -9,45 +9,37 @@ const messages = {
 };
 const METHODS = ['all', 'allSettled', 'any', 'race'];
 
-const getArrayElements = node => node.arguments[0].elements;
-
-const getMethodName = node => node.callee.property.name;
-
-const getFixer = ({sourceCode}, awaitedElements) => function * (fixer) {
-	for (const awaitedElement of awaitedElements) {
-		const firstToken = sourceCode.getFirstToken(awaitedElement);
-		const secondToken = sourceCode.getTokenAfter(firstToken);
-
-		yield fixer.removeRange([firstToken.range[0], secondToken.range[0]]);
-	}
-};
-
 /** @param {import('eslint').Rule.RuleContext} context */
 const create = context => ({
-	CallExpression(node) {
+	* CallExpression(node) {
 		if (!isPromiseMethodWithArray(node, METHODS)) {
 			return;
 		}
 
-		const awaitedElements = getArrayElements(node).filter(element => element?.type === 'AwaitExpression');
+		for (const element of node.arguments[0].elements) {
+			if (element?.type !== 'AwaitExpression') {
+				continue;
+			}
 
-		if (awaitedElements.length === 0) {
-			return;
-		}
-
-		context.report({
-			node,
-			messageId: MESSAGE_ID_ERROR,
-			data: {
-				method: getMethodName(node),
-			},
-			suggest: [
-				{
-					messageId: MESSAGE_ID_SUGGESTION,
-					fix: getFixer(context, awaitedElements),
+			yield {
+				node: element,
+				messageId: MESSAGE_ID_ERROR,
+				data: {
+					method: node.callee.property.name,
 				},
-			],
-		});
+				suggest: [
+					{
+						messageId: MESSAGE_ID_SUGGESTION,
+						* fix(fixer) {
+							const firstToken = context.sourceCode.getFirstToken(element);
+							const secondToken = context.sourceCode.getTokenAfter(firstToken);
+
+							yield fixer.removeRange([firstToken.range[0], secondToken.range[0]]);
+						},
+					},
+				],
+			};
+		}
 	},
 });
 
