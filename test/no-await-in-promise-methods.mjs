d91eb235e02b65b1862c9aa3d3e6a3f7b7216f28
fisker <lionkay@gmail.com>
2024-01-28 05:02:06 +0800

    Better names

--- test/no-await-in-promise-methods.mjs
+++ test/no-await-in-promise-methods.mjs
@@ -4,20 +4,20 @@ const {test} = getTester(import.meta);
 
 test.snapshot({
 	valid: [
-		'Promise.all([promise, promise, promise, promise])',
-		'Promise.allSettled([promise, promise, promise, promise])',
-		'Promise.any([promise, promise, promise, promise])',
-		'Promise.race([promise, promise, promise, promise])',
+		'Promise.all([promise1, promise2, promise3, promise4])',
+		'Promise.allSettled([promise1, promise2, promise3, promise4])',
+		'Promise.any([promise1, promise2, promise3, promise4])',
+		'Promise.race([promise1, promise2, promise3, promise4])',
 		'Promise.resolve([await promise])',
 		'Promise[all]([await promise])',
 		'Promise.all([,])',
 	],
 
 	invalid: [
-		'Promise.all([promise, await promise, await promise, promise])',
+		'Promise.all([promise1, await promise2, await promise3, promise4])',
 		'Promise.all([, await promise])',
-		'Promise.allSettled([promise, await promise, await promise, promise])',
-		'Promise.any([promise, await promise, await promise, promise])',
-		'Promise.race([promise, await promise, await promise, promise])',
+		'Promise.allSettled([promise1, await promise2, await promise3, promise4])',
+		'Promise.any([promise1, await promise2, await promise3, promise4])',
+		'Promise.race([promise1, await promise2, await promise4, promise4])',
 	],
 });
