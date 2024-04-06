012cd16389fb849dac159c7628e420826d78c1ae
fisker <lionkay@gmail.com>
2024-04-06 15:47:13 +0800

    Linting

--- test/utils/snapshot-rule-tester.mjs
+++ test/utils/snapshot-rule-tester.mjs
@@ -1,4 +1,4 @@
-import * as path from 'node:path';
+import path from 'node:path';
 import {Linter} from 'eslint';
 import {codeFrameColumns} from '@babel/code-frame';
 import outdent from 'outdent';
