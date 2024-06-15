a55f3ee62cb6af93b61fb4c7c504cd57cb2f0dc0
Stephen Zhou <hi@hyoban.cc>
2024-06-15 19:42:46 +0800

    lint

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
-import type { ESLint, Linter } from "eslint";
+import type {ESLint, Linter} from 'eslint';
 
 declare const eslintPluginUnicorn: ESLint.Plugin & {
 	configs: {
 		recommended: Linter.Config;
 		all: Linter.Config;
-		"flat/all": Linter.FlatConfig;
-		"flat/recommended": Linter.FlatConfig;
+		'flat/all': Linter.FlatConfig;
+		'flat/recommended': Linter.FlatConfig;
 	};
 };
 
