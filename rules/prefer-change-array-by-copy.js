2d8dc3ff3850115c9c2918a8a4f4b4106b383508
Axetroy <axetroy.dev@gmail.com>
2024-08-20 14:31:41 +0800

    chore: Refactor prefer-change-array-by-copy rule for improved readability and maintainability

--- rules/prefer-change-array-by-copy.js
+++ rules/prefer-change-array-by-copy.js
@@ -7,34 +7,36 @@ const messages = {
 	[MESSAGE_ID_SUGGESTION]: 'Replace `{{value}}` with `{{replacement}}`.',
 };
 
+const mutableMethodsMap = {
+	reverse: 'toReversed',
+	sort: 'toSorted',
+	splice: 'toSpliced',
+};
+
+const arrayTypes = new Set([
+	'Array',
+	'Int8Array',
+	'Uint8Array',
+	'Uint8ClampedArray',
+	'Int16Array',
+	'Uint16Array',
+	'Int32Array',
+	'Uint32Array',
+	'Float32Array',
+	'Float64Array',
+	'BigInt64Array',
+	'BigUint64Array',
+]);
+
 /**
-Check if the given node is a new array expression.
+Check if the node is an array constructor.
 
 @param {import('estree').Node} node
-@returns {node is import('estree').ArrayExpression}
+@returns {node is import('estree').NewExpression}
 */
-function isNewArrayExpression(node) {
-	return (
-		node
-		&& node.type === 'NewExpression'
-		&& node.callee.type === 'Identifier'
-		&& [
-			'Array',
-			// https://github.com/tc39/proposal-change-array-by-copy#change-array-by-copy
-			'Int8Array',
-			'Uint8Array',
-			'Uint8ClampedArray',
-			'Int16Array',
-			'Uint16Array',
-			'Int32Array',
-			'Uint32Array',
-			'Float32Array',
-			'Float64Array',
-			'BigInt64Array',
-			'BigUint64Array',
-		].includes(node.callee.name)
-	);
-}
+const isArrayConstructor = node =>
+	node?.type === 'NewExpression'
+	&& arrayTypes.has(node.callee.name);
 
 /**
 Check if the given variable is an array-like variable.
@@ -44,7 +46,6 @@ Check if the given variable is an array-like variable.
 @returns {boolean}
 */
 function isArrayLikeVariable(scope, variable) {
-	// Check if the variable is an array-like variable
 	const variableScope = scope.variables.find(({name}) => name === variable);
 
 	if (!variableScope) {
@@ -56,152 +57,105 @@ function isArrayLikeVariable(scope, variable) {
 			continue;
 		}
 
-		if (definition.node.init?.type === 'ArrayExpression' || isNewArrayExpression(definition.node.init)) {
+		if (definition.node.init?.type === 'ArrayExpression' || isArrayConstructor(definition.node.init)) {
 			return true;
 		}
 	}
 }
 
 /**
-Check if the given node is a mutable array method call expression.
+Check if the node represents a mutable array method call.
 
 @param {import('estree').CallExpression} node
-@returns {true}
+@returns {boolean}
 */
-function isMutableArrayMethodCallExpression(node) {
+const isMutableArrayMethodCall = node => {
 	if (node.callee.type !== 'MemberExpression') {
 		return false;
 	}
 
 	return (
 		(node.callee.object.type === 'ArrayExpression' // [].sort()
-			|| isNewArrayExpression(node.callee.object) // New Array().sort()
+			|| isArrayConstructor(node.callee.object) // New Array().sort()
 			|| node.callee.object.type === 'Identifier') // Array.sort()
 		&& node.callee.property.type === 'Identifier'
-		&& ['reverse', 'sort', 'splice'].includes(node.callee.property.name)
+		&& Object.hasOwn(mutableMethodsMap, node.callee.property.name)
 	);
-}
+};
+
+/**
+Generate the replacement code for the given method.
+
+@param {import('eslint').SourceCode} sourceCode
+@param {import('estree').Node} node
+@param {string} replacementMethod
+@returns {string}
+*/
+const getReplacementCode = (sourceCode, node, replacementMethod) => {
+	const arguments_ = node.arguments.map(argument => sourceCode.getText(argument)).join(', ');
+	return `${sourceCode.getText(node.callee.object)}.${replacementMethod}(${arguments_})`;
+};
 
 /** @param {import('eslint').Rule.RuleContext} context */
 const create = context => ({
 	/** @param {import('estree').CallExpression} node */
 	CallExpression(node) {
-		const {sourceCode} = context;
-
-		if (node.callee.type !== 'MemberExpression') {
-			return;
-		}
-
-		if (!isMutableArrayMethodCallExpression(node)) {
+		if (!isMutableArrayMethodCall(node)) {
 			return;
 		}
 
 		const methodName = node.callee.property.name;
+		const replacementMethod = mutableMethodsMap[methodName];
 
-		// Determine the immutable method replacement and check conditions
-		let replacementMethod;
-
-		switch (methodName) {
-			case 'reverse': {
-				if (node.arguments.length === 0) {
-					replacementMethod = 'toReversed';
-				}
-
-				break;
-			}
-
-			case 'sort': {
-				if (node.arguments.length <= 1) {
-					replacementMethod = 'toSorted';
-				}
-
-				break;
-			}
-
-			case 'splice': {
-				if (node.arguments.length <= 2) {
-					replacementMethod = 'toSpliced';
-				}
-
-				break;
-			}
-
-			default: {
-				return;
-			}
+		if (methodName === 'reverse' && node.arguments.length > 0) {
+			return;
 		}
 
-		if (replacementMethod) {
-			context.report({
-				node,
-				messageId: MESSAGE_ID_ERROR,
-				data: {
-					value: `Array#${methodName}()`,
-					replacement: `Array#${replacementMethod}()`,
-				},
-				/** @param {import('eslint').Rule.RuleFixer} fixer */
-				suggest: [
-					{
-						messageId: MESSAGE_ID_SUGGESTION,
-						data: {
-							value: `Array#${methodName}()`,
-							replacement: `Array#${replacementMethod}()`,
-						},
-						/** @param {import('eslint').Rule.RuleFixer} fixer */
-						fix(fixer) {
-							const argumentCode = node.arguments
-								.map(argument => sourceCode.getText(argument))
-								.join(', ');
-
-							const replacementCode = `${sourceCode.getText(node.callee.object)}.${replacementMethod}(${argumentCode})`;
-
-							return fixer.replaceText(node, replacementCode);
-						},
-					},
-				],
-			});
+		if (methodName === 'sort' && node.arguments.length > 1) {
+			return;
 		}
+
+		if (methodName === 'splice' && node.arguments.length > 2) {
+			return;
+		}
+
+		context.report({
+			node,
+			messageId: MESSAGE_ID_ERROR,
+			data: {value: `Array#${methodName}()`, replacement: `Array#${replacementMethod}()`},
+			suggest: [{
+				messageId: MESSAGE_ID_SUGGESTION,
+				data: {value: `Array#${methodName}()`, replacement: `Array#${replacementMethod}()`},
+				fix: fixer => fixer.replaceText(node, getReplacementCode(context.sourceCode, node, replacementMethod)),
+			}],
+		});
 	},
+
 	/** @param {import('estree').AssignmentExpression} node */
 	AssignmentExpression(node) {
-		// Check if the assignment is a subtraction operation on an array element
 		if (
 			node.operator === '='
 			&& node.left.type === 'MemberExpression'
+			&& node.left.computed
 			&& isArrayLikeVariable(
 				context.sourceCode.getScope(node.left.object),
 				node.left.object.name,
 			)
-			&& node.left.computed
 		) {
-			// Array[1] = 'changed';
+			const {sourceCode} = context;
+			const objectText = sourceCode.getText(node.left.object);
+			const propertyText = sourceCode.getText(node.left.property);
+			const rightText = sourceCode.getText(node.right);
 
 			context.report({
 				node,
 				messageId: MESSAGE_ID_ERROR,
-				data: {
-					value: 'Array#[index]',
-					replacement: 'Array#with()',
-				},
-				/** @param {import('eslint').Rule.RuleFixer} fixer */
-				suggest: [
-					{
-						messageId: MESSAGE_ID_SUGGESTION,
-						data: {
-							value: 'Array#[index]',
-							replacement: 'Array#with()',
-						},
-						/** @param {import('eslint').Rule.RuleFixer} fixer */
-						fix(fixer) {
-							const {sourceCode} = context;
-							const objectIdentifier = sourceCode.getText(node.left.object);
-							return fixer.replaceText(
-								node,
-								`${objectIdentifier}.with(${sourceCode.getText(node.left.property)}, ${sourceCode.getText(node.right)})`,
-							);
-						},
-					},
-				],
+				data: {value: 'Array#[index]', replacement: 'Array#with()'},
+				suggest: [{
+					messageId: MESSAGE_ID_SUGGESTION,
+					data: {value: 'Array#[index]', replacement: 'Array#with()'},
+					fix: fixer => fixer.replaceText(node, `${objectText}.with(${propertyText}, ${rightText})`),
+				}],
 			});
 		}
 	},
