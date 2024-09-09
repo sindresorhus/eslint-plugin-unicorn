d92b7b40f9bdc195b8e0d3d8246f19c4ac7ab456
Axetroy <axetroy.dev@gmail.com>
2024-09-09 10:28:26 +0800

    fix

--- rules/utils/is-boolean.js
+++ rules/utils/is-boolean.js
@@ -5,16 +5,16 @@ Determines whether a node is a Boolean Expression.
 
 @param {import('eslint').Rule.RuleContext} context
 @param {import('estree').Expression} node
-@param {number} [deep] - The current recursion depth. Users do not need to pass this parameter.
+@param {number} [depth] - The current recursion depth. Users do not need to pass this parameter.
 */
-function isBooleanExpression(context, node, deep = 0) {
+function isBooleanExpression(context, node, depth = 0) {
 	if (!node) {
 		return false;
 	}
 
 	// Prevent infinite recursion
 	// This value should not be too large, otherwise there may be performance issues
-	if (deep > 3) {
+	if (depth > 3) {
 		return false;
 	}
 
@@ -38,7 +38,7 @@ function isBooleanExpression(context, node, deep = 0) {
 				switch (definition.type) {
 					case 'Variable': {
 						if (
-							isBooleanExpression(context, definition.node.init, deep + 1)
+							isBooleanExpression(context, definition.node.init, depth + 1)
 							|| isBooleanTypeAnnotation(definition.node.id.typeAnnotation)
 						) {
 							return true;
@@ -81,19 +81,19 @@ function isBooleanExpression(context, node, deep = 0) {
 		// Const isAdult = age >= 18 ? true : false;
 		case 'ConditionalExpression': {
 			return (
-				isBooleanExpression(context, node.consequent, deep + 1)
-				&& isBooleanExpression(context, node.alternate, deep + 1)
+				isBooleanExpression(context, node.consequent, depth + 1)
+				&& isBooleanExpression(context, node.alternate, depth + 1)
 			);
 		}
 
 		// Await true
 		case 'AwaitExpression': {
-			return isBooleanExpression(context, node.argument, deep + 1);
+			return isBooleanExpression(context, node.argument, depth + 1);
 		}
 
 		// Yield true
 		case 'YieldExpression': {
-			return isBooleanExpression(context, node.argument, deep + 1);
+			return isBooleanExpression(context, node.argument, depth + 1);
 		}
 
 		// New Boolean(true)
@@ -150,12 +150,12 @@ function isBooleanExpression(context, node, deep = 0) {
 
 		// (0, true)
 		case 'SequenceExpression': {
-			return isBooleanExpression(context, node.expressions.at(-1), deep + 1);
+			return isBooleanExpression(context, node.expressions.at(-1), depth + 1);
 		}
 
 		// (foo = true)
 		case 'AssignmentExpression': {
-			return isBooleanExpression(context, node.right, deep + 1);
+			return isBooleanExpression(context, node.right, depth + 1);
 		}
 
 		// @typescript-eslint/parser
