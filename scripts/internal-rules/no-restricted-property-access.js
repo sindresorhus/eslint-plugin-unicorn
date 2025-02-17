a577d218c4177490381c4baf21527761d2f279b2
fisker <lionkay@gmail.com>
2025-02-17 21:50:45 +0800

    Add rule

---
+++ scripts/internal-rules/no-restricted-property-access.js
@@ -0,0 +1,79 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {isMemberExpression} from '../../rules/ast/index.js';
+import {removeMemberExpressionProperty} from '../../rules/fix/index.js';
+
+const messageId = path.basename(fileURLToPath(import.meta.url), '.js');
+
+const properties = new Map([
+	// Todo: forbid `.range`
+	['loc', 'sourceCode.getLoc'],
+]);
+
+const propertyNames = [...properties.keys()];
+
+const config = {
+	create(context) {
+		return {
+			MemberExpression(memberExpression) {
+				if (!isMemberExpression(memberExpression, {
+					properties: propertyNames,
+					optional: false,
+					computed: false,
+				})) {
+					return;
+				}
+
+				const name = memberExpression.property.name;
+				const replacementFunction = properties.get(name);
+
+				context.report({
+					node: memberExpression.property,
+					messageId,
+					data: {
+						name,
+						replacementFunction,
+					},
+					* fix(fixer) {
+						const {sourceCode} = context;
+						yield removeMemberExpressionProperty(fixer, memberExpression, sourceCode);
+						yield fixer.insertTextBefore(`${replacementFunction}(`, memberExpression);
+						yield fixer.insertTextAfter(`)`, memberExpression);
+					}
+				});
+			},
+			ObjectPattern(objectPattern) {
+				for (const property of objectPattern.properties) {
+					if (!(
+						property.type === 'Property'
+						&& !property.computed
+						&& property.key.type === 'Identifier'
+						&& propertyNames.includes(property.key.name)
+					)) {
+						continue;
+					}
+
+					const name = property.key.name;
+					const replacementFunction = properties.get(name);
+
+					context.report({
+						node: property.key,
+						messageId,
+						data: {
+							name,
+							replacementFunction,
+						},
+					});
+				}
+			},
+		};
+	},
+	meta: {
+		fixable: 'code',
+		messages: {
+			[messageId]: 'Use `{{replacementFunction}}(node)` instead of accessing `node.${name}`.',
+		},
+	},
+};
+
+export default config;
