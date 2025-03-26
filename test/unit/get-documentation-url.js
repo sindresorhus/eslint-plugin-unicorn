7107b20f9b48c7e15ef6d5748523b93551b3aab6
yosuke ota <otameshiyo23@gmail.com>
2025-03-26 19:41:19 +0900

    chore: revert `test/unit/get-documentation-url.js`

--- test/unit/get-documentation-url.js
+++ test/unit/get-documentation-url.js
@@ -1,8 +1,10 @@
+import url from 'node:url';
 import test from 'ava';
 import getDocumentationUrl from '../../rules/utils/get-documentation-url.js';
 import packageJson from '../../package.json' with {type: 'json'};
 
-const filename = import.meta.filename.replace(/\.js$/, '.js');
+// eslint-disable-next-line unicorn/prefer-module -- We still use Node.js v18 for CI.
+const filename = url.fileURLToPath(import.meta.url).replace(/\.js$/, '.js');
 
 test('returns the URL of the a named rule\'s documentation', t => {
 	const url = `https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v${packageJson.version}/docs/rules/foo.md`;
