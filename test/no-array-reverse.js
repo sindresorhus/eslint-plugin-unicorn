b3761e02d5aba599b228dcb6ffdbb9d1d4e54a63
fisker <lionkay@gmail.com>
2025-06-13 00:50:40 +0800

    Add `no-array-reverse`

---
+++ test/no-array-reverse.js
@@ -0,0 +1,38 @@
+import {getTester} from './utils/test.js';
+
+const {test} = getTester(import.meta);
+
+const FORBID_EXPRESSION_OPTIONS = [{allowExpressionStatement: false}];
+
+test.snapshot({
+	valid: [
+		'reversed =[...array].toReversed()',
+		'reversed =array.toReversed()',
+		'reversed =[...array].reverse',
+		'reversed =[...array].reverse?.()',
+		'array.reverse()',
+		'array.reverse?.()',
+		'array?.reverse()',
+		'reversed = array.reverse(extraArgument)',
+	],
+	invalid: [
+		'reversed = [...array].reverse()',
+		'reversed = [...array]?.reverse()',
+		'reversed = array.reverse()',
+		'reversed = array?.reverse()',
+		{
+			code: 'array.reverse()',
+			options: FORBID_EXPRESSION_OPTIONS,
+		},
+		{
+			code: 'array?.reverse()',
+			options: FORBID_EXPRESSION_OPTIONS,
+		},
+		// Don't care about `allowExpression`
+		{
+			code: '[...array].reverse()',
+			options: FORBID_EXPRESSION_OPTIONS,
+		},
+		'reversed = [...(0, array)].reverse()',
+	],
+});
