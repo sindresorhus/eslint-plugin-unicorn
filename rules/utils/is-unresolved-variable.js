e11b084b013d80b58a094880e2ebb641e3ef0ba5
fisker Cheung <lionkay@gmail.com>
2025-06-19 21:24:11 +0800

    Use `context.sourceCode.isGlobalReference()` in more places (#2685)

---
+++ rules/utils/is-unresolved-variable.js
@@ -0,0 +1,13 @@
+/**
+Checks if the given identifier node is shadowed in the given scope.
+
+@param {string} node - The identifier node to check
+@param {import('eslint').Rule.RuleContext} context - The ESLint rule context.
+@returns {boolean} Whether or not the name is unresolved.
+*/
+export default function isUnresolvedVariable(node, context) {
+	const scope = context.sourceCode.getScope(node);
+	const reference = scope.references
+		.find(reference => reference.identifier === node);
+	return !reference.resolved;
+}
