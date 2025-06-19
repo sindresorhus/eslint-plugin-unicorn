36e6bae8602e800576e8380b7ab914d3f869e051
fisker <lionkay@gmail.com>
2025-06-19 11:55:51 +0800

    Update comment

--- rules/utils/is-unresolved-variable.js
+++ rules/utils/is-unresolved-variable.js
@@ -2,8 +2,8 @@
 Checks if the given identifier node is shadowed in the given scope.
 
 @param {string} node The identifier node to check
-@param {Object} scope The current scope.
-@returns {boolean} Whether or not the name is shadowed.
+@param {import('eslint').Rule.RuleContext} context The ESLint rule context.
+@returns {boolean} Whether or not the name is unresolved.
 */
 export default function isUnresolvedVariable(node, context) {
 	const scope = context.sourceCode.getScope(node);
