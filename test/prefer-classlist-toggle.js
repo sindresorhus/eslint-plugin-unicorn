77119b0fa189c9777616c1adc9d670361ef32dd4
fisker <lionkay@gmail.com>
2025-09-03 14:50:12 +0800

    el -> element

--- test/prefer-classlist-toggle.js
+++ test/prefer-classlist-toggle.js
@@ -8,48 +8,48 @@ test.snapshot({
 	valid: [
 		outdent`
 			if (condition) {
-				el.classList.notAdd('className');
+				element.classList.notAdd('className');
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		outdent`
 			if (condition) {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		outdent`
 			if (condition) {
-				el.classList.add('className');
+				element.classList.add('className');
 			} else {
-				el.classList.add('className');
+				element.classList.add('className');
 			}
 		`,
 		outdent`
 			if (condition) {
-				el.classList.add('className1');
+				element.classList.add('className1');
 			} else {
-				el.classList.remove('className2');
+				element.classList.remove('className2');
 			}
 		`,
 		outdent`
-			el.classList.add('className');
-			el.classList.remove('className');
+			element.classList.add('className');
+			element.classList.remove('className');
 		`,
 		outdent`
 			if (condition) {
-				el.classList.add?.('className');
+				element.classList.add?.('className');
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		outdent`
 			if (condition) {
-				el.classList?.add('className');
+				element.classList?.add('className');
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		outdent`
@@ -61,107 +61,107 @@ test.snapshot({
 		`,
 		outdent`
 			if (condition) {
-				el.notClassList.add('className');
+				element.notClassList.add('className');
 			} else {
-				el.notClassList.remove('className');
+				element.notClassList.remove('className');
 			}
 		`,
 		outdent`
 			if (condition) {
-				el.classList.add('className');
+				element.classList.add('className');
 				foo();
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		outdent`
 			if (condition) {
 				{
-					el.classList.add('className');
+					element.classList.add('className');
 				}
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		outdent`
 			if (condition) {
-				el1.classList.add('className');
+				element1.classList.add('className');
 			} else {
-				el2.classList.remove('className');
+				element2.classList.remove('className');
 			}
 		`,
 	],
 	invalid: [
 		outdent`
 			if (condition) {
-				el.classList.add('className');
+				element.classList.add('className');
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		// Not `BlockStatement`
 		outdent`
 			if (condition)
-				el.classList.add('className');
+				element.classList.add('className');
 			else
-				el.classList.remove('className');
+				element.classList.remove('className');
 		`,
 		outdent`
 			if (condition)
-				el.classList.add('className');
+				element.classList.add('className');
 			else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		// Optional element
 		outdent`
 			if (condition) {
-				el?.classList.add('className');
+				element?.classList.add('className');
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		outdent`
 			if (condition)
-				el.classList.add('className');
+				element.classList.add('className');
 			else
-				el?.classList.remove('className');
+				element?.classList.remove('className');
 		`,
 		outdent`
 			if (condition) {
-				el.classList.add('className');
+				element.classList.add('className');
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		// Negative
 		outdent`
 			if (condition) {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			} else {
-				el.classList.add('className');
+				element.classList.add('className');
 			}
 		`,
 		// Parentheses
 		outdent`
 			if (condition) {
-				(( el )).classList.add('className');
+				(( element )).classList.add('className');
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		outdent`
 			if (0, condition) {
-				el.classList.add('className');
+				element.classList.add('className');
 			} else {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			}
 		`,
 		outdent`
 			if (0, condition) {
-				el.classList.remove('className');
+				element.classList.remove('className');
 			} else {
-				el.classList.add('className');
+				element.classList.add('className');
 			}
 		`,
 		// ASI
@@ -169,17 +169,16 @@ test.snapshot({
 			foo
 
 			if (condition) {
-				(( el )).classList.add('className')
+				(( element )).classList.add('className')
 			} else {
-				el.classList.remove('className')
+				element.classList.remove('className')
 			}
 		`,
-		// ASI
 		outdent`
 			if (condition) {
-				(( el )).classList.add('className')
+				(( element )).classList.add('className')
 			} else {
-				el.classList.remove('className')
+				element.classList.remove('className')
 			}
 
 			[].forEach(foo);
