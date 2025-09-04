cb672160a772f415d39de2d4a21f287a9b5583dc
fisker <lionkay@gmail.com>
2025-09-04 14:55:19 +0800

    Linting

--- rules/fix/replace-member-expression-property.js
+++ rules/fix/replace-member-expression-property.js
@@ -6,4 +6,4 @@ export function replaceMemberExpressionProperty(fixer, memberExpression, sourceC
 	return fixer.replaceTextRange([start, end], text);
 }
 
-export const removeMemberExpressionProperty = (fixer, memberExpression, sourceCode) => replaceMemberExpressionProperty(fixer, memberExpression, sourceCode, '')
+export const removeMemberExpressionProperty = (fixer, memberExpression, sourceCode) => replaceMemberExpressionProperty(fixer, memberExpression, sourceCode, '');
