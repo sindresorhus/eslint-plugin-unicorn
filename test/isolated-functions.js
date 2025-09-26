dbf974f16d22b7b68bc632e7d43c952fcff9da46
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-09-26 20:39:19 +0100

    outdent + lint

--- test/isolated-functions.js
+++ test/isolated-functions.js
@@ -1,4 +1,4 @@
-import stripIndent from 'strip-indent';
+import outdent from 'outdent';
 import {getTester} from './utils/test.js';
 
 const {test} = getTester(import.meta);
@@ -11,119 +11,119 @@ test({
 	invalid: [
 		{
 			name: 'out of scope variable under makeSynchronous (arrow function)',
-			code: stripIndent(`
+			code: outdent`
 				const foo = 'hi';
 				makeSynchronous(() => foo.slice());
-			`),
+			`,
 			errors: [fooInMakeSynchronousError],
 		},
 		{
 			name: 'out of scope variable under makeSynchronous (async arrow function)',
-			code: stripIndent(`
+			code: outdent`
 				const foo = 'hi';
 				makeSynchronous(async () => foo.slice());
-			`),
+			`,
 			errors: [fooInMakeSynchronousError],
 		},
 		{
 			name: 'out of scope variable under makeSynchronous (function expression)',
-			code: stripIndent(`
+			code: outdent`
 				const foo = 'hi';
 				makeSynchronous(function () {
 					return foo.slice();
 				});
-			`),
+			`,
 			errors: [fooInMakeSynchronousError],
 		},
 		{
 			name: 'out of scope variable under makeSynchronous (async function expression)',
-			code: stripIndent(`
+			code: outdent`
 				const foo = 'hi';
 				makeSynchronous(async function () {
 					return foo.slice();
 				});
-			`),
+			`,
 			errors: [fooInMakeSynchronousError],
 		},
 		{
 			name: 'out of scope variable under makeSynchronous (named function expression)',
-			code: stripIndent(`
+			code: outdent`
 				const foo = 'hi';
 				makeSynchronous(function abc () {
 					return foo.slice();
 				});
-			`),
+			`,
 			errors: [fooInMakeSynchronousError],
 		},
 		{
 			name: 'out of scope variable under makeSynchronous (named async function expression)',
-			code: stripIndent(`
+			code: outdent`
 				const foo = 'hi';
 				makeSynchronous(async function abc () {
 					return foo.slice();
 				});
-			`),
+			`,
 			errors: [fooInMakeSynchronousError],
 		},
 		{
 			name: '@isolated comment on function declaration',
-			code: stripIndent(`
+			code: outdent`
 				const foo = 'hi';
 				/** @isolated */
 				function abc () {
 					return foo.slice();
 				}
-			`),
+			`,
 			errors: [error({name: 'foo', reason: 'follows comment containing "isolated"'})],
 		},
 		{
 			name: '@isolated comment on arrow function',
-			code: stripIndent(`
+			code: outdent`
 				const foo = 'hi';
 				/** @isolated */
 				const abc = () => foo.slice();
-			`),
+			`,
 			errors: [error({name: 'foo', reason: 'follows comment containing "isolated"'})],
 		},
 		{
 			name: '@isolated inline comment',
-			code: stripIndent(`
+			code: outdent`
 				const foo = 'hi';
 				// @isolated
 				const abc = () => foo.slice();
-			`),
+			`,
 			errors: [error({name: 'foo', reason: 'follows comment containing "isolated"'})],
 		},
 		{
 			name: 'all global variables can be explicitly disallowed',
 			languageOptions: {globals: {foo: true}},
 			options: [{globals: {}}],
-			code: stripIndent(`
+			code: outdent`
 				makeSynchronous(function () {
 					return foo.slice();
 				});
-			`),
+			`,
 			errors: [fooInMakeSynchronousError],
 		},
 		{
 			name: 'individual global variables can be explicitly disallowed',
 			options: [{globals: {URLSearchParams: 'readonly', URL: 'off'}}],
-			code: stripIndent(`
+			code: outdent`
 				makeSynchronous(function () {
 					return new URL('https://example.com?') + new URLSearchParams({a: 'b'}).toString();
 				});
-			`),
+			`,
 			errors: [error({name: 'URL', reason: 'callee of function named "makeSynchronous"'})],
 		},
 		{
 			name: 'check globals writability',
-			code: stripIndent(`
+			code: outdent`
 				makeSynchronous(function () {
 					location = new URL('https://example.com');
 					process = {env: {}};
 					process.env.FOO = 'bar';
 				});
-			`),
+			`,
 			errors: [
 				// Only one error, `location = new URL('https://example.com')` and `process.env.FOO = 'bar'` are fine, the problem is `process = {...}`.
 				error({
@@ -136,7 +136,7 @@ test({
 			name: 'make a function isolated by a selector',
 			// In this case, we're imagining some naming convention for lambda functions that will be created via `fn.toString()`
 			options: [{selectors: ['FunctionDeclaration[id.name=/lambdaHandler.*/]']}],
-			code: stripIndent(`
+			code: outdent`
 				const foo = 'hi';
 
 				function lambdaHandlerFoo() {
@@ -151,7 +151,7 @@ test({
 					name: 'fooLambda',
 					code: lambdaHandlerFoo.toString(),
 				});
-			`),
+			`,
 			errors: [
 				error({name: 'foo', reason: 'matches selector "FunctionDeclaration[id.name=/lambdaHandler.*/]"'}),
 			],
@@ -161,58 +161,58 @@ test({
 	valid: [
 		{
 			name: 'variable defined in scope of isolated function',
-			code: stripIndent(`
+			code: outdent`
 				makeSynchronous(() => {
 					const foo = 'hi';
 					return foo.slice();
 				});
-			`),
+			`,
 		},
 		{
 			name: 'variable defined as parameter of isolated function',
-			code: stripIndent(`
+			code: outdent`
 				makeSynchronous(foo => {
 					return foo.slice();
 				});
-			`),
+			`,
 		},
 		{
 			name: 'inner function can access outer function parameters',
-			code: stripIndent(`
+			code: outdent`
 				/** @isolated */
 				function abc () {
 					const foo = 'hi';
 					const slice = () => foo.slice();
 					return slice();
 				}
-			`),
+			`,
 		},
 		{
 			name: 'variable defined as parameter of isolated function (async)',
-			code: stripIndent(`
+			code: outdent`
 				makeSynchronous(async function (foo) {
 					return foo.slice();
 				});
-			`),
+			`,
 		},
 		{
 			name: 'can implicitly allow global variables from language options',
 			languageOptions: {globals: {foo: true}},
-			code: stripIndent(`
+			code: outdent`
 				makeSynchronous(function () {
 					return foo.slice();
 				});
-			`),
+			`,
 		},
 		{
 			name: 'allow global variables separate from language options',
 			languageOptions: {globals: {abc: true}},
 			options: [{globals: {foo: true}}],
-			code: stripIndent(`
+			code: outdent`
 				makeSynchronous(function () {
 					return foo.slice();
 				});
-			`),
+			`,
 		},
 	],
 });
