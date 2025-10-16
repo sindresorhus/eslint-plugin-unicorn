dc24839c7496e2b0144c9e3205f46f964ab46998
fisker <lionkay@gmail.com>
2025-10-16 20:09:37 +0800

    Merge tests

--- test/text-encoding-identifier-case.js
+++ test/text-encoding-identifier-case.js
@@ -3,6 +3,15 @@ import {getTester} from './utils/test.js';
 const {test} = getTester(import.meta);
 
 test.snapshot({
+	testerOptions: {
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+		},
+	},
 	valid: [
 		'`UTF-8`',
 		'"utf8"',
@@ -12,6 +21,8 @@ test.snapshot({
 		String.raw`"\u0055tf8"`,
 		'const ASCII = 1',
 		'const UTF8 = 1',
+		'<meta charset="utf-8" />',
+		'<META CHARSET="utf-8" />',
 	],
 	invalid: [
 		'"UTF-8"',
@@ -33,10 +44,14 @@ test.snapshot({
 		'await fs.readFile(file, "UTF-8",)',
 		'fs.promises.readFile(file, "UTF-8",)',
 		'whatever.readFile(file, "UTF-8",)',
+		'<not-meta charset="utf-8" />',
+		'<meta not-charset="utf-8" />',
+		'<meta charset="ASCII" />',
+		'<META CHARSET="ASCII" />',
 	],
 });
 
-// JSX
+// `withDash` option
 test.snapshot({
 	testerOptions: {
 		languageOptions: {
@@ -48,25 +63,13 @@ test.snapshot({
 		},
 	},
 	valid: [
-		'<meta charset="utf-8" />',
-		'<META CHARSET="utf-8" />',
-	],
-	invalid: [
-		'<not-meta charset="utf-8" />',
-		'<meta not-charset="utf-8" />',
-		'<meta charset="ASCII" />',
-		'<META CHARSET="ASCII" />',
-	],
-});
-
-// 'withDash'
-test.snapshot({
-	valid: [
 		'`Utf-8`;',
 		'"utf-8";',
 		'"   Utf8   ";',
 		'\'utf-8\';',
 		'const utf8 = 2;',
+		'<meta charset="utf-8" id="with-dash"/>',
+		'<META CHARSET="utf-8" id="with-dash"/>',
 	].map(code => ({code, options: [{withDash: true}]})),
 	invalid: [
 		'"UTF-8";',
@@ -75,27 +78,8 @@ test.snapshot({
 		'\'utf8\';',
 		'"Utf8";',
 		'"ASCII";',
-
-		'whatever.readFile(file, "utf8",);',
-	].map(code => ({code, options: [{withDash: true}]})),
-});
-
-// 'withDash' JSX
-test.snapshot({
-	testerOptions: {
-		languageOptions: {
-			parserOptions: {
-				ecmaFeatures: {
-					jsx: true,
-				},
-			},
-		},
-	},
-	valid: [
-		'<meta charset="utf-8" id="with-dash"/>',
-		'<META CHARSET="utf-8" id="with-dash"/>',
-	].map(code => ({code, options: [{withDash: true}]})),
-	invalid: [
+		'fs.readFile(file, "utf8",);',
+		'whatever.readFile(file, "UTF8",)',
 		'<meta charset="utf8" id="with-dash"/>',
 		'<META CHARSET="utf8" id="with-dash"/>',
 		'<not-meta charset="utf8" id="with-dash"/>',
