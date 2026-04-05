7db5446698445866b1d0671cef6795d1ea4df9d7
tudragon154203 <76395825+tudragon154203@users.noreply.github.com>
2026-04-06 00:58:10 +0700

    fix: resolve #2587 — Rule proposal: Provide autofix for core rule `no-useless-concat`
    Fixes #2587
    
    Signed-off-by: tudragon154203 <76395825+tudragon154203@users.noreply.github.com>

--- rules/index.js
+++ rules/index.js
@@ -65,6 +65,7 @@ export {default as 'no-unreadable-array-destructuring'} from './no-unreadable-ar
 export {default as 'no-unreadable-iife'} from './no-unreadable-iife.js';
 export {default as 'no-unused-properties'} from './no-unused-properties.js';
 export {default as 'no-useless-collection-argument'} from './no-useless-collection-argument.js';
+export {default as 'no-useless-concat'} from './no-useless-concat.js';
 export {default as 'no-useless-error-capture-stack-trace'} from './no-useless-error-capture-stack-trace.js';
 export {default as 'no-useless-fallback-in-spread'} from './no-useless-fallback-in-spread.js';
 export {default as 'no-useless-iterator-to-array'} from './no-useless-iterator-to-array.js';
