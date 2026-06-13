3afe927ae046372b2126ebd0f3aac85bdc51aade
Sindre Sorhus <sindresorhus@gmail.com>
2026-06-13 02:25:10 +0200

    Add `consistent-export-decorator-position` rule
    Fixes #2038

---
+++ test/consistent-export-decorator-position.js
@@ -0,0 +1,172 @@
+import outdent from 'outdent';
+import {getTester, parsers} from './utils/test.js';
+
+const {test} = getTester(import.meta);
+
+const typescriptParser = {
+	languageOptions: {
+		parser: parsers.typescript,
+	},
+};
+
+const withTypescriptParser = testCase => {
+	if (typeof testCase === 'string') {
+		return {
+			code: testCase,
+			...typescriptParser,
+		};
+	}
+
+	return {
+		...testCase,
+		...typescriptParser,
+	};
+};
+
+test.snapshot({
+	valid: [
+		'@decorator\nexport default class Foo {}',
+		'@decorator\nexport default class {}',
+		'@foo\n@bar(options)\nexport default class Foo {}',
+		'@decorator\nexport class Foo {}',
+		'@foo\n@bar(options)\nexport class Foo {}',
+		outdent`
+			@decorator(
+				foo
+			)
+			export class Foo {}
+		`,
+		{
+			code: '@decorator export default class Foo {}',
+			options: ['before'],
+		},
+		{
+			code: '@foo @bar(options) export default class Foo {}',
+			options: ['before'],
+		},
+		{
+			code: '@decorator export class Foo {}',
+			options: ['before'],
+		},
+		{
+			code: outdent`
+				@decorator(
+					foo
+				) export class Foo {}
+			`,
+			options: ['before'],
+		},
+		{
+			code: 'export default @decorator class Foo {}',
+			options: ['after'],
+		},
+		{
+			code: 'export default @foo @bar(options) class Foo {}',
+			options: ['after'],
+		},
+		{
+			code: 'export @decorator class Foo {}',
+			options: ['after'],
+		},
+		'class Foo {}',
+		'@decorator\nclass Foo {}',
+		'export default class Foo {}',
+		'export class Foo {}',
+		'export default (@decorator class Foo {})',
+		'export default class Foo { @decorator method() {} }',
+		'export {Foo}',
+	].map(testCase => withTypescriptParser(testCase)),
+	invalid: [
+		'export default @decorator class Foo {}',
+		'@decorator export default class Foo {}',
+		'export @decorator class Foo {}',
+		'@decorator export class Foo {}',
+		'export default\n@decorator\nclass Foo {}',
+		'export\n@decorator\nclass Foo {}',
+		'export default @foo @bar(options) class Foo {}',
+		'@foo @bar(options) export default class Foo {}',
+		'@foo\n@bar export class Foo {}',
+		'@foo export @bar class Foo {}',
+		'@foo export default @bar class Foo {}',
+		outdent`
+			@decorator(
+				foo
+			) export class Foo {}
+		`,
+		outdent`
+			namespace N {
+				@decorator export class Foo {}
+			}
+		`,
+		{
+			code: '@decorator\nexport default class Foo {}',
+			options: ['before'],
+		},
+		{
+			code: '@foo\n@bar export class Foo {}',
+			options: ['before'],
+		},
+		{
+			code: '@foo export @bar class Foo {}',
+			options: ['before'],
+		},
+		{
+			code: '@foo export default @bar class Foo {}',
+			options: ['before'],
+		},
+		{
+			code: 'export default @decorator class Foo {}',
+			options: ['before'],
+		},
+		{
+			code: '@decorator\nexport class Foo {}',
+			options: ['before'],
+		},
+		{
+			code: 'export @decorator class Foo {}',
+			options: ['before'],
+		},
+		{
+			code: '@decorator\nexport default class Foo {}',
+			options: ['after'],
+		},
+		{
+			code: '@decorator export default class Foo {}',
+			options: ['after'],
+		},
+		{
+			code: '@decorator\nexport class Foo {}',
+			options: ['after'],
+		},
+		{
+			code: '@decorator export class Foo {}',
+			options: ['after'],
+		},
+		{
+			code: '@foo export @bar class Foo {}',
+			options: ['after'],
+		},
+		{
+			code: '@foo export default @bar class Foo {}',
+			options: ['after'],
+		},
+		{
+			code: 'export default @decorator(/* comment */ value) class Foo {}',
+		},
+		{
+			code: 'export default /* comment */ @decorator class Foo {}',
+		},
+		{
+			code: '@decorator /* comment */ export default class Foo {}',
+		},
+		{
+			code: '@decorator\nexport default abstract class Foo {}',
+			options: ['after'],
+		},
+		outdent`
+			export default @decorator class Foo {
+				method() {}
+			}
+		`,
+	].map(testCase => withTypescriptParser(testCase)),
+});
