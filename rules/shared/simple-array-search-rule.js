d25f4e0482937dd7235a5e9d244439e98c14483e
Sindre Sorhus <sindresorhus@gmail.com>
2026-06-14 14:27:39 +0200

    Format `simple-array-search-rule` imports across multiple lines

--- rules/shared/simple-array-search-rule.js
+++ rules/shared/simple-array-search-rule.js
@@ -1,6 +1,11 @@
 import {hasSideEffect, findVariable} from '@eslint-community/eslint-utils';
 import {isMethodCall} from '../ast/index.js';
-import {isSameIdentifier, isFunctionSelfUsedInside, isParenthesized, shouldSkipKnownNonArrayReceiver} from '../utils/index.js';
+import {
+	isSameIdentifier,
+	isFunctionSelfUsedInside,
+	isParenthesized,
+	shouldSkipKnownNonArrayReceiver,
+} from '../utils/index.js';
 
 const booleanLiteralTypeNames = new Set(['false', 'true']);
 
