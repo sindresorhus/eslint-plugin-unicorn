cd01d0bcaac29a095ba2b6aca6f64f3155c8bf28
Sindre Sorhus <sindresorhus@gmail.com>
2026-06-29 20:13:51 +0200

    `no-collection-bracket-access`: Handle conditional member keys (#3462)

--- test/no-collection-bracket-access.js
+++ test/no-collection-bracket-access.js
@@ -39,7 +39,10 @@ test.snapshot({
 		'const map = new Map(); map["size"];',
 		'const map = new Map(); map["get"]("foo");',
 		'const map = new Map(); map["set"]("foo", "bar");',
+		'const map = new Map(); map[condition ? "get" : "has"]("foo");',
 		'const set = new Set(); set["has"]("foo");',
+		'const set = new Set(); set[set.has("foo") ? "delete" : "add"]("foo");',
+		'const set = new Set(); set[condition ? "delete" : otherCondition ? "add" : "has"]("foo");',
 		'const set = new Set(); set["union"](other);',
 		'const set = new Set(); set["size"];',
 		'const weakMap = new WeakMap(); weakMap["delete"](key);',
@@ -48,6 +51,7 @@ test.snapshot({
 		// Well-known `Symbol` keys
 		'const map = new Map(); map[Symbol.iterator];',
 		'const map = new Map(); map[Symbol.toStringTag];',
+		'const map = new Map(); map[condition ? Symbol.iterator : Symbol.toStringTag];',
 
 		// Inherited `Object.prototype` members
 		'const map = new Map(); map["toString"];',
@@ -89,6 +93,8 @@ test.snapshot({
 		'const map = new Map(); map[Symbol.for("foo")] = value;',
 		// Key resolves to a non-member string through a variable
 		'const map = new Map(); const key = "foo"; map[key];',
+		// One branch is a real member, but the other is a collection entry key
+		'const set = new Set(); set[condition ? "add" : "foo"]("foo");',
 		// Dynamic template key
 		'const map = new Map(); map[`${prefix}foo`];', // eslint-disable-line no-template-curly-in-string
 
