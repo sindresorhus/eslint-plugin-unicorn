c47a64a4c02e56f94c9a344d5a41908aaf5cba62
Sindre Sorhus <sindresorhus@gmail.com>
2026-09-15 00:56:46 +0200

    `no-immediate-mutation`: Test static `Object.assign` keys
    Fixes #3682

--- test/no-immediate-mutation.js
+++ test/no-immediate-mutation.js
@@ -73,6 +73,7 @@ test.snapshot({
 		'const object = {}; if (enabled) { object["__proto__"] = prototype; }',
 		'const object = {}; if (enabled) { Object.assign(object, {foo: 1,}); }',
 		'const object = {}; if (enabled) { Object.assign(object, {["__proto__"]: prototype}); }',
+		'const key = "__proto__", object = {}; if (enabled) { Object.assign(object, {[key]: prototype}); }',
 		'const object = {}; enabled && Object.assign(object, getSource());',
 		'const object = {}; enabled && Object.assign(object, (first, second));',
 		'const set = new Set([initial]); enabled ? set.add(first) : set.add(second);',
