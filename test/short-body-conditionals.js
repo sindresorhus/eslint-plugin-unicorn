0f4a4ddf0e90bcd3c1db95649e1683645ae5e9f5
Sindre Sorhus <sindresorhus@gmail.com>
2026-09-16 11:30:49 +0700

    Fix short-body test lint
    Fixes #3691

--- test/short-body-conditionals.js
+++ test/short-body-conditionals.js
@@ -47,7 +47,8 @@ for (const [rule, exit, opening] of [
 	test(`${rule}: preserves local CRLF line endings`, t => {
 		const code = `const prefix = true;\n${opening}\r\n\tif (!condition) {\r\n\t\t${exit}\r\n\t}\r\n\twork();\r\n}`;
 		const expected = `const prefix = true;\n${opening}\r\n\tif (condition) {\r\n\t\twork();\r\n\t}\r\n}`;
-		const result = new Linter().verifyAndFix(code, config);
+		const linter = new Linter();
+		const result = linter.verifyAndFix(code, config);
 		t.is(result.output, expected);
 	});
 }
@@ -55,7 +56,8 @@ for (const [rule, exit, opening] of [
 test('short-body wrapping preserves mixed line endings inside the moved body', t => {
 	const code = 'function foo() {\r\n\tif (!condition) {\r\n\t\treturn;\r\n\t}\r\n\twork(\n\t\tvalue,\r\n\t);\r\n}';
 	const expected = 'function foo() {\r\n\tif (condition) {\r\n\t\twork(\n\t\t\tvalue,\r\n\t\t);\r\n\t}\r\n}';
-	const result = new Linter().verifyAndFix(code, config);
+	const linter = new Linter();
+	const result = linter.verifyAndFix(code, config);
 	t.is(result.output, expected);
 });
 
